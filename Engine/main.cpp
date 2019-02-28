f6cd99cb94aa8411e241907aace4496c003fa357
Alex <wolfplayeral@yahoo.co.uk>
2019-02-28 07:20:44 +0000

    WIP 2: Tutorial 1

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -111,15 +111,136 @@ void enableDebugLayer(){
 #endif
 }
 
-//Stopped on:
+void registerWindowClass(HINSTANCE hIsnt, const wchar_t* windowClassName){
+	WNDCLASSEXW windowClass = {};
 
-/*
-The IID_PPV_ARGS macro shown here on line 118 is used to retrieve an interface pointer, supplying the IID value of the requested interface automatically based on the type of the interface pointer used.
-A common syntax in methods that retrieve an interface pointer includes two parameters:
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = &wndProc;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = hIsnt;
+	windowClass.hIcon = ::LoadIcon(hIsnt, NULL);
+	windowClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	windowClass.lpszMenuName = NULL;
+	windowClass.lpszClassName = windowClassName;
+	windowClass.hIconSm = ::LoadIcon(hIsnt, NULL);
 
-	An [in] parameter, normally of type REFIID, to specify the IID of the interface to retrieve.
-	An [out] parameter, normally of type void**, to receive the interface pointer.
+	static ATOM atom = ::RegisterClassExW(&windowClass);
+	assert(atom > 0);
+}
 
-This macro computes the IID based on the type of interface pointer, which prevents coding errors in which the IID and interface pointer type do not match. Windows developers should always use this macro with any method that requires separate IID and interface pointer parameters.
+HWND createWindow(const wchar_t* windowClassName, HINSTANCE hInst, const wchar_t* windowTitle, uint32_t width, uint32_t height){
+	int screenWidth = ::GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = ::GetSystemMetrics(SM_CYSCREEN);
 
-*/
+	RECT windowRect = { 0, 0, static_cast<LONG>(width), static_cast<LONG>(height) };
+	::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	int windowWidth = windowRect.right - windowRect.left;
+	int windowHeight = windowRect.bottom - windowRect.top;
+
+	//Center window within the screen. clamp 0, 0, for the top left corner
+	int windowX = std::max<int>(0, (screenWidth - windowWidth) / 2);
+	int windowY = std::max<int>(0, (screenHeight - windowHeight) / 2);
+
+	HWND hWnd = ::CreateWindowExW(
+		NULL,
+		windowClassName,
+		windowTitle,
+		WS_OVERLAPPEDWINDOW,
+		windowX,
+		windowY,
+		windowWidth,
+		windowHeight,
+		NULL,
+		NULL,
+		hInst,
+		nullptr
+	);
+
+	assert(hWnd && "Failed to create window");
+
+	return hWnd;
+}
+
+ComPtr<IDXGIAdapter4> getAdapter(bool useWarp){
+	ComPtr<IDXGIFactory4> dxgiFactory;
+	UINT createFactoryFlags = 0;
+#if defined(_DEBUG)
+	createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+	ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dxgiFactory)));
+
+	ComPtr<IDXGIAdapter1> dxgiAdapter1;
+	ComPtr<IDXGIAdapter4> dxgiAdapter4;
+
+	if(useWarp){
+		ThrowIfFailed(dxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&dxgiAdapter1)));
+		ThrowIfFailed(dxgiAdapter1.As(&dxgiAdapter4));
+	} else{
+		SIZE_T maxDedicatedVideoMemory = 0;
+		for(UINT i = 0; dxgiFactory->EnumAdapters1(i, &dxgiAdapter1) != DXGI_ERROR_NOT_FOUND; ++i){
+			DXGI_ADAPTER_DESC1 dxgiAdapterDesc1;
+			dxgiAdapter1->GetDesc1(&dxgiAdapterDesc1);
+
+			// Check to see if the adapter can create a D3D12 device without actually 
+			// creating it. The adapter with the largest dedicated video memory
+			// is favored.
+			if((dxgiAdapterDesc1.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) == 0 && 
+				SUCCEEDED(D3D12CreateDevice(dxgiAdapter1.Get(), D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr)) &&
+				dxgiAdapterDesc1.DedicatedVideoMemory > maxDedicatedVideoMemory){
+				maxDedicatedVideoMemory = dxgiAdapterDesc1.DedicatedVideoMemory;
+				ThrowIfFailed(dxgiAdapter1.As(&dxgiAdapter4));
+			}
+		}
+	}
+
+	return dxgiAdapter4;
+}
+
+ComPtr<ID3D12Device2> createDevice(ComPtr<IDXGIAdapter4> adapter){
+	ComPtr<ID3D12Device2> d3d12Device2;
+	ThrowIfFailed(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&d3d12Device2)));
+
+	//Enable debug messages in debug mode
+#if defined(_DEBUG)
+	ComPtr<ID3D12InfoQueue> pInfoQueue;
+	if(SUCCEEDED(d3d12Device2.As(&pInfoQueue))){
+		pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, TRUE);
+		pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, TRUE);
+		pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, TRUE);
+
+		//Suppress whole categories of messages
+		//D3D12_MESSAGE_CATEGORY categories[] = {};
+
+		//Suppress messages based on their severit level
+		D3D12_MESSAGE_SEVERITY severities[] = {
+			D3D12_MESSAGE_SEVERITY_INFO
+		};
+
+		//Suppress individual messages byu their ID
+		D3D12_MESSAGE_ID denyIds[] = {
+			D3D12_MESSAGE_ID_CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE,   // I'm really not sure how to avoid this message.
+			D3D12_MESSAGE_ID_MAP_INVALID_NULLRANGE,							// This warning occurs when using capture frame while graphics debugging.
+			D3D12_MESSAGE_ID_UNMAP_NULLRANGE								// This warning occurs when using capture frame while graphics debugging.
+		};
+
+		D3D12_INFO_QUEUE_FILTER newFilter = {};
+		//newFilter.DenyList.NumCategories = _countof(categories);
+		//newFilter.DenyList.pCategoryList = categories;
+		newFilter.DenyList.NumSeverities = _countof(severities);
+		newFilter.DenyList.pSeverityList = severities;
+		newFilter.DenyList.NumIDs = _countof(denyIds);
+		newFilter.DenyList.pIDList = denyIds;
+
+		ThrowIfFailed(pInfoQueue->PushStorageFilter(&newFilter));
+	}
+#endif // defined(_DEBUG)
+
+	return d3d12Device2;
+}
+
+//at: Create the Command Queue
