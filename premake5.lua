65e0741ed68f2a6fcbdb60901f6d4285a460dc58
Alex <wolfplayeral@yahoo.co.uk>
2019-03-02 11:32:55 +0000

    Set up a premake script to build the project
    Squashed commit of the following:
        loose files from project generation
        Added a generate project file
        Created premake lua
        Added premake .exe

---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "Clove"
+	architecture "x64"
+
+	configurations{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Clove"
+	location "Clove"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines{
+			"CLV_PLATFORM_WINDOWS",
+			"CLV_BUILD_DLL"
+		}
+
+		postbuildcommands{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CLV_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CLV_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CLV_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs{
+		"Clove/vendor/spdlog/include",
+		"Clove/src"
+	}
+
+	links{
+		"Clove"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines{
+			"CLV_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CLV_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CLV_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CLV_DIST"
+		optimize "On"
