fa5791939d5b9ad2948492acbe5bc5c837f8ace4
Alex <wolfplayeral@yahoo.co.uk>
2019-03-24 20:50:41 +0000

    Added some rough coding standards
    Squashed commit of the following:
        Added some rough coding standards
        Added the coding standards class

---
+++ Clove/src/Clove/CodingStandards.cpp
@@ -0,0 +1,22 @@
+#include "src\Clove\CodingStandards.h"
+#include "clvpch.h"
+#include "CodingStandards.h"
+
+//[namespace.source] always wrap the function declarations in clv namespace
+namespace clv{
+	CodingStandards::CodingStandards(){
+	}
+
+	inline void CodingStandards::goodInlineFunction(){
+
+	}
+
+	void CodingStandards::nonMutable() const{
+	}
+
+	void CodingStandards::constInFunction(){
+		int const variablesToUseLayer = 4;
+
+
+	}
+}
---
+++ Clove/src/Clove/CodingStandards.h
@@ -0,0 +1,47 @@
+#pragma once
+
+/*
+coding standards!
+
+*/
+
+//[IWYU] please keep includes in header files as small as possible
+
+//[namespace.header] always wrap the class in the clv namespace
+namespace clv{
+	class CodingStandards{
+		//[order.section.variables] variables first with comment please!
+		//VARIABLES
+	public:
+
+	protected:
+
+	private:
+		//[initalisation] always initialise variables in the header when possible
+		int myInt = 0;
+
+		//[raw pointer] avoid raw pointers - try and use shared_ptr or unique_ptr
+		int* rawPtrInt = nullptr;
+
+		//[order.section.functions] functions second with comment please!
+		//FUNCTIONS
+	public:
+		CodingStandards();
+		//[destructor.default] no empty constructors! either use default or just remove the definition
+		~CodingStandards() = default;
+
+		//[function.inline] please do not do this!
+		inline void badInlineFunction() { /*do some thing*/ }
+
+		//[function.inline] if you need it inline, mark it as such and declare in cpp
+		inline void goodInlineFunction();
+
+		//[function.const correct] please keep functiosn const correct
+		void nonMutable() const;
+
+	protected:
+		void constInFunction();
+
+	private:
+	};
+}
