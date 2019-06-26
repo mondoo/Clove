950f779a5830e7e82db9106063ac0f9ab2dc4d5a
Alex <wolfplayeral@yahoo.co.uk>
2019-06-26 07:26:40 +0100

    Set up the renderable 2d component

--- Clove/src/Clove/ECS/Components/Renderable2DComponent.cpp
+++ Clove/src/Clove/ECS/Components/Renderable2DComponent.cpp
@@ -2,5 +2,15 @@
 #include "Renderable2DComponent.hpp"
 
 namespace clv::ecs{
+	Renderable2DComponent::Renderable2DComponent() = default;
 
+	Renderable2DComponent::Renderable2DComponent(Renderable2DComponent&& other) noexcept = default;
+
+	Renderable2DComponent& Renderable2DComponent::operator=(Renderable2DComponent&& other) noexcept = default;
+
+	Renderable2DComponent::~Renderable2DComponent() = default;
+
+	void Renderable2DComponent::setTexture(std::unique_ptr<gfx::Texture> texture){
+		this->texture = std::move(texture);
+	}
 }
--- Clove/src/Clove/ECS/Components/Renderable2DComponent.hpp
+++ Clove/src/Clove/ECS/Components/Renderable2DComponent.hpp
@@ -2,13 +2,31 @@
 
 #include "Clove/ECS/Component.hpp"
 
+namespace clv::gfx{
+	class Texture;
+}
+
 namespace clv::ecs{
 	class Renderable2DComponent : public Component{
+		friend class Render2DSystem;
+
 		//VARIABLES
 	public:
 		static constexpr ComponentID ID = 0x28fcf790; //VS Generated GUID
 
 	private:
-		//...
+		std::unique_ptr<gfx::Texture> texture;
+
+		//FUNCTIONS	
+	public:
+		Renderable2DComponent();
+		Renderable2DComponent(const Renderable2DComponent& other) = delete;
+		Renderable2DComponent(Renderable2DComponent&& other) noexcept;
+		Renderable2DComponent& operator=(const Renderable2DComponent& other) = delete;
+		Renderable2DComponent& operator=(Renderable2DComponent&& other) noexcept;
+		virtual ~Renderable2DComponent();
+
+	protected:
+		void setTexture(std::unique_ptr<gfx::Texture> texture);
 	};
 }
