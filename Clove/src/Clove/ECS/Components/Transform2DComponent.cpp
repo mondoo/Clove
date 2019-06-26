5617c5cec8ed9354dbb8912307622acca2c3336a
Alex <wolfplayeral@yahoo.co.uk>
2019-06-26 07:19:59 +0100

    Set up the transform 2d component

--- Clove/src/Clove/ECS/Components/Transform2DComponent.cpp
+++ Clove/src/Clove/ECS/Components/Transform2DComponent.cpp
@@ -2,5 +2,46 @@
 #include "Transform2DComponent.hpp"
 
 namespace clv::ecs{
+	Transform2DComponent::Transform2DComponent() = default;
 
+	Transform2DComponent::Transform2DComponent(Transform2DComponent&& other) noexcept = default;
+
+	Transform2DComponent& Transform2DComponent::operator=(Transform2DComponent&& other) noexcept = default;
+
+	Transform2DComponent::~Transform2DComponent() = default;
+
+	void Transform2DComponent::setLocalPosition(const math::Vector2f& inLocalPosition){
+		localPosition = inLocalPosition;
+	}
+
+	const math::Vector2f& Transform2DComponent::getLocalPosition() const{
+		return localPosition;
+	}
+
+	void Transform2DComponent::setLocalRotation(const std::pair<math::Vector2f, float>& inLocalRotation){
+		localRotation = inLocalRotation;
+	}
+
+	const std::pair<math::Vector2f, float>& Transform2DComponent::getLocalRotation() const{
+		return localRotation;
+	}
+
+	void Transform2DComponent::setLocalScale(const math::Vector2f& inLocalScale){
+		localScale = inLocalScale;
+	}
+
+	const math::Vector2f& Transform2DComponent::getLocalScale() const{
+		return localScale;
+	}
+
+	Transform2DComponent* Transform2DComponent::getParent() const{
+		return parent;
+	}
+
+	void Transform2DComponent::addChild(Transform2DComponent* child){
+		if(child && child != this){
+			children.push_back(child);
+			child->parent = this;
+		}
+	}
 }
--- Clove/src/Clove/ECS/Components/Transform2DComponent.hpp
+++ Clove/src/Clove/ECS/Components/Transform2DComponent.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Clove/ECS/Component.hpp"
+#include <Clove\ECS\Components\Transform3DComponent.cpp>
 
 namespace clv::ecs{
 	class Transform2DComponent : public Component{
@@ -9,6 +10,32 @@ namespace clv::ecs{
 		static constexpr ComponentID ID = 0x1ae98fd4; //VS Generated GUID
 
 	private:
-		//...
+		math::Vector2f						localPosition = { 0.0f, 0.0f };
+		std::pair<math::Vector2f, float>	localRotation = std::pair<math::Vector2f, float>(math::Vector2f(1.0f), 0.0f);
+		math::Vector2f						localScale = { 1.0f, 1.0f };
+
+		Transform2DComponent* parent = nullptr;
+		std::vector<Transform2DComponent*> children;
+
+		//FUNCTIONS	
+	public:
+		Transform2DComponent();
+		Transform2DComponent(const Transform2DComponent& other) = delete;
+		Transform2DComponent(Transform2DComponent&& other) noexcept;
+		Transform2DComponent& operator=(const Transform2DComponent& other) = delete;
+		Transform2DComponent& operator=(Transform2DComponent&& other) noexcept;
+		virtual ~Transform2DComponent();
+
+		void setLocalPosition(const math::Vector2f& inLocalPosition);
+		const math::Vector2f& getLocalPosition() const;
+
+		void setLocalRotation(const std::pair<math::Vector2f, float>& inLocalRotation);
+		const std::pair<math::Vector2f, float>& getLocalRotation() const;
+
+		void setLocalScale(const math::Vector2f& inLocalScale);
+		const math::Vector2f& getLocalScale() const;
+
+		Transform2DComponent* getParent() const;
+		void addChild(Transform2DComponent* child);
 	};
 }
