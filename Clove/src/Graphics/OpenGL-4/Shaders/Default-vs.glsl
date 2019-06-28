56c3e25a726883f99e3fb56593fdd00c6fac1594
Alex <wolfplayeral@yahoo.co.uk>
2019-06-28 07:22:54 +0100

    Input param on gl vertex shader has been made to be a position 3d

--- Clove/src/Graphics/OpenGL-4/Shaders/Default-vs.glsl
+++ Clove/src/Graphics/OpenGL-4/Shaders/Default-vs.glsl
@@ -1,6 +1,6 @@
 #version 460 core
 
-layout(location = 0) in vec4 position;
+layout(location = 0) in vec3 position;
 layout(location = 1) in vec2 texCoord;
 layout(location = 2) in vec3 normal;
 
@@ -17,11 +17,13 @@ out vec3 vertPos;
 out vec3 vertNormal;
 
 void main(){
-   gl_Position = projection * view * model * position;
+	const vec4 pos4D = vec4(position, 1.0f);
+
+   gl_Position = projection * view * model * pos4D;
   
    vertTexCoord = texCoord;
 
    //Convert frag and normal to world space
-   vertPos		= vec3(model * position);
+   vertPos		= vec3(model * pos4D);
    vertNormal	= mat3(normalMatrix) * normal;
 };
