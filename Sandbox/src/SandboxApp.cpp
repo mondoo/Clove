9b0430faa84cc803a0a5767e7daa9f61feee7fc0
Alex <wolfplayeral@yahoo.co.uk>
2019-07-21 18:02:45 +0100

    Added the Gamelayer in sandbox

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,13 @@
 #include <Clove.hpp>
 
 #include "ExampleLayer.hpp"
+#include "GameLayer.hpp"
 
 class SandBox : public clv::Application{
 public:
 	SandBox(){
-		pushLayer(std::make_shared<ExampleLayer>());
+		//pushLayer(std::make_shared<ExampleLayer>());
+		pushLayer(std::make_shared<GameLayer>());
 	}
 	~SandBox(){
 
