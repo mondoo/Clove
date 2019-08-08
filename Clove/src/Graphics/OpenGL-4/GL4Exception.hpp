037cc956d5187946172e4c110c76b6039aea6223
Alex <wolfplayeral@yahoo.co.uk>
2019-08-08 19:24:41 +0100

    Added the APIENTRY macro to correctly prototype the function

--- Clove/src/Graphics/OpenGL-4/GL4Exception.hpp
+++ Clove/src/Graphics/OpenGL-4/GL4Exception.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace clv::gfx{
-	void errorCallback(uint32 source, uint32 type, uint32 id,
+	void APIENTRY errorCallback(uint32 source, uint32 type, uint32 id,
 					   uint32 severity, int32 length,
 					   const char* msg, const void* data);
 }
