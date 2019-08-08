13a976440b5a94f8b3ebb1f6d7c00d9411cbfb05
Alex <wolfplayeral@yahoo.co.uk>
2019-08-08 18:30:19 +0100

    Added the clv integer types

---
+++ Clove/src/Clove/IntTypes.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace clv{
+	using int8		= int8_t;
+	using int16		= int16_t;
+	using int32		= int32_t;
+	using int64		= int64_t;
+
+	using uint8		= int8_t;
+	using uint16	= int16_t;
+	using uint32	= int32_t;
+	using uint64	= int64_t;
+}
