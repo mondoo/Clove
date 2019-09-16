77b77a7f804317174a62e037d1f68618a79e48ed
Alex <wolfplayeral@yahoo.co.uk>
2019-09-16 20:27:41 +0100

    Moved the font files to clove

--- Clove/src/Clove/UI/Font.hpp
+++ Clove/src/Clove/UI/Font.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+/*
+Inteprets font files into some generic data that can be used to create tex coords ect.
+
+TODO: put inside utility???
+*/
+
 namespace clv::ui{
 	class Font{
 		//VARIABLES
