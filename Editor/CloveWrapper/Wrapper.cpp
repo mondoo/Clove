18383dbd383577621f5c6fdd3a51ad6f202166c9
Alex <wolfplayeral@yahoo.co.uk>
2019-10-04 07:37:52 +0100

    Can now open the window from the C# application

--- Editor/CloveWrapper/Wrapper.cpp
+++ Editor/CloveWrapper/Wrapper.cpp
@@ -1,27 +1,33 @@
 #include "Wrapper.hpp"
-//#include "clvpch.hpp"//TODO: See note in example layer
-//#include <Clove.hpp>
-//
-//class Editor : public clv::Application{
-//public:
-//	Editor(){
-//		//pushLayer(std::make_shared<TestLayer>());
-//		//pushLayer(std::make_shared<GameLayer>());
-//	}
-//	~Editor(){
-//
-//	}
-//
-//private:
-//
-//};
-//
-//clv::Application* clv::createApplication(){
-//	return new Editor;
-//}
+
+#include "clvpch.hpp"//TODO: See note in example layer
+#include <Clove.hpp>
+
+class Editor : public clv::Application{
+public:
+	Editor(){
+		//pushLayer(std::make_shared<TestLayer>());
+		//pushLayer(std::make_shared<GameLayer>());
+	}
+	~Editor(){
+
+	}
+
+private:
+
+};
+
+clv::Application* clv::createApplication(){
+	return new Editor;
+}
 
 void Set(int val){
 	globaldata = val;
+
+	clv::Log::init();
+	auto* app = clv::createApplication();
+	app->run();
+	delete app;
 }
 
 int Get(){
