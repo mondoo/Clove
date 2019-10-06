78a1f4dbf4ed06421eebf6eebb44c02e4c9bf988
Alex <wolfplayeral@yahoo.co.uk>
2019-10-06 08:38:42 +0100

    Set up debugging for the CLR project

--- Editor/CloveWrapper/Wrapper.cpp
+++ Editor/CloveWrapper/Wrapper.cpp
@@ -5,18 +5,10 @@
 
 #include "Clove/Application.hpp"
 
-/*
-something inside https://help.veracode.com/reader/4EKhlLSMHm5jC8P8j3XccQ/UWyzuOKNbFbwQACRLkZVjA
-helped debug - TODO: Set these up in CMake (bar RTC1)
-
-also fix the C# platform
-*/
-
 class Editor : public clv::Application{
 public:
 	Editor(){
-		//pushLayer(std::make_shared<TestLayer>());
-		//pushLayer(std::make_shared<GameLayer>());
+		
 	}
 	~Editor(){
 
