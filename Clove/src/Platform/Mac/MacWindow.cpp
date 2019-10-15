a1a6407fa57e23b96af54a95a98ccd904ce4be18
Alex <wolfplayeral@yahoo.co.uk>
2019-10-15 23:00:07 +0100

    Set up the skeleton for the mac window

--- Clove/src/Platform/Mac/MacWindow.cpp
+++ Clove/src/Platform/Mac/MacWindow.cpp
@@ -1,1 +1,21 @@
-//TODO
+#include "MacWindow.hpp"
+
+namespace clv {
+    MacWindow::~MacWindow() = default;
+
+    MacWindow::MacWindow(const WindowProps& props){
+        //TODO
+    }
+    
+    MacWindow::MacWindow(const WindowProps& props, gfx::API api){
+        //TODO
+    }
+
+    Window* Window::create(const WindowProps& props){
+        return new MacWindow(props);
+    }
+
+    Window* Window::create(const WindowProps& props, gfx::API api){
+        return new MacWindow(props, api);
+    }
+}
--- Clove/src/Platform/Mac/MacWindow.hpp
+++ Clove/src/Platform/Mac/MacWindow.hpp
@@ -1,1 +1,22 @@
-//TODO
+#pragma once
+
+#include "Clove/Platform/Window"
+
+namespace clv{
+    class MacWindow : public Window{
+        //VARIABLES
+    private:
+        
+        //FUNCTIONS
+    public:
+        MacWindow() = delete;
+        MacWindow(const MacWindow& other) = delete;
+        MacWindow(MacWindow&& other) noexcept = delete;
+        MacWindow& operator=(const MacWindow& other) = delete;
+        MacWindow& operator=(MacWindow&& other) noexcept = delete;
+        virtual ~MacWindow();
+        
+        MacWindow(const WindowProps& props);
+        MacWindow(const WindowProps& props, gfx::API api);
+    };
+}
