5c66e3dd963cdb7ea82f6dbb13361cb4e2983ecf
Alex <wolfplayeral@yahoo.co.uk>
2019-10-20 18:04:21 +0100

    Put the sandbox back as a basic cpp application

--- Sandbox/src/SandboxApp.mm
+++ Sandbox/src/SandboxApp.mm
@@ -1,10 +1,4 @@
-#import "Platform/Mac/CloveMac.h"
-#import <Clove.hpp>
-
-#import "TestLayer.hpp"
-#import "GameLayer.hpp"
-
-#import "AppDelegate.h"
+#import "AppDelegate.hpp"
 
 int main(int argc, char** argv){
 	int status = 0;
@@ -14,33 +8,4 @@ int main(int argc, char** argv){
 		status = NSApplicationMain(argc, (const char**)argv);
 	}
 	return status;
-	
-	try{
-		//auto* app = new clv::Application();
-
-		//app->pushLayer(std::make_shared<TestLayer>());
-		//app->pushLayer(std::make_shared<GameLayer>());
-
-		//while(app->getState() == clv::ApplicationState::running){
-		//	app->update();
-		//}
-
-		//delete app;
-	} catch(const clv::CloveException & e){
-	#if CLV_PLATFORM_WINDOWS
-		MessageBoxA(nullptr, e.what(), e.getType(), MB_OK | MB_ICONEXCLAMATION);
-	#endif
-	#if	CLV_RELEASE
-	} catch(const std::exception & e){
-	#if CLV_PLATFORM_WINDOWS
-		MessageBoxA(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
-	#endif
-	} catch(...){
-	#if CLV_PLATFORM_WINDOWS
-		MessageBoxA(nullptr, "No details available", "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
-	#endif
-	#endif
-	}
-	
-	return 0;
 }
