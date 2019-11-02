242a29a199b5310db9281127c51e3216d4b8f402
Alex <wolfplayeral@yahoo.co.uk>
2019-11-02 14:42:51 +0000

    Added an std::hash specialisation for the MulticastDelegateHandle

--- Clove/src/Core/Utils/Delegate.hpp
+++ Clove/src/Core/Utils/Delegate.hpp
@@ -1,6 +1,29 @@
 #pragma once
 
 namespace clv::utl{
+	struct MultiCastDelegateHandle{
+		//VARIABLES
+	public:
+		const std::optional<int32> ID = {};
+
+		//FUNCTIONS
+	public:
+		MultiCastDelegateHandle() = default;
+		MultiCastDelegateHandle(int32 ID) : ID(ID){}
+
+		operator int() const{ return ID.value_or(-1); }
+	};
+}
+
+namespace std{
+	template<> struct hash<clv::utl::MultiCastDelegateHandle>{
+		size_t operator()(const clv::utl::MultiCastDelegateHandle& handle){
+			return hash<int>()(handle);
+		}
+	};
+}
+
+namespace clv::utl{
 	template<typename FunctionPrototype>
 	class SingleCastDelegate{
 		//VARIABLES
@@ -23,26 +46,11 @@ namespace clv::utl{
 		auto broadcast(Args&& ...args) const;
 	};
 
-	struct MultiCastDelegateHandle{
-		//VARIABLES
-	public:
-		const std::optional<int32> ID = {};
-
-		//FUNCTIONS
-	public:
-		MultiCastDelegateHandle() = default;
-		MultiCastDelegateHandle(int32 ID) : ID(ID){}
-
-		operator int() const{ return ID.value_or(-1); }
-
-		bool operator <(const MultiCastDelegateHandle& rhs) const{ return ID.value_or(-1) < rhs.ID.value_or(-1); }
-	};
-
 	template<typename FunctionPrototype>
 	class MultiCastDelegate{
 		//VARIABLES
 	private:
-		std::map<MultiCastDelegateHandle, std::function<FunctionPrototype>> functionPointers;
+		std::unordered_map<MultiCastDelegateHandle, std::function<FunctionPrototype>> functionPointers;
 
 		int32 nextID = 0;
 
