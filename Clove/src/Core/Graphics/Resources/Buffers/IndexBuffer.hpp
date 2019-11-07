77ce1487be31691ce4d68854d617433adc850ab4
Alex <wolfplayeral@yahoo.co.uk>
2019-11-07 07:38:02 +0000

    Index buffer descriptor now describes the buffer instead of giving it the data

--- Clove/src/Core/Graphics/Resources/Buffers/IndexBuffer.hpp
+++ Clove/src/Core/Graphics/Resources/Buffers/IndexBuffer.hpp
@@ -4,7 +4,8 @@
 
 namespace clv::gfx{
 	struct IndexBufferDescriptor{
-		std::vector<uint32> indices;
+		uint32 elementsize = 0;
+		uint32 bufferSize = 0;
 	};
 }
 
@@ -12,6 +13,8 @@ namespace clv::gfx{
 	class IndexBuffer : public Buffer{
 		//FUNCTIONS
 	public:
+		virtual void setIndices(const std::vector<uint32>& indices) = 0;
+
 		virtual uint32 getIndexCount() const = 0;
 	};
 }
