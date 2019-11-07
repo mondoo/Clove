17dd80697a2cf1c375ec516f1b335e88eb4e7e68
Alex <wolfplayeral@yahoo.co.uk>
2019-11-07 06:59:25 +0000

    Vertex descriptor now has the same arguments as the old constructor

--- Clove/src/Core/Graphics/Resources/Buffers/VertexBuffer.hpp
+++ Clove/src/Core/Graphics/Resources/Buffers/VertexBuffer.hpp
@@ -2,30 +2,20 @@
 
 #include "Core/Graphics/Resources/Buffer.hpp"
 
-//#include "Core/Graphics/VertexLayout.hpp"
-
 namespace clv::gfx{
+	class Shader;
+
 	struct VertexBufferDescriptor{
-		//TODO
+		VertexBufferData bufferData;
+		std::shared_ptr<Shader> shader;
 	};
 }
 
 namespace clv::gfx{
 	class VertexBuffer : public Buffer{
-		//VARIABLES
-	protected:
-		//VertexBufferData bufferData; //TODO: Chuck back in?
-
 		//FUNCTIONS
 	public:
-		VertexBuffer()/* = delete*/ = default;
-		VertexBuffer(const VertexBuffer& other) = delete;
-		VertexBuffer(VertexBuffer&& other) noexcept = default;
-		VertexBuffer& operator=(const VertexBuffer& other) = delete;
-		VertexBuffer& operator=(VertexBuffer&& other) noexcept = default;
-		virtual ~VertexBuffer() = default;
-
-		//VertexBuffer(const VertexBufferData& bufferData);
+		//TODO
 	};
 }
 
