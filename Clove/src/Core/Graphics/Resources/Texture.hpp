12dc946e4fab16ec8dab82bcfbe3202882bd198e
Alex <wolfplayeral@yahoo.co.uk>
2019-11-07 07:28:24 +0000

    Updated the texture API to allow the resource to change

--- Clove/src/Core/Graphics/Resources/Texture.hpp
+++ Clove/src/Core/Graphics/Resources/Texture.hpp
@@ -3,18 +3,19 @@
 #include "Core/Graphics/GraphicsTypes.hpp"
 
 namespace clv::gfx{
-	struct TextureDescriptor{
-		TextureStyle style = TextureStyle::Default;
-		TextureUsage usage = TextureUsage::Default;
-		math::Vector<2, uint32, math::qualifier::defaultp> dimensions = { 0, 0 };
-		uint8 arraySize = 1;
-	};
-
 	enum TextureBindingPoint{
 		TBP_Albedo = 1u,
 		TBP_Specular = 2u,
 		TBP_Shadow = 3u,
 		TBP_None = 4u
+	};
+
+	struct TextureDescriptor{
+		TextureBindingPoint bindingPoint = TBP_Albedo;
+		TextureStyle style = TextureStyle::Default;
+		TextureUsage usage = TextureUsage::Default;
+		math::Vector<2, uint32, math::qualifier::defaultp> dimensions = { 0, 0 };
+		uint8 arraySize = 1;
 	};
 }
 
@@ -22,6 +23,9 @@ namespace clv::gfx{
 	class Texture{
 		//FUNCTIONS
 	public:
+		virtual void map(const std::string& pathToTexture) = 0;
+		virtual void map(void* data) = 0;
+
 		virtual uint32 getWidth() const = 0;
 		virtual uint32 getHeight() const = 0;
 
