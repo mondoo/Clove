d38cb1b697fa3d604bb6b5850a64f147b65b09b3
Alex <wolfplayeral@yahoo.co.uk>
2019-11-09 17:48:12 +0000

    Refactored the D3DShader to be able to get shaders directly

--- Clove/src/Graphics/Direct3D/D3DShader.cpp
+++ Clove/src/Graphics/Direct3D/D3DShader.cpp
@@ -12,17 +12,9 @@ namespace clv::gfx::d3d::_11{
 		initialise(d3dDevice, descriptor.style);
 	}
 
-	D3DShader::D3DShader(D3DShader&& other) noexcept{
-		d3dShaders = std::move(other.d3dShaders);
-		vertexShader = other.vertexShader;
-	}
+	D3DShader::D3DShader(D3DShader&& other) noexcept = default;
 
-	D3DShader& D3DShader::operator=(D3DShader&& other) noexcept{
-		d3dShaders = std::move(other.d3dShaders);
-		vertexShader = other.vertexShader;
-
-		return *this;
-	}
+	D3DShader& D3DShader::operator=(D3DShader&& other) noexcept = default;
 
 	D3DShader::~D3DShader() = default;
 
@@ -35,13 +27,13 @@ namespace clv::gfx::d3d::_11{
 
 		Microsoft::WRL::ComPtr<ID3D11ShaderReflection> reflector;
 
-		if(vertexShader == nullptr){
+		if(d3dVertexShader == nullptr){
 			CLV_LOG_ERROR("{0}: vertex shader hasn't been initialised, potentially calling function too early!");
 			return outData;
 		}
 
 		DX11_INFO_PROVIDER;
-		DX11_THROW_INFO(D3DReflect(vertexShader->getBytePointer(), vertexShader->getByteSize(), IID_ID3D11ShaderReflection, &reflector));
+		DX11_THROW_INFO(D3DReflect(vertexByteData, vertexByteSize, IID_ID3D11ShaderReflection, &reflector));
 
 		D3D11_SHADER_DESC shaderDescription;
 		DX11_THROW_INFO(reflector->GetDesc(&shaderDescription));
@@ -58,142 +50,77 @@ namespace clv::gfx::d3d::_11{
 		return outData;
 	}
 
-	const std::unordered_map<ShaderType, std::unique_ptr<D3DShaderElement>>& D3DShader::getD3DShaders() const{
-		return d3dShaders;
+	const Microsoft::WRL::ComPtr<ID3D11VertexShader>& D3DShader::getD3DVertexShader() const{
+		return d3dVertexShader;
+	}
+
+	const Microsoft::WRL::ComPtr<ID3D11GeometryShader>& D3DShader::getD3DGeometryShader() const{
+		return d3dGeometryShader;
+	}
+
+	const Microsoft::WRL::ComPtr<ID3D11PixelShader>& D3DShader::getD3DPixelShader() const{
+		return d3dPixelShader;
 	}
 
 	void D3DShader::initialise(ID3D11Device& d3dDevice, ShaderStyle style){
+		DX11_INFO_PROVIDER;
+
 		switch(style){
 			case ShaderStyle::Lit_3D:
-				{
-					auto vs = std::make_unique<D3DVertexShader>(shader_Lit_vs, sizeof(shader_Lit_vs));
-					vertexShader = vs.get();
+				vertexByteData = shader_Lit_vs;
+				vertexByteSize = sizeof(shader_Lit_vs);
 
-					d3dShaders[ShaderType::Vertex] = std::move(vs);
-					d3dShaders[ShaderType::Pixel] = std::make_unique<D3DPixelShader>(shader_Lit_ps, sizeof(shader_Lit_ps));
-				}
+				DX11_THROW_INFO(d3dDevice.CreateVertexShader(vertexByteData, vertexByteSize, nullptr, &d3dVertexShader));
+				DX11_THROW_INFO(d3dDevice.CreatePixelShader(shader_Lit_ps, sizeof(shader_Lit_ps), nullptr, &d3dPixelShader));
+
 				break;
 
 			case ShaderStyle::Unlit_3D:
-				{
-					auto vs = std::make_unique<D3DVertexShader>(shader_Unlit_vs, sizeof(shader_Unlit_vs));
-					vertexShader = vs.get();
+				vertexByteData = shader_Unlit_vs;
+				vertexByteSize = sizeof(shader_Unlit_vs);
 
-					d3dShaders[ShaderType::Vertex] = std::move(vs);
-					d3dShaders[ShaderType::Pixel] = std::make_unique<D3DPixelShader>(shader_Unlit_ps, sizeof(shader_Unlit_ps));
-				}
+				DX11_THROW_INFO(d3dDevice.CreateVertexShader(vertexByteData, vertexByteSize, nullptr, &d3dVertexShader));
+				DX11_THROW_INFO(d3dDevice.CreatePixelShader(shader_Unlit_ps, sizeof(shader_Unlit_ps), nullptr, &d3dPixelShader));
+
 				break;
 
 			case ShaderStyle::Unlit_2D:
-				{
-					auto vs = std::make_unique<D3DVertexShader>(shader_2D_vs, sizeof(shader_2D_vs));
-					vertexShader = vs.get();
+				vertexByteData = shader_2D_vs;
+				vertexByteSize = sizeof(shader_2D_vs);
 
-					d3dShaders[ShaderType::Vertex] = std::move(vs);
-					d3dShaders[ShaderType::Pixel] = std::make_unique<D3DPixelShader>(shader_2D_ps, sizeof(shader_2D_ps));
-				}
+				DX11_THROW_INFO(d3dDevice.CreateVertexShader(vertexByteData, vertexByteSize, nullptr, &d3dVertexShader));
+				DX11_THROW_INFO(d3dDevice.CreatePixelShader(shader_2D_ps, sizeof(shader_2D_ps), nullptr, &d3dPixelShader));
+
 				break;
 
 			case ShaderStyle::RT:
-				{
-					auto vs = std::make_unique<D3DVertexShader>(shader_RT_vs, sizeof(shader_RT_vs));
-					vertexShader = vs.get();
+				vertexByteData = shader_RT_vs;
+				vertexByteSize = sizeof(shader_RT_vs);
 
-					d3dShaders[ShaderType::Vertex] = std::move(vs);
-					d3dShaders[ShaderType::Pixel] = std::make_unique<D3DPixelShader>(shader_RT_ps, sizeof(shader_RT_ps));
-				}
+				DX11_THROW_INFO(d3dDevice.CreateVertexShader(vertexByteData, vertexByteSize, nullptr, &d3dVertexShader));
+				DX11_THROW_INFO(d3dDevice.CreatePixelShader(shader_RT_ps, sizeof(shader_RT_ps), nullptr, &d3dPixelShader));
 				break;
 
 			case ShaderStyle::Font:
-				{
-					auto vs = std::make_unique<D3DVertexShader>(shader_Font_vs, sizeof(shader_Font_vs));
-					vertexShader = vs.get();
+				vertexByteData = shader_Font_vs;
+				vertexByteSize = sizeof(shader_Font_vs);
 
-					d3dShaders[ShaderType::Vertex] = std::move(vs);
-					d3dShaders[ShaderType::Pixel] = std::make_unique<D3DPixelShader>(shader_Font_ps, sizeof(shader_Font_ps));
-				}
+				DX11_THROW_INFO(d3dDevice.CreateVertexShader(vertexByteData, vertexByteSize, nullptr, &d3dVertexShader));
+				DX11_THROW_INFO(d3dDevice.CreatePixelShader(shader_Font_ps, sizeof(shader_Font_ps), nullptr, &d3dPixelShader));
 				break;
 
 			case ShaderStyle::CubeShadowMap:
-				{
-					auto vs = std::make_unique<D3DVertexShader>(shader_CubeShadowMap_vs, sizeof(shader_CubeShadowMap_vs));
-					vertexShader = vs.get();
+				vertexByteData = shader_CubeShadowMap_vs;
+				vertexByteSize = sizeof(shader_CubeShadowMap_vs);
 
-					d3dShaders[ShaderType::Vertex] = std::move(vs);
-					d3dShaders[ShaderType::Pixel] = std::make_unique<D3DPixelShader>(shader_CubeShadowMap_ps, sizeof(shader_CubeShadowMap_ps));
-					d3dShaders[ShaderType::Geometry] = std::make_unique<D3DGeometryShader>(shader_CubeShadowMap_gs, sizeof(shader_CubeShadowMap_gs));
-				}
+				DX11_THROW_INFO(d3dDevice.CreateVertexShader(vertexByteData, vertexByteSize, nullptr, &d3dVertexShader));
+				DX11_THROW_INFO(d3dDevice.CreateGeometryShader(shader_CubeShadowMap_gs, sizeof(shader_CubeShadowMap_gs), nullptr, &d3dGeometryShader));
+				DX11_THROW_INFO(d3dDevice.CreatePixelShader(shader_CubeShadowMap_ps, sizeof(shader_CubeShadowMap_ps), nullptr, &d3dPixelShader));
 				break;
 
 			default:
 				CLV_ASSERT(false, "Unknown type! {0}", CLV_FUNCTION_NAME);
 				break;
 		}
-	}
-
-	D3DVertexShader::D3DVertexShader(ID3D11Device& d3dDevice, const BYTE* shaderByteData, SIZE_T shaderByteSize){
-		this->shaderByteData = shaderByteData;
-		this->shaderByteSize = shaderByteSize;
-
-		DX11_INFO_PROVIDER;
-		DX11_THROW_INFO(d3dDevice.CreateVertexShader(shaderByteData, shaderByteSize, nullptr, &vertexShader));
-	}
-
-	D3DVertexShader::D3DVertexShader(D3DVertexShader&& other) noexcept = default;
-
-	D3DVertexShader& D3DVertexShader::operator=(D3DVertexShader&& other) noexcept = default;
-
-	D3DVertexShader::~D3DVertexShader() = default;
-
-	const BYTE* D3DVertexShader::getBytePointer() const{
-		return shaderByteData;
-	}
-
-	SIZE_T D3DVertexShader::getByteSize() const{
-		return shaderByteSize;
-	}
-
-	D3DPixelShader::D3DPixelShader(ID3D11Device& d3dDevice, const BYTE* shaderByteData, SIZE_T shaderByteSize){
-		this->shaderByteData = shaderByteData;
-		this->shaderByteSize = shaderByteSize;
-
-		DX11_INFO_PROVIDER;
-		DX11_THROW_INFO(d3dDevice.CreatePixelShader(shaderByteData, shaderByteSize, nullptr, &pixelShader));
-	}
-
-	D3DPixelShader::D3DPixelShader(D3DPixelShader&& other) noexcept = default;
-
-	D3DPixelShader& D3DPixelShader::operator=(D3DPixelShader&& other) noexcept = default;
-
-	D3DPixelShader::~D3DPixelShader() = default;
-
-	const BYTE* D3DPixelShader::getBytePointer() const{
-		return shaderByteData;
-	}
-
-	SIZE_T D3DPixelShader::getByteSize() const{
-		return shaderByteSize;
-	}
-
-	D3DGeometryShader::D3DGeometryShader(ID3D11Device& d3dDevice, const BYTE* shaderByteData, SIZE_T shaderByteSize){
-		this->shaderByteData = shaderByteData;
-		this->shaderByteSize = shaderByteSize;
-
-		DX11_INFO_PROVIDER;
-		DX11_THROW_INFO(d3dDevice.CreateGeometryShader(shaderByteData, shaderByteSize, nullptr, &geometryShader));
-	}
-
-	D3DGeometryShader::D3DGeometryShader(D3DGeometryShader&& other) noexcept = default;
-
-	D3DGeometryShader& D3DGeometryShader::operator=(D3DGeometryShader&& other) noexcept = default;
-
-	D3DGeometryShader::~D3DGeometryShader() = default;
-
-	const BYTE* D3DGeometryShader::getBytePointer() const{
-		return shaderByteData;
-	}
-
-	SIZE_T D3DGeometryShader::getByteSize() const{
-		return shaderByteSize;
 	}
 }
--- Clove/src/Graphics/Direct3D/D3DShader.hpp
+++ Clove/src/Graphics/Direct3D/D3DShader.hpp
@@ -5,12 +5,21 @@
 #include <wrl.h>
 #include <d3d.h>
 
+struct ID3D11Device;
+struct ID3D11VertexShader;
+struct ID3D11GeometryShader;
+struct ID3D11PixelShader;
+
 namespace clv::gfx::d3d::_11{
 	class D3DShader : public Shader{
 		//VARIABLES
 	private:
-		std::unordered_map<ShaderType, std::unique_ptr<D3DShaderElement>> d3dShaders;
-		D3DVertexShader* vertexShader = nullptr;
+		Microsoft::WRL::ComPtr<ID3D11VertexShader> d3dVertexShader;
+		Microsoft::WRL::ComPtr<ID3D11GeometryShader> d3dGeometryShader;
+		Microsoft::WRL::ComPtr<ID3D11PixelShader> d3dPixelShader;
+
+		const BYTE* vertexByteData = nullptr;
+		SIZE_T vertexByteSize = 0u;
 
 		ShaderDescriptor descriptor;
 		ShaderReflectionData reflectionData;
@@ -28,84 +37,11 @@ namespace clv::gfx::d3d::_11{
 		virtual const ShaderDescriptor& getDescriptor() const override;
 		virtual ShaderReflectionData getReflectionData() const override;
 
-		const std::unordered_map<ShaderType, std::unique_ptr<D3DShaderElement>>& getD3DShaders() const;
+		const Microsoft::WRL::ComPtr<ID3D11VertexShader>& getD3DVertexShader() const;
+		const Microsoft::WRL::ComPtr<ID3D11GeometryShader>& getD3DGeometryShader() const;
+		const Microsoft::WRL::ComPtr<ID3D11PixelShader>& getD3DPixelShader() const;
 
 	private:
 		void initialise(ID3D11Device& d3dDevice, ShaderStyle style);
-	};
-
-	//TODO: move all of this out of this header
-
-	class D3DShaderElement{
-		//FUNCTIONS	
-	public:
-		virtual const BYTE* getBytePointer() const = 0;
-		virtual SIZE_T getByteSize() const = 0;
-	};
-
-	class D3DVertexShader : public D3DShaderElement{
-		//VARIABLES
-	private:
-		Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader;
-		
-		const BYTE* shaderByteData;
-		SIZE_T shaderByteSize;
-
-		//FUNCTIONS
-	public:
-		D3DVertexShader() = delete;
-		D3DVertexShader(ID3D11Device& d3dDevice, const BYTE* shaderByteData, SIZE_T shaderByteSize);
-		D3DVertexShader(const D3DVertexShader& other) = delete;
-		D3DVertexShader(D3DVertexShader&& other) noexcept;
-		D3DVertexShader& operator=(const D3DVertexShader& other) = delete;
-		D3DVertexShader& operator=(D3DVertexShader&& other) noexcept;
-		~D3DVertexShader();
-
-		virtual const BYTE* getBytePointer() const override;
-		virtual SIZE_T getByteSize() const override;
-	};
-
-	class D3DPixelShader : public D3DShaderElement{
-		//VARIABLES
-	private:
-		Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader;
-
-		const BYTE* shaderByteData;
-		SIZE_T shaderByteSize;
-
-		//FUNCTIONS
-	public:
-		D3DPixelShader() = delete;
-		D3DPixelShader(ID3D11Device& d3dDevice, const BYTE* shaderByteData, SIZE_T shaderByteSize);
-		D3DPixelShader(const D3DPixelShader& other) = delete;
-		D3DPixelShader(D3DPixelShader&& other) noexcept;
-		D3DPixelShader& operator=(const D3DPixelShader& other) = delete;
-		D3DPixelShader& operator=(D3DPixelShader&& other) noexcept;
-		~D3DPixelShader();
-
-		virtual const BYTE* getBytePointer() const override;
-		virtual SIZE_T getByteSize() const override;
-	};
-
-	class D3DGeometryShader : public D3DShaderElement{
-		//VARIABLES
-	private:
-		Microsoft::WRL::ComPtr<ID3D11GeometryShader> geometryShader;
-		
-		const BYTE* shaderByteData;
-		SIZE_T shaderByteSize;
-
-		//FUNCTIONS
-	public:
-		D3DGeometryShader() = delete;
-		D3DGeometryShader(ID3D11Device& d3dDevice, const BYTE* shaderByteData, SIZE_T shaderByteSize);
-		D3DGeometryShader(const D3DGeometryShader& other) = delete;
-		D3DGeometryShader(D3DGeometryShader&& other) noexcept;
-		D3DGeometryShader& operator=(const D3DGeometryShader& other) = delete;
-		D3DGeometryShader& operator=(D3DGeometryShader&& other) noexcept;
-		~D3DGeometryShader();
-
-		virtual const BYTE* getBytePointer() const override;
-		virtual SIZE_T getByteSize() const override;
 	};
 }
