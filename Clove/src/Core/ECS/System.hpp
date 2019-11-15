f23f6e7d6a5689de8e3e18a3f87c4fb83b49ac21
Alex <wolfplayeral@yahoo.co.uk>
2019-11-15 07:54:05 +0000

    Fixed the system's destructor

--- Clove/src/Core/ECS/System.hpp
+++ Clove/src/Core/ECS/System.hpp
@@ -1,17 +1,11 @@
 #pragma once
 
-//#include "Core/ECS/ECSTypes.hpp"
-//#include "Core/Utils/DeltaTime.hpp"
-
 namespace clv::ecs{
 	class System{
 		//FUNCTIONS
 	public:
-		virtual ~SystemBase() = default;
+		virtual ~System() = default;
 
 		virtual void update(utl::DeltaTime deltaTime) = 0;
 	};
-
 }
-
-//#include "System.inl"
