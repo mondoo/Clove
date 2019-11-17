aa88ec12c8fd14701c4cff6785936dd93ebc461c
Alex <wolfplayeral@yahoo.co.uk>
2019-11-17 17:29:03 +0000

    Removed Component source file

--- Clove/src/Core/ECS/Component.hpp
+++ Clove/src/Core/ECS/Component.hpp
@@ -3,15 +3,19 @@
 #include "Core/ECS/ECSTypes.hpp"
 #include "Core/Utils/HashString.hpp"
 
+//TODO: .inl
+
 namespace clv::ecs{
-	class IComponent{
+	class ComponentInterface{
 		//FUNCTIONS
 	public:
 		virtual ComponentID getID() const = 0;
+
+		virtual ~ComponentInterface() = default;
 	};
 
 	template<typename DerivedClassType>
-	class Component : public IComponent{
+	class Component : public ComponentInterface{
 		//VARIABLES
 	public:
 		static constexpr ComponentID ID = HASH_CLASS(DerivedClassType);
@@ -20,17 +24,16 @@ namespace clv::ecs{
 
 		//FUNCTIONS
 	public:
-		Component();
+		Component() = default;
 
 		Component(const Component& other) = delete;
-		Component(Component&& other) noexcept;
+		Component(Component&& other) noexcept = default;
 
 		Component& operator=(const Component& other) = delete;
-		Component& operator=(Component&& other) noexcept;
+		Component& operator=(Component&& other) noexcept = default;
 		
-		virtual ~Component();
+		virtual ~Component() = default;
 
-		//TODO: .inl
 		virtual ComponentID getID() const{ return ID; }
 	};
 }
