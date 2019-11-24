18ce95c421b56614cbbdc21a7a3587fc9ca6a600
Alex <wolfplayeral@yahoo.co.uk>
2019-11-24 16:03:22 +0000

    Added a rotating cube to the scene

--- Bulb/Bulb.Engine/Wrapper.cpp
+++ Bulb/Bulb.Engine/Wrapper.cpp
@@ -8,8 +8,10 @@
 #include <functional>
 #include <sstream>
 #include <optional>
+#include <array>
 
 //Includes required when not using pch
+#include "Core/Core.hpp"
 #include "Core/IntTypes.hpp"
 #include "Core/Utils/Delegate.hpp"
 #include "Core/Maths/MathsTypes.hpp"
@@ -20,6 +22,20 @@
 #include "Core/Graphics/GraphicsTypes.hpp"
 #include "Core/Graphics/Surface.hpp"
 #include "Core/Graphics/RenderCommand.hpp"
+
+//Defining this here to fix errors - need a way to get access to logging (or sort out headers)
+#define CLV_LOG_ERROR(...)
+
+//Other includes
+#include "Core/Layer.hpp"
+#include "Core/ECS/Manager.hpp"
+#include "Core/ECS/Entity.hpp"
+#include "Core/ECS/3D/Components/TransformComponent.hpp"
+#include "Core/ECS/3D/Components/MeshComponent.hpp"
+#include "Core/ECS/3D/Components/CameraComponent.hpp"
+#include "Core/Graphics/Renderables/Mesh.hpp"
+#include "Core/Graphics/Material.hpp"
+#include "Core/Graphics/MaterialInstance.hpp"
 
 using namespace System::Windows;
 //using namespace System::Runtime::RuntimeInteropServices;
@@ -82,12 +98,53 @@ public:
 	}
 };
 
+//Temp layer to draw something
+class TestLayer : public clv::Layer{
+	//VARIABLES
+private:
+	clv::ecs::Entity cubeEnt;
+	clv::ecs::Entity camEnt;
+
+	//FUNCTIONS
+public:
+	virtual void onAttach() override{
+		auto cubeMaterial = std::make_shared<clv::gfx::Material>();
+		cubeMaterial->setData(clv::gfx::BBP_Colour, clv::mth::vec4f{ 0.4f, 0.4f, 0.4f, 1.0f }, clv::gfx::ShaderType::Pixel);
+		cubeMaterial->setData(clv::gfx::BBP_MaterialData, clv::gfx::MaterialData{ 32.0f }, clv::gfx::ShaderType::Pixel);
+
+		auto inst = cubeMaterial->createInstance();
+		auto mesh = std::make_shared<clv::gfx::Mesh>("res/Objects/cube.obj", inst); //NOTE: Manually copied to the correct folder for now
+
+		cubeEnt = clv::plt::Application::get().getManager().createEntity();
+		cubeEnt.addComponent<clv::ecs::_3D::TransformComponent>()->setPosition({ 10.0f, 0.0f, 0.0f });
+		cubeEnt.addComponent<clv::ecs::_3D::MeshComponent>()->setMesh(mesh);
+
+
+		camEnt = clv::plt::Application::get().getManager().createEntity();
+		camEnt.addComponent<clv::ecs::_3D::TransformComponent>();
+		camEnt.addComponent<clv::ecs::_3D::CameraComponent>(clv::gfx::Viewport{ 0, 0, 800, 400 }); //Any old viewport for now
+	}
+
+	virtual void onDetach() override{
+	
+	}
+
+	virtual void onUpdate(clv::utl::DeltaTime deltaTime) override{
+		static float rotDelta = 0.0f;
+		const float rotSpeed = 1.5f;
+
+		cubeEnt.getComponent<clv::ecs::_3D::TransformComponent>()->setRotation(clv::mth::asQuaternion(rotDelta, clv::mth::vec3f{ 0.0f, 1.0f, 0.0f }));
+
+		rotDelta += rotSpeed * deltaTime;
+	}
+};
+
 void Bulb::Engine::Wrapper::OpenClove(IntPtr hWnd){
 	app = new EditorApplication(hWnd);
 	
 	app->start();
 
-	//TODO: Push some layers on
+	app->pushLayer(std::make_shared<TestLayer>());
 
 	while(app->getState() == clv::plt::ApplicationState::running){
 		app->update();
