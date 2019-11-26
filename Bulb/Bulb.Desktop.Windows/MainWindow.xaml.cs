fbd6c0c0fd7ea7f7195c6a8823f70ab6ff688f80
Alex <wolfplayeral@yahoo.co.uk>
2019-11-26 19:07:05 +0000

    Found a way to render within a separate window with WPF

--- Bulb/Bulb.Desktop.Windows/MainWindow.xaml.cs
+++ Bulb/Bulb.Desktop.Windows/MainWindow.xaml.cs
@@ -28,12 +28,39 @@ namespace Bulb.Desktop.Windows {
         [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+        static extern IntPtr CreateWindowEx(
+            int dwExStyle,
+            string lpszClassName,
+            string lpszWindowName,
+            int style,
+            int x, int y,
+            int width, int height,
+            IntPtr hwndParent,
+            IntPtr hMenu,
+            IntPtr hInst,
+            [MarshalAs(UnmanagedType.AsAny)] object pvParam
+        );
+
         private void Button_Click(object sender, RoutedEventArgs e) {
             IntPtr hWnd = FindWindow(null, this.Title);
+
+            IntPtr RenderWindow = CreateWindowEx(0,
+               "static",
+               "",
+               0x40000000 | 0x10000000, //WS_CHILD | WS_VISIBLE
+               (int)RenderArea.Margin.Left, (int)RenderArea.Margin.Top,
+               (int)RenderArea.ActualWidth, (int)RenderArea.ActualHeight,
+               hWnd,
+               IntPtr.Zero,
+               IntPtr.Zero,
+               0);
+
+
             //This is an extremely jank solution to check for the problem
             //Clove will either need to run on another thread 
             //or (more appropriately) not have the while loop in run
-            Task testTask = new Task(async () => await (runClove(token, hWnd)));
+            Task testTask = new Task(async () => await (RunClove(token, RenderWindow, (int)RenderArea.ActualWidth, (int)RenderArea.ActualHeight)));
             testTask.Start();
         }
 
@@ -41,8 +68,8 @@ namespace Bulb.Desktop.Windows {
             wp.StopClove();
         }
 
-        private async Task runClove(CancellationToken token, IntPtr hWnd) {
-            wp.OpenClove(hWnd);
+        private async Task RunClove(CancellationToken token, IntPtr hWnd, int width, int height) {
+            wp.OpenClove(hWnd, width, height);
         }
     }
 }
