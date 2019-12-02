577e1d67f3108074dad729f3b09cf9c2754bd94b
Alex <wolfplayeral@yahoo.co.uk>
2019-12-02 07:52:41 +0000

    MainWindow will now open and close the engine when the window opens and closes

--- Bulb/Bulb.Desktop.Windows/MainWindow.xaml.cs
+++ Bulb/Bulb.Desktop.Windows/MainWindow.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System;
 using System.Threading.Tasks;
 using Bulb.Engine;
+using System.ComponentModel;
 
 namespace Bulb.Desktop.Windows {
     /// <summary>
@@ -11,65 +12,64 @@ namespace Bulb.Desktop.Windows {
     /// </summary>
     public partial class MainWindow : Window
     {
-        private CancellationTokenSource cancelTokenSource;
-        private CancellationToken token;
+		#region VARIABLES
 
-        private readonly Wrapper wp;
+		private readonly Wrapper wp;
+		private Thread engineThread;
 
-        public MainWindow() {
-            InitializeComponent();
+		#endregion
 
-            cancelTokenSource = new CancellationTokenSource();
-            token = cancelTokenSource.Token;
+		#region FUNCTIONS
 
-            wp = new Wrapper();
-        }
+		[DllImport("user32.dll", SetLastError = true)]
+		static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+		[DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+		static extern IntPtr CreateWindowEx(
+			int dwExStyle,
+			string lpszClassName,
+			string lpszWindowName,
+			int style,
+			int x, int y,
+			int width, int height,
+			IntPtr hwndParent,
+			IntPtr hMenu,
+			IntPtr hInst,
+			[MarshalAs(UnmanagedType.AsAny)] object pvParam
+		);
 
-        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
-        static extern IntPtr CreateWindowEx(
-            int dwExStyle,
-            string lpszClassName,
-            string lpszWindowName,
-            int style,
-            int x, int y,
-            int width, int height,
-            IntPtr hwndParent,
-            IntPtr hMenu,
-            IntPtr hInst,
-            [MarshalAs(UnmanagedType.AsAny)] object pvParam
-        );
+		public MainWindow() {
+			InitializeComponent();
 
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            IntPtr hWnd = FindWindow(null, this.Title);
+			wp = new Wrapper();
 
-            IntPtr RenderWindow = CreateWindowEx(0,
-               "static",
-               "",
-               0x40000000 | 0x10000000, //WS_CHILD | WS_VISIBLE
-               (int)RenderArea.Margin.Left, (int)RenderArea.Margin.Top,
-               (int)RenderArea.ActualWidth, (int)RenderArea.ActualHeight,
-               hWnd,
-               IntPtr.Zero,
-               IntPtr.Zero,
-               0);
+			Loaded += (object sender, RoutedEventArgs e) => StartEngine();
+			Closing += (object sender, CancelEventArgs e) => StopEngine();
+		}
 
+        #endregion
 
-            //This is an extremely jank solution to check for the problem
-            //Clove will either need to run on another thread 
-            //or (more appropriately) not have the while loop in run
-            Task testTask = new Task(async () => await (RunClove(token, RenderWindow, (int)RenderArea.ActualWidth, (int)RenderArea.ActualHeight)));
-            testTask.Start();
-        }
+		private void StartEngine() {
+			IntPtr hWnd = FindWindow(null, this.Title);
 
-        private void Button2_Click(object sender, RoutedEventArgs e) {
-            wp.StopClove();
-        }
+			IntPtr RenderWindow = CreateWindowEx(0,
+			   "static",
+			   "",
+			   0x40000000 | 0x10000000, //WS_CHILD | WS_VISIBLE
+			   (int)RenderArea.Margin.Left, (int)RenderArea.Margin.Top,
+			   (int)RenderArea.ActualWidth, (int)RenderArea.ActualHeight,
+			   hWnd,
+			   IntPtr.Zero,
+			   IntPtr.Zero,
+			   0);
 
-        private async Task RunClove(CancellationToken token, IntPtr hWnd, int width, int height) {
-            wp.OpenClove(hWnd, width, height);
-        }
+			engineThread = new Thread(() => wp.OpenClove(RenderWindow, (int)RenderArea.ActualWidth, (int)RenderArea.ActualHeight));
+			engineThread.Start();
+		}
+
+		private void StopEngine() {
+			wp.StopClove();
+			engineThread.Join();
+		}
     }
 }
