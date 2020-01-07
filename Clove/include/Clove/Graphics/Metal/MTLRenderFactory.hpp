32792c2af8223b9bcd0a851c7c9c6d757bc3d02e
Alex <wolfplayeral@yahoo.co.uk>
2020-01-07 10:05:19 +0000

    Updated the code formatting

--- Clove/include/Clove/Graphics/Metal/MTLRenderFactory.hpp
+++ Clove/include/Clove/Graphics/Metal/MTLRenderFactory.hpp
@@ -22,11 +22,16 @@ namespace clv::gfx::mtl{
 		virtual ~MTLRenderFactory();
 
 		virtual std::shared_ptr<Buffer> createBuffer(const BufferDescriptor& descriptor, const void* data) override;
+
 		virtual std::shared_ptr<Texture> createTexture(const TextureDescriptor& descriptor, const std::string& pathToTexture) override;
 		virtual std::shared_ptr<Texture> createTexture(const TextureDescriptor& descriptor, const void* data, int32 BPP) override;
+
 		virtual std::shared_ptr<PipelineObject> createPipelineObject(const std::shared_ptr<Shader>& shader) override;
+
 		virtual std::shared_ptr<RenderTarget> createRenderTarget(Texture* colourTexture, Texture* depthStencilTexture) override;
+
 		virtual std::shared_ptr<Shader> createShader(const ShaderDescriptor& descriptor) override;
+
 		virtual std::shared_ptr<Surface> createSurface(void* windowData) override;
 	};
 }
