bf27a157b50aec399a5da8604088efa0693b43c6
Alex <wolfplayeral@yahoo.co.uk>
2020-02-01 09:22:18 +0000

    Added the API to the window descriptor

--- Clove/include/Clove/Platform/Core/PlatformTypes.hpp
+++ Clove/include/Clove/Platform/Core/PlatformTypes.hpp
@@ -1,15 +1,12 @@
 #pragma once
 
+#include "Clove/Graphics/Core/GraphicsTypes.hpp"
+
 namespace clv::plt{
 	struct WindowDescriptor{
 		std::string title;
 		int32 width;
 		int32 height;
-
-		WindowDescriptor(const std::string& inTitle = "Clove Engine", int32 inWidth = 1280u, int32 inHeight = 720u)
-			: title(inTitle)
-			, width(inWidth)
-			, height(inHeight){
-		}
+		gfx::API api;
 	};
 }
