fdc44392ab74a60391722b577f0b51b91c025eb9
Alex <wolfplayeral@yahoo.co.uk>
2020-02-12 06:46:25 +0000

    Allowed D3D buffers to be initialised to nullptr

--- Clove/src/Graphics/Direct3D/Resources/D3DBuffer.cpp
+++ Clove/src/Graphics/Direct3D/Resources/D3DBuffer.cpp
@@ -18,13 +18,18 @@ namespace clv::gfx::d3d{
 		bufferDesc.MiscFlags			= 0u;
 		bufferDesc.StructureByteStride	= static_cast<UINT>(descriptor.elementSize);
 
-		D3D11_SUBRESOURCE_DATA initData{};
-		initData.pSysMem			= data;
-		initData.SysMemPitch		= 0u;
-		initData.SysMemSlicePitch	= 0u;
+		if (data){
+			D3D11_SUBRESOURCE_DATA initData{};
+			initData.pSysMem			= data;
+			initData.SysMemPitch		= 0u;
+			initData.SysMemSlicePitch	= 0u;
 
-		DX11_INFO_PROVIDER;
-		DX11_THROW_INFO(d3dDevice.CreateBuffer(&bufferDesc, &initData, &d3dBuffer));
+			DX11_INFO_PROVIDER;
+			DX11_THROW_INFO(d3dDevice.CreateBuffer(&bufferDesc, &initData, &d3dBuffer));
+		} else{
+			DX11_INFO_PROVIDER;
+			DX11_THROW_INFO(d3dDevice.CreateBuffer(&bufferDesc, nullptr, &d3dBuffer));
+		}
 	}
 
 	D3DBuffer::D3DBuffer(D3DBuffer&& other) noexcept = default;
