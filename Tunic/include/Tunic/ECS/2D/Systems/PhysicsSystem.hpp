38debf9902ca5c67b23a35361d2fbdb0795754ad
Alex <wolfplayeral@yahoo.co.uk>
2020-02-13 07:31:30 +0000

    Formatting

--- Tunic/include/Tunic/ECS/2D/Systems/PhysicsSystem.hpp
+++ Tunic/include/Tunic/ECS/2D/Systems/PhysicsSystem.hpp
@@ -22,10 +22,13 @@ namespace tnc::ecs::_2D{
 		//FUNCTIONS
 	public:
 		PhysicsSystem();
+
 		PhysicsSystem(const PhysicsSystem& other) = delete;
 		PhysicsSystem(PhysicsSystem&& other) noexcept;
+
 		PhysicsSystem& operator=(const PhysicsSystem& other) = delete;
 		PhysicsSystem& operator=(PhysicsSystem&& other) noexcept;
+
 		virtual ~PhysicsSystem();
 
 		virtual void update(clv::utl::DeltaTime deltaTime) override;
