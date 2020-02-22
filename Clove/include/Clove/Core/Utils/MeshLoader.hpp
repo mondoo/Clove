ce1be7a4865fcc6f5cf20ebe023564113be36bde
Alex <wolfplayeral@yahoo.co.uk>
2020-02-22 11:34:17 +0000

    Concatenated an old namespace

--- Clove/include/Clove/Core/Utils/MeshLoader.hpp
+++ Clove/include/Clove/Core/Utils/MeshLoader.hpp
@@ -2,38 +2,36 @@
 
 #include "Clove/Graphics/Core/GraphicsTypes.hpp"
 
-namespace clv{
-	namespace loader{
-		struct MeshInfo{
-			std::vector<float> vertexData;
-			std::vector<uint32_t> indices;
+namespace clv::loader{
+	struct MeshInfo{
+		std::vector<float> vertexData;
+		std::vector<uint32_t> indices;
 
-			std::vector<mth::vec3f> verticies;
-			std::vector<mth::vec2f> texCoords;
-			std::vector<mth::vec3f> normals;
+		std::vector<mth::vec3f> verticies;
+		std::vector<mth::vec2f> texCoords;
+		std::vector<mth::vec3f> normals;
 
-			template<gfx::VertexElementType layout>
-			auto& getData(){
-				if constexpr(layout == gfx::VertexElementType::position3D){
-					return verticies;
-				} else if constexpr(layout == gfx::VertexElementType::texture2D){
-					return texCoords;
-				} else if constexpr(layout == gfx::VertexElementType::normal){
-					return normals;
-				}
+		template<gfx::VertexElementType layout>
+		auto& getData(){
+			if constexpr (layout == gfx::VertexElementType::position3D){
+				return verticies;
+			} else if constexpr (layout == gfx::VertexElementType::texture2D){
+				return texCoords;
+			} else if constexpr (layout == gfx::VertexElementType::normal){
+				return normals;
 			}
-		};
+		}
+	};
 
-		class MeshLoader{
-			//VARIABLES
-		private:
-			static MeshInfo emptyMeshInfo;
-			static std::unordered_map<std::string, MeshInfo> loadedMeshes;
+	class MeshLoader{
+		//VARIABLES
+	private:
+		static MeshInfo emptyMeshInfo;
+		static std::unordered_map<std::string, MeshInfo> loadedMeshes;
 
-			//FUNCTIONS
-		public:
-			static const MeshInfo& loadOBJ(const std::string& filepath);
+		//FUNCTIONS
+	public:
+		static const MeshInfo& loadOBJ(const std::string& filepath);
 
-		};
-	}
+	};
 }
