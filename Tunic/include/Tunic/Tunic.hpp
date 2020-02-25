87227e4b262afb33ea347233874415e725e02da1
Alex <wolfplayeral@yahoo.co.uk>
2020-02-25 06:46:11 +0000

    Added the ECS headers to the main Tunic header

--- Tunic/include/Tunic/Tunic.hpp
+++ Tunic/include/Tunic/Tunic.hpp
@@ -1,5 +1,21 @@
 #pragma once
 
+#include "Tunic/ECS/Core/World.hpp"
+#include "Tunic/ECS/Core/Entity.hpp"
+#include "Tunic/ECS/2D/Systems/RenderSystem.hpp"
+#include "Tunic/ECS/3D/Components/CameraComponent.hpp"
+#include "Tunic/ECS/3D/Components/LightComponent.hpp"
+#include "Tunic/ECS/3D/Components/MeshComponent.hpp"
+#include "Tunic/ECS/3D/Components/RigidBodyComponent.hpp"
+#include "Tunic/ECS/3D/Components/TransformComponent.hpp"
+#include "Tunic/ECS/3D/Systems/RenderSystem.hpp"
+#include "Tunic/ECS/3D/Systems/PhysicsSystem.hpp"
+#include "Tunic/ECS/Audio/Components/AudioComponent.hpp"
+#include "Tunic/ECS/Audio/Systems/AudioSystem.hpp"
+#include "Tunic/ECS/UI/Components/TextComponent.hpp"
+#include "Tunic/ECS/UI/Components/WidgetComponent.hpp"
+#include "Tunic/ECS/UI/Components/TransformComponent.hpp"
+
 #include "Tunic/Rendering/Renderables/Mesh.hpp"
 #include "Tunic/Rendering/Renderables/Sprite.hpp"
 #include "Tunic/Rendering/Material.hpp"
