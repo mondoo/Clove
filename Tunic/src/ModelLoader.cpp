43717a2730e995cbb53200b9a6ef6d7aed87953d
Alex <wolfplayeral@yahoo.co.uk>
2020-02-26 07:58:43 +0000

    Added indices loading

--- Tunic/src/ModelLoader.cpp
+++ Tunic/src/ModelLoader.cpp
@@ -2,6 +2,8 @@
 
 #include "Tunic/Rendering/Renderables/Mesh.hpp"
 
+#include <Clove/Graphics/Core/VertexLayout.hpp>
+
 #include <assimp/Importer.hpp>
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
@@ -15,24 +17,35 @@ namespace tnc::ModelLoader {
 			material->GetTexture(type, i, &str);
 			paths.emplace_back(str.C_Str());
 		}
+
 		return paths;
 	}
 
 	static std::shared_ptr<rnd::Mesh> processMesh(aiMesh* mesh, const aiScene* scene) {
+		clv::gfx::VertexBufferData vertexBufferData{
+			{} //TODO: Proper layout
+		};
+		std::vector<uint32_t> indices;
+		std::shared_ptr<rnd::Material> material;
+
 		for(size_t i = 0; i < mesh->mNumVertices; ++i) {
 			//TODO: Vertices
 		}
+
 		for(size_t i = 0; i < mesh->mNumFaces; ++i) {
 			aiFace face = mesh->mFaces[i];
 			for(size_t j = 0; j < face.mNumIndices; ++i) {
-				//TODO: Indices
+				indices.emplace_back(face.mIndices[j]);
 			}
 		}
+
 		if(mesh->mMaterialIndex >= 0) {
 			std::vector<std::string> diffuseTextures = loadMaterialTextures(scene->mMaterials[mesh->mMaterialIndex], aiTextureType_DIFFUSE);
 			std::vector<std::string> specularTextures = loadMaterialTextures(scene->mMaterials[mesh->mMaterialIndex], aiTextureType_SPECULAR);
 			//TODO: Process textures
 		}
+
+		return std::make_shared<rnd::Mesh>(vertexBufferData, indices, material->createInstance());
 	}
 	
 	//TODO: Needs to take in a Model to add to
@@ -41,6 +54,7 @@ namespace tnc::ModelLoader {
 			aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
 			//TODO: Push mesh back
 		}
+
 		for(size_t i = 0; i < node->mNumChildren; ++i) {
 			processNode(node->mChildren[i], scene);
 		}
@@ -53,7 +67,9 @@ namespace tnc::ModelLoader {
 			CLV_LOG_ERROR("Assimp Error: {0}", importer.GetErrorString());
 			return {};
 		}
+
 		processNode(scene->mRootNode, scene);
+
 		//TEMP: returning nothing
 		return {};
 	}
