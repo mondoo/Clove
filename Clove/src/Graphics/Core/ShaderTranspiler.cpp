b2daed31016fd818ddcdb721f47ee1242c31f758
Alex <wolfplayeral@yahoo.co.uk>
2020-03-05 08:06:39 +0000

    Separated out functionality in the ShaderTranspiler

--- Clove/src/Graphics/Core/ShaderTranspiler.cpp
+++ Clove/src/Graphics/Core/ShaderTranspiler.cpp
@@ -27,6 +27,98 @@ namespace clv::gfx::ShaderTranspiler{
 		}
 	}
 
+	static std::string spirvToGlsl(ShaderStage stage, const std::vector<uint32_t>& spirvSource) {
+		spirv_cross::CompilerGLSL glsl(spirvSource);
+		spirv_cross::CompilerGLSL::Options scoptions;
+
+		scoptions.version = 460;
+		scoptions.es = false;
+		scoptions.enable_420pack_extension = false;
+		glsl.set_common_options(scoptions);
+
+		glsl.build_combined_image_samplers();
+
+		spirv_cross::ShaderResources resources = glsl.get_shader_resources();
+
+		//Make sure the combined image/samplers keep the binding id
+		std::map<spirv_cross::ID, uint32_t> samplerBindings;
+		for(auto& resource : resources.separate_samplers) {
+			samplerBindings[resource.id] = glsl.get_decoration(resource.id, spv::DecorationBinding);
+		}
+		for(auto& remap : glsl.get_combined_image_samplers()) {
+			glsl.set_decoration(remap.combined_id, spv::DecorationBinding, samplerBindings[remap.sampler_id]);
+		}
+
+		//Remap names to semantics
+		if(stage == ShaderStage::Vertex) {
+			for(auto& resource : resources.stage_inputs) {
+				const uint32_t location = glsl.get_decoration(resource.id, spv::DecorationLocation);
+				std::string str = glsl.get_decoration_string(resource.id, spv::DecorationUserSemantic);
+
+				glsl.set_name(resource.id, str);
+			}
+		}
+
+		if(stage == ShaderStage::Geometry) {
+			const uint32_t invocations = glsl.get_execution_mode_argument(spv::ExecutionMode::ExecutionModeInvocations);
+			if(invocations == 196624) { //Unset invocations default to this, unset it
+				glsl.unset_execution_mode(spv::ExecutionMode::ExecutionModeInvocations);
+			}
+		}
+
+		return glsl.compile();
+	}
+
+	static std::string spirvToMSL(ShaderStage stage, const std::vector<uint32_t>& spirvSource) {
+		spirv_cross::CompilerMSL msl(spirvSource);
+		spirv_cross::CompilerMSL::Options scoptions;
+
+		scoptions.platform = spirv_cross::CompilerMSL::Options::Platform::macOS;
+		msl.set_msl_options(scoptions);
+
+		spirv_cross::ShaderResources resources = msl.get_shader_resources();
+
+		const auto remapMSLBindings = [&msl](const uint32_t binding, const spirv_cross::ID resourceID) {
+			spirv_cross::MSLResourceBinding resourceBinding;
+			resourceBinding.stage = msl.get_execution_model();
+			resourceBinding.desc_set = msl.get_decoration(resourceID, spv::DecorationDescriptorSet);
+			resourceBinding.binding = binding;
+			//Can cause issue if not all are set
+			resourceBinding.msl_buffer = binding;
+			resourceBinding.msl_texture = binding;
+			resourceBinding.msl_sampler = binding;
+			msl.add_msl_resource_binding(resourceBinding);
+		};
+
+		//Set up correct buffer bindings
+		for(auto& resource : resources.uniform_buffers) {
+			const uint32_t binding = msl.get_decoration(resource.id, spv::DecorationBinding);
+			remapMSLBindings(binding, resource.id);
+		}
+
+		//Set up correct texture bindings
+		for(auto& resource : resources.separate_images) {
+			const uint32_t binding = msl.get_decoration(resource.id, spv::DecorationBinding);
+			remapMSLBindings(binding, resource.id);
+		}
+		for(auto& resource : resources.separate_samplers) {
+			const uint32_t binding = msl.get_decoration(resource.id, spv::DecorationBinding);
+			remapMSLBindings(binding, resource.id);
+		}
+
+		//Remap names to semantics
+		if(stage == ShaderStage::Vertex) {
+			for(auto& resource : resources.stage_inputs) {
+				const uint32_t location = msl.get_decoration(resource.id, spv::DecorationLocation);
+				std::string str = msl.get_decoration_string(resource.id, spv::DecorationUserSemantic);
+
+				msl.set_name(resource.id, str);
+			}
+		}
+
+		return msl.compile();
+	}
+
 	std::string transpileFromFile(std::string_view filePath, ShaderStage stage, ShaderType outputType){
 		std::ifstream stream(filePath.data());
 
@@ -110,93 +202,9 @@ namespace clv::gfx::ShaderTranspiler{
 		glslang::FinalizeProcess();
 
 		if(outputType == ShaderType::GLSL){
-			spirv_cross::CompilerGLSL glsl(spirvSource);
-			spirv_cross::CompilerGLSL::Options scoptions;
-
-			scoptions.version = 460;
-			scoptions.es = false;
-			scoptions.enable_420pack_extension = false;
-			glsl.set_common_options(scoptions);
-
-			glsl.build_combined_image_samplers();
-
-			spirv_cross::ShaderResources resources = glsl.get_shader_resources();
-
-			//Make sure the combined image/samplers keep the binding id
-			std::map<spirv_cross::ID, uint32_t> samplerBindings;
-			for(auto& resource : resources.separate_samplers) {
-				samplerBindings[resource.id] = glsl.get_decoration(resource.id, spv::DecorationBinding);
-			}
-			for(auto& remap : glsl.get_combined_image_samplers()) {
-				glsl.set_decoration(remap.combined_id, spv::DecorationBinding, samplerBindings[remap.sampler_id]);
-			}
-
-			//Remap names to semantics
-			if(stage == ShaderStage::Vertex) {
-				for(auto& resource : resources.stage_inputs) {
-					const uint32_t location = glsl.get_decoration(resource.id, spv::DecorationLocation);
-					std::string str = glsl.get_decoration_string(resource.id, spv::DecorationUserSemantic);
-
-					glsl.set_name(resource.id, str);
-				}
-			}
-
-			if(stage == ShaderStage::Geometry) {
-				const uint32_t invocations = glsl.get_execution_mode_argument(spv::ExecutionMode::ExecutionModeInvocations);
-				if(invocations == 196624) { //Unset invocations default to this, unset it
-					glsl.unset_execution_mode(spv::ExecutionMode::ExecutionModeInvocations);
-				}
-			}
-
-			return glsl.compile();
+			return spirvToGlsl(stage, spirvSource);
 		} else if(outputType == ShaderType::MSL){
-			spirv_cross::CompilerMSL msl(spirvSource);
-			spirv_cross::CompilerMSL::Options scoptions;
-
-			scoptions.platform = spirv_cross::CompilerMSL::Options::Platform::macOS;
-			msl.set_msl_options(scoptions);
-
-			spirv_cross::ShaderResources resources = msl.get_shader_resources();
-
-			const auto remapMSLBindings = [&msl](const uint32_t binding, const spirv_cross::ID resourceID) {
-				spirv_cross::MSLResourceBinding resourceBinding;
-				resourceBinding.stage = msl.get_execution_model();
-				resourceBinding.desc_set = msl.get_decoration(resourceID, spv::DecorationDescriptorSet);
-				resourceBinding.binding = binding;
-				//Can cause issue if not all are set
-				resourceBinding.msl_buffer = binding;
-				resourceBinding.msl_texture = binding;
-				resourceBinding.msl_sampler = binding;
-				msl.add_msl_resource_binding(resourceBinding);
-			};
-
-			//Set up correct buffer bindings
-			for(auto& resource : resources.uniform_buffers) {
-				const uint32_t binding = msl.get_decoration(resource.id, spv::DecorationBinding);
-				remapMSLBindings(binding, resource.id);
-			}
-
-			//Set up correct texture bindings
-			for(auto& resource : resources.separate_images) {
-				const uint32_t binding = msl.get_decoration(resource.id, spv::DecorationBinding);
-				remapMSLBindings(binding, resource.id);
-			}
-			for(auto& resource : resources.separate_samplers) {
-				const uint32_t binding = msl.get_decoration(resource.id, spv::DecorationBinding);
-				remapMSLBindings(binding, resource.id);
-			}
-
-			//Remap names to semantics
-			if(stage == ShaderStage::Vertex) {
-				for(auto& resource : resources.stage_inputs) {
-					const uint32_t location = msl.get_decoration(resource.id, spv::DecorationLocation);
-					std::string str = msl.get_decoration_string(resource.id, spv::DecorationUserSemantic);
-
-					msl.set_name(resource.id, str);
-				}
-			}
-
-			return msl.compile();
+			return spirvToMSL(stage, spirvSource);
 		}
 
 		return "";
