16d84fae85f681499af12314e592c5981783ef35
Alex <wolfplayeral@yahoo.co.uk>
2020-03-06 07:17:08 +0000

    Unified the API for setting a transform's components

--- Tunic/src/ECS/3D/Components/TransformComponent.cpp
+++ Tunic/src/ECS/3D/Components/TransformComponent.cpp
@@ -52,100 +52,73 @@ namespace tnc::ecs::_3D{
 		}
 	}
 
-	mth::vec3f TransformComponent::getPosition() const{
-		const mth::mat4f transformMatrix = getWorldTransformMatrix();
-
-		return { transformMatrix[3][0], transformMatrix[3][1], transformMatrix[3][2] };
-	}
-
-	const mth::vec3f& TransformComponent::getLocalPosition() const{
-		return localPosition;
-	}
-	
-	mth::quatf TransformComponent::getRotation() const{
-		mth::mat4f transformMatrix = getWorldTransformMatrix();
-		
-		transformMatrix[3][0] = 0.0f;
-		transformMatrix[3][1] = 0.0f;
-		transformMatrix[3][2] = 0.0f;
-
-		const mth::vec3f scaleX = { transformMatrix[0][0], transformMatrix[0][1], transformMatrix[0][2] };
-		const mth::vec3f scaleY = { transformMatrix[1][0], transformMatrix[1][1], transformMatrix[1][2] };
-		const mth::vec3f scaleZ = { transformMatrix[2][0], transformMatrix[2][1], transformMatrix[2][2] };
-
-		mth::vec3f scale = { mth::length(scaleX), mth::length(scaleY), mth::length(scaleZ) };
-
-		transformMatrix[0][0] /= scale.x;
-		transformMatrix[0][1] /= scale.x;
-		transformMatrix[0][2] /= scale.x;
-
-		transformMatrix[1][0] /= scale.y;
-		transformMatrix[1][1] /= scale.y;
-		transformMatrix[1][2] /= scale.y;
-
-		transformMatrix[2][0] /= scale.z;
-		transformMatrix[2][1] /= scale.z;
-		transformMatrix[2][2] /= scale.z;
-
-		return mth::matrixToQuaternion(transformMatrix);
-	}
-
-	const mth::quatf& TransformComponent::getLocalRotation() const{
-		return localRotation;
-	}
-	
-	mth::vec3f TransformComponent::getScale() const{
-		const mth::mat4f transformMatrix = getWorldTransformMatrix();
-
-		const mth::vec3f scaleX = { transformMatrix[0][0], transformMatrix[0][1], transformMatrix[0][2] };
-		const mth::vec3f scaleY = { transformMatrix[1][0], transformMatrix[1][1], transformMatrix[1][2] };
-		const mth::vec3f scaleZ = { transformMatrix[2][0], transformMatrix[2][1], transformMatrix[2][2] };
-
-		return { mth::length(scaleX), mth::length(scaleY), mth::length(scaleZ) };
-	}
-
-	const mth::vec3f& TransformComponent::getLocalScale() const{
-		return localScale;
-	}
-	
-	void TransformComponent::setPosition(const mth::vec3f& position){
-		if(parent != nullptr){
-			setLocalPosition(position - parent->getPosition());
-		} else{
-			setLocalPosition(position);
+	clv::mth::vec3f TransformComponent::getPosition(TransformSpace space) const {
+		switch(space) {
+			case TransformSpace::Local:
+				return getLocalPosition();
+			case TransformSpace::World:
+				return getWorldPosition();
+			default:
+				CLV_ASSERT(false, "Default statement hit in {0}", CLV_FUNCTION_NAME_PRETTY);
+				return {};
 		}
 	}
 
-	void TransformComponent::setLocalPosition(const mth::vec3f& position){
-		localPosition = position;
-	}
-
-	void TransformComponent::setRotation(const mth::quatf& rotation){
-		if(parent != nullptr){
-			const mth::mat4f rotMat			= mth::quaternionToMatrix4(rotation);
-			const mth::mat4f parentRotMat	= mth::quaternionToMatrix4(parent->getRotation());
-			const mth::mat4f adjustedRot	= parentRotMat / rotMat;
-
-			setLocalRotation(mth::matrixToQuaternion(adjustedRot));
-		} else{
-			setLocalRotation(rotation);
+	clv::mth::quatf TransformComponent::getRotation(TransformSpace space) const {
+		switch(space) {
+			case TransformSpace::Local:
+				return getLocalRotation();
+			case TransformSpace::World:
+				return getWorldRotation();
+			default:
+				CLV_ASSERT(false, "Default statement hit in {0}", CLV_FUNCTION_NAME_PRETTY);
+				return {};
 		}
 	}
 
-	void TransformComponent::setLocalRotation(const mth::quatf& rotation){
-		localRotation = rotation;
-	}
-
-	void TransformComponent::setScale(const mth::vec3f& scale){
-		if(parent != nullptr){
-			setLocalScale(scale / parent->getScale());
-		} else{
-			setLocalScale(scale);
+	clv::mth::vec3f TransformComponent::getScale(TransformSpace space) const {
+		switch(space) {
+			case TransformSpace::Local:
+				return getLocalScale();
+			case TransformSpace::World:
+				return getWorldScale();
+			default:
+				CLV_ASSERT(false, "Default statement hit in {0}", CLV_FUNCTION_NAME_PRETTY);
+				return {};
 		}
 	}
 
-	void TransformComponent::setLocalScale(const mth::vec3f& scale){
-		localScale = scale;
+	void TransformComponent::setPosition(const clv::mth::vec3f& position, TransformSpace space) {
+		switch(space) {
+			case TransformSpace::Local:
+				setLocalPosition(position);
+				break;
+			case TransformSpace::World:
+				setWorldPosition(position);
+				break;
+		}
+	}
+
+	void TransformComponent::setRotation(const clv::mth::quatf& rotation, TransformSpace space) {
+		switch(space) {
+			case TransformSpace::Local:
+				setLocalRotation(rotation);
+				break;
+			case TransformSpace::World:
+				setWorldRotation(rotation);
+				break;
+		}
+	}
+
+	void TransformComponent::setScale(const clv::mth::vec3f& scale, TransformSpace space) {
+		switch(space) {
+			case TransformSpace::Local:
+				setLocalScale(scale);
+				break;
+			case TransformSpace::World:
+				setWorldScale(scale);
+				break;
+		}
 	}
 
 	TransformComponent* TransformComponent::getParent() const{
@@ -176,5 +149,101 @@ namespace tnc::ecs::_3D{
 		const mth::mat4f scaleMatrix		= mth::scale(mth::mat4f(1.0f), localScale);
 
 		return translationMatrix * rotationMatrix * scaleMatrix;
+	}
+
+	mth::vec3f TransformComponent::getWorldPosition() const {
+		const mth::mat4f transformMatrix = getWorldTransformMatrix();
+
+		return { transformMatrix[3][0], transformMatrix[3][1], transformMatrix[3][2] };
+	}
+
+	const mth::vec3f& TransformComponent::getLocalPosition() const {
+		return localPosition;
+	}
+
+	mth::quatf TransformComponent::getWorldRotation() const {
+		mth::mat4f transformMatrix = getWorldTransformMatrix();
+
+		transformMatrix[3][0] = 0.0f;
+		transformMatrix[3][1] = 0.0f;
+		transformMatrix[3][2] = 0.0f;
+
+		const mth::vec3f scaleX = { transformMatrix[0][0], transformMatrix[0][1], transformMatrix[0][2] };
+		const mth::vec3f scaleY = { transformMatrix[1][0], transformMatrix[1][1], transformMatrix[1][2] };
+		const mth::vec3f scaleZ = { transformMatrix[2][0], transformMatrix[2][1], transformMatrix[2][2] };
+
+		mth::vec3f scale = { mth::length(scaleX), mth::length(scaleY), mth::length(scaleZ) };
+
+		transformMatrix[0][0] /= scale.x;
+		transformMatrix[0][1] /= scale.x;
+		transformMatrix[0][2] /= scale.x;
+
+		transformMatrix[1][0] /= scale.y;
+		transformMatrix[1][1] /= scale.y;
+		transformMatrix[1][2] /= scale.y;
+
+		transformMatrix[2][0] /= scale.z;
+		transformMatrix[2][1] /= scale.z;
+		transformMatrix[2][2] /= scale.z;
+
+		return mth::matrixToQuaternion(transformMatrix);
+	}
+
+	const mth::quatf& TransformComponent::getLocalRotation() const {
+		return localRotation;
+	}
+
+	mth::vec3f TransformComponent::getWorldScale() const {
+		const mth::mat4f transformMatrix = getWorldTransformMatrix();
+
+		const mth::vec3f scaleX = { transformMatrix[0][0], transformMatrix[0][1], transformMatrix[0][2] };
+		const mth::vec3f scaleY = { transformMatrix[1][0], transformMatrix[1][1], transformMatrix[1][2] };
+		const mth::vec3f scaleZ = { transformMatrix[2][0], transformMatrix[2][1], transformMatrix[2][2] };
+
+		return { mth::length(scaleX), mth::length(scaleY), mth::length(scaleZ) };
+	}
+
+	const mth::vec3f& TransformComponent::getLocalScale() const {
+		return localScale;
+	}
+
+	void TransformComponent::setWorldPosition(const mth::vec3f& position) {
+		if(parent != nullptr) {
+			setLocalPosition(position - parent->getPosition());
+		} else {
+			setLocalPosition(position);
+		}
+	}
+
+	void TransformComponent::setLocalPosition(const mth::vec3f& position) {
+		localPosition = position;
+	}
+
+	void TransformComponent::setWorldRotation(const mth::quatf& rotation) {
+		if(parent != nullptr) {
+			const mth::mat4f rotMat			= mth::quaternionToMatrix4(rotation);
+			const mth::mat4f parentRotMat	= mth::quaternionToMatrix4(parent->getRotation());
+			const mth::mat4f adjustedRot	= parentRotMat / rotMat;
+
+			setLocalRotation(mth::matrixToQuaternion(adjustedRot));
+		} else {
+			setLocalRotation(rotation);
+		}
+	}
+
+	void TransformComponent::setLocalRotation(const mth::quatf& rotation) {
+		localRotation = rotation;
+	}
+
+	void TransformComponent::setWorldScale(const mth::vec3f& scale) {
+		if(parent != nullptr) {
+			setLocalScale(scale / parent->getScale());
+		} else {
+			setLocalScale(scale);
+		}
+	}
+
+	void TransformComponent::setLocalScale(const mth::vec3f& scale) {
+		localScale = scale;
 	}
 }
