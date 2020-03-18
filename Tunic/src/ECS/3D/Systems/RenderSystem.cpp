e347ce3a1ea300557ded79ea53bed199fccf2f48
Alex <wolfplayeral@yahoo.co.uk>
2020-03-18 07:11:13 +0000

    Aligned the values for the point light shadow transforms

--- Tunic/src/ECS/3D/Systems/RenderSystem.cpp
+++ Tunic/src/ECS/3D/Systems/RenderSystem.cpp
@@ -94,12 +94,12 @@ namespace tnc::ecs::_3D {
 			const mth::vec3f& position = transform->getPosition();
 
 			light->lightData.data.position = position;
-			light->lightData.shadowTransforms[0] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{ 1.0, 0.0, 0.0 }, mth::vec3f{ 0.0, -1.0, 0.0 });
-			light->lightData.shadowTransforms[1] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{ -1.0, 0.0, 0.0 }, mth::vec3f{ 0.0, -1.0, 0.0 });
-			light->lightData.shadowTransforms[2] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{ 0.0, 1.0, 0.0 }, mth::vec3f{ 0.0, 0.0, 1.0 });
-			light->lightData.shadowTransforms[3] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{ 0.0, -1.0, 0.0 }, mth::vec3f{ 0.0, 0.0, -1.0 });
-			light->lightData.shadowTransforms[4] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{ 0.0, 0.0, 1.0 }, mth::vec3f{ 0.0, -1.0, 0.0 });
-			light->lightData.shadowTransforms[5] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{ 0.0, 0.0, -1.0 }, mth::vec3f{ 0.0, -1.0, 0.0 });
+			light->lightData.shadowTransforms[0] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{  1.0,  0.0,  0.0 }, mth::vec3f{  0.0, -1.0,  0.0 });
+			light->lightData.shadowTransforms[1] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{ -1.0,  0.0,  0.0 }, mth::vec3f{  0.0, -1.0,  0.0 });
+			light->lightData.shadowTransforms[2] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{  0.0,  1.0,  0.0 }, mth::vec3f{  0.0,  0.0,  1.0 });
+			light->lightData.shadowTransforms[3] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{  0.0, -1.0,  0.0 }, mth::vec3f{  0.0,  0.0, -1.0 });
+			light->lightData.shadowTransforms[4] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{  0.0,  0.0,  1.0 }, mth::vec3f{  0.0, -1.0,  0.0 });
+			light->lightData.shadowTransforms[5] = light->shadowProj * mth::lookAt(position, position + mth::vec3f{  0.0,  0.0, -1.0 }, mth::vec3f{  0.0, -1.0,  0.0 });
 
 			renderer->submitLight({ light->lightData.data, light->lightData.shadowTransforms });
 		}
