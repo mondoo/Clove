5cdb7d8fdb8c6d532c687356c3c76efad95a97cc
Alex <wolfplayeral@yahoo.co.uk>
2020-04-04 18:08:16 +0100

    Set up a near + far plane on the camera's ortho projection

--- Tunic/src/ECS/3D/Components/CameraComponent.cpp
+++ Tunic/src/ECS/3D/Components/CameraComponent.cpp
@@ -1,17 +1,18 @@
 #include "Tunic/ECS/3D/Components/CameraComponent.hpp"
 
-#include <Clove/Platform/Core/Window.hpp>
 #include <Clove/Graphics/Core/Surface.hpp>
+#include <Clove/Platform/Core/Window.hpp>
 
 using namespace clv;
 
-namespace tnc::ecs::_3D{
+namespace tnc::ecs::_3D {
 	CameraComponent::CameraComponent(std::shared_ptr<clv::gfx::RenderTarget> renderTarget, const gfx::Viewport& viewport, const ProjectionMode projection)
-		: renderTarget(std::move(renderTarget)), viewport(viewport){
+		: renderTarget(std::move(renderTarget)),
+			viewport(viewport) {
 		setProjectionMode(projection);
 	}
 
-	CameraComponent::CameraComponent(plt::Window& window, const ProjectionMode projection){
+	CameraComponent::CameraComponent(plt::Window& window, const ProjectionMode projection) {
 		viewport = { 0, 0, window.getSize().x, window.getSize().y };
 		window.onWindowResize.bind(&CameraComponent::updateViewportSize, this);
 		renderTarget = window.getSurface()->getRenderTarget();
@@ -28,47 +29,52 @@ namespace tnc::ecs::_3D{
 
 	CameraComponent::~CameraComponent() = default;
 
-	const clv::mth::mat4f& CameraComponent::getProjection() const{
+	const clv::mth::mat4f& CameraComponent::getProjection() const {
 		return currentProjection;
 	}
 
-	const clv::mth::mat4f& CameraComponent::getView() const{
+	const clv::mth::mat4f& CameraComponent::getView() const {
 		return currentView;
 	}
 
-	const mth::vec3f& CameraComponent::getFront() const{
+	const mth::vec3f& CameraComponent::getFront() const {
 		return cameraFront;
 	}
 
-	const mth::vec3f& CameraComponent::getUp() const{
+	const mth::vec3f& CameraComponent::getUp() const {
 		return cameraUp;
 	}
 
-	const mth::vec3f& CameraComponent::getRight() const{
+	const mth::vec3f& CameraComponent::getRight() const {
 		return cameraRight;
 	}
 
-	const clv::gfx::Viewport& CameraComponent::getViewport() const{
+	const clv::gfx::Viewport& CameraComponent::getViewport() const {
 		return viewport;
 	}
 
-	void CameraComponent::setProjectionMode(const ProjectionMode mode){
+	void CameraComponent::setProjectionMode(const ProjectionMode mode) {
 		const float width = static_cast<float>(viewport.width);
 		const float height = static_cast<float>(viewport.height);
+		const float aspect = width / height;
+		const float size = 15.0f;
+
+		const float nearPlane = 0.5f;
+		const float farPlane = 10000.0f;
 
 		currentProjectionMode = mode;
 
-		switch (currentProjectionMode){
-		case ProjectionMode::orthographic:
-			currentProjection = mth::createOrthographicMatrix(-(width / 2.0f), (width / 2.0f), -(height / 2.0f), (height / 2.0f));
-			break;
+		switch(currentProjectionMode) {
+			case ProjectionMode::orthographic:
+				currentProjection = mth::createOrthographicMatrix(-size * aspect, size * aspect, -size, size, nearPlane, farPlane);
+				break;
 
-		case ProjectionMode::perspective:
-			currentProjection = mth::createPerspectiveMatrix(45.0f, width / height, 0.5f, 10000.0f);
-			break;
+			case ProjectionMode::perspective:
+				currentProjection = mth::createPerspectiveMatrix(45.0f, aspect, nearPlane, farPlane);
+				break;
 
-		default:
-			break;
+			default:
+				break;
 		}
 	}
 
@@ -76,6 +82,7 @@ namespace tnc::ecs::_3D{
 		return currentProjectionMode;
 	}
 
+	void CameraComponent::updateViewportSize(const mth::vec2ui& viewportSize) {
 		viewport.width = viewportSize.x;
 		viewport.height = viewportSize.y;
 
