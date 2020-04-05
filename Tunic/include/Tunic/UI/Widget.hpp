9382ae732004adcedd55374199e3aa66f005cd36
Alex <wolfplayeral@yahoo.co.uk>
2020-04-05 20:39:40 +0100

    Added the basic values onto the base widget

--- Tunic/include/Tunic/UI/Widget.hpp
+++ Tunic/include/Tunic/UI/Widget.hpp
@@ -2,6 +2,14 @@
 
 namespace tnc::ui {
 	class Widget{
+		//VARIABLES
+	private:
+		clv::mth::vec2f position{ 0.0f, 0.0f };
+		clv::mth::vec2f alignment{ 0.0f, 0.0f };
+		clv::mth::vec2f anchor{ 0.0f, 0.0f };
+
+		//FUNCTIONS
+	public:
 
 	};
 }
