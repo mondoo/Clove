2a1cf5840f20bcf98781416ce4e0c93674cf2557
Alex <wolfplayeral@yahoo.co.uk>
2020-04-08 06:58:12 +0100

    Added rotation and scale

--- Tunic/include/Tunic/UI/Widget.hpp
+++ Tunic/include/Tunic/UI/Widget.hpp
@@ -7,8 +7,12 @@ namespace tnc::rnd {
 namespace tnc::ui {
 	class Widget{
 		//VARIABLES
-	private:
+	protected:
+		//TODO: Does this all need to be in the base class?
 		clv::mth::vec2f position{ 0.0f, 0.0f };
+		float rotation{ 0.0f };
+		clv::mth::vec2f scale{ 1.0f, 1.0f };
+
 		clv::mth::vec2f alignment{ 0.0f, 0.0f };
 		clv::mth::vec2f anchor{ 0.0f, 0.0f };
 
