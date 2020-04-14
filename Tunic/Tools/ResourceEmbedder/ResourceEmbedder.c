4d1b9cd16f3d3ded107c0277f26ecee13d9ca502
Alex <wolfplayeral@yahoo.co.uk>
2020-04-14 06:55:28 +0100

    Added the code that'll convert files into c strings

--- Tunic/Tools/ResourceEmbedder/ResourceEmbedder.c
+++ Tunic/Tools/ResourceEmbedder/ResourceEmbedder.c
@@ -1,3 +1,53 @@
-int main() {
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE* openOrExit(const char* fileName, const char* mode) {
+	FILE* file = fopen(fileName, mode);
+	if(file == NULL) {
+		perror(fileName);
+		exit(EXIT_FAILURE);
+	}
+	return file;
+}
+
+int main(int argc, char** argv) {
+	if(argc < 3) {
+		fprintf(stderr, "USAGE: %s \n\n", "Creates {file}.c from the contents of {res}\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	const char* name = argv[1];
+	FILE* resource = openOrExit(argv[2], "r");
+
+	char fileName[256];
+	snprintf(fileName, sizeof(fileName), "%s.c", name);
+
+	FILE* out = openOrExit(fileName, "w");
+	fprintf(out, "#include <stblib.h>\n\n");
+	fprintf(out, "const char %s[] = {\n", name);
+
+	unsigned char buf[256];
+	size_t nread = 0;
+	size_t lineCount = 0;
+	do {
+		nread = fread(buf, 1, sizeof(buf), resource);
+		for(size_t i = 0; i < nread; ++i) {
+			fprintf(out, "0x%02x", buf[i]);
+			if(++lineCount == 10) {
+				fprintf(out, "\n");
+				lineCount = 0;
+			}
+		}
+	} while(nread > 0);
+
+	if(lineCount > 0) {
+		fprintf(out, "\n");
+	}
+	fprintf(out, "};\n\n");
+	fprintf(out, "const size_t %sLength = sizeof(%s);\n\n", name, name);
+
+	fclose(resource);
+	fclose(out);
+
+	return EXIT_SUCCESS;
 }
