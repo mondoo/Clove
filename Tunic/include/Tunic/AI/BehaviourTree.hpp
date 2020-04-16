6e7e28b5deab051422e907dee7551c276df413aa
Alex <wolfplayeral@yahoo.co.uk>
2020-04-16 06:44:42 +0100

    Setup the BehaviourTree class itself

--- Tunic/include/Tunic/AI/BehaviourTree.hpp
+++ Tunic/include/Tunic/AI/BehaviourTree.hpp
@@ -1,6 +1,27 @@
 #pragma once
 
+#include "Tunic/AI/Task.hpp"
+
 namespace tnc::ai {
 	class BehaviourTree {
+		//VARIABLES
+	private:
+		std::unique_ptr<Task> rootTask;
+
+		//FUNCTIONS
+	public:
+		BehaviourTree();
+
+		BehaviourTree(const BehaviourTree& other) = delete;
+		BehaviourTree(BehaviourTree&& other) noexcept;
+
+		BehaviourTree& operator=(const BehaviourTree& other) = delete;
+		BehaviourTree& operator=(BehaviourTree&& other) noexcept;
+
+		~BehaviourTree();
+
+		void setRootTask(std::unique_ptr<Task> task);
+
+		void run();
 	};
 }
--- Tunic/src/AI/BehaviourTree.cpp
+++ Tunic/src/AI/BehaviourTree.cpp
@@ -1,1 +1,19 @@
 #include "Tunic/AI/BehaviourTree.hpp"
+
+namespace tnc::ai {
+	BehaviourTree::BehaviourTree() = default;
+
+	BehaviourTree::BehaviourTree(BehaviourTree&& other) noexcept = default;
+
+	BehaviourTree& BehaviourTree::operator=(BehaviourTree&& other) noexcept = default;
+
+	BehaviourTree::~BehaviourTree() = default;
+
+	void BehaviourTree::setRootTask(std::unique_ptr<Task> task) {
+		rootTask = std::move(task);
+	}
+
+	void BehaviourTree::run() {
+		rootTask->activate();
+	}
+}
