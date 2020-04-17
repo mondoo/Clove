1fe074708965d8068fa55c1c48bf61de115ff331
Alex <wolfplayeral@yahoo.co.uk>
2020-04-17 21:23:34 +0100

    Added the Selector node

---
+++ Tunic/src/AI/Selector.cpp
@@ -0,0 +1,21 @@
+#include "Tunic/AI/Selector.hpp"
+
+namespace tnc::ai {
+	Selector::Selector() = default;
+
+	Selector::Selector(Selector&& other) noexcept = default;
+
+	Selector& Selector::operator=(Selector&& other) noexcept = default;
+
+	Selector::~Selector() = default;
+
+	Task::Status Selector::activate() {
+		for(const auto& child : children) {
+			const Status status = child->activate();
+			if(status != Status::failure) {
+				return status;
+			}
+		}
+		return Status::failure;
+	}
+}
