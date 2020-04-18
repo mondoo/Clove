a843e3bc60b153ff5f277fccacaa1ed4c4cb450e
Alex <wolfplayeral@yahoo.co.uk>
2020-04-19 00:07:06 +0100

    updated static constexpr into inline constexpr

--- Tunic/include/Tunic/ECS/ECSTypes.hpp
+++ Tunic/include/Tunic/ECS/ECSTypes.hpp
@@ -4,5 +4,5 @@ namespace tnc::ecs {
 	using EntityID		= uint32_t;
 	using ComponentID	= uint64_t;
 
-	static constexpr EntityID INVALID_ENTITY_ID = 0;
+	inline constexpr EntityID INVALID_ENTITY_ID = 0;
 }
