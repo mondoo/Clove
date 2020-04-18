fec0c5e3ad774bfab8217c549eeff5df0a7d86f8
Alex <wolfplayeral@yahoo.co.uk>
2020-04-18 13:37:45 +0100

    Added a warning when trying to copy a TransformComponent

--- Tunic/src/ECS/Components/TransformComponent.cpp
+++ Tunic/src/ECS/Components/TransformComponent.cpp
@@ -17,6 +17,7 @@ namespace tnc::ecs{
 
 		parent = other.parent;
 		//Not copying children for now, transform component has no knowledge of other components
+		CLV_LOG_WARN("{0}: Unable to copy transform's children.", CLV_FUNCTION_NAME_PRETTY);
 
 		if(parent != nullptr){
 			parent->children.push_back(this);
@@ -32,6 +33,7 @@ namespace tnc::ecs{
 
 		parent = other.parent;
 		//Not copying children for now, transform component has no knowledge of other components
+		CLV_LOG_WARN("{0}: Unable to copy transform's children.", CLV_FUNCTION_NAME_PRETTY);
 
 		if(parent != nullptr){
 			parent->children.push_back(this);
