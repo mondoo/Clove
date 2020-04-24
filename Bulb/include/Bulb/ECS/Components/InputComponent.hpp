e05871cd04b382f92c0f9ca303b63b7f011b930a
Alex <wolfplayeral@yahoo.co.uk>
2020-04-24 08:01:50 +0100

    Added a system to track keybindings, allowing keys to be unbound

--- Bulb/include/Bulb/ECS/Components/InputComponent.hpp
+++ Bulb/include/Bulb/ECS/Components/InputComponent.hpp
@@ -16,11 +16,21 @@ namespace blb::ecs {
 	class InputComponent : public Component<InputComponent> {
 		friend class InputSystem;
 
+		//TYPES
+	public:
+		using BindingId = uint64_t;
+		using KeyBindingFunction	= std::function<InputResponse(const clv::Keyboard::Event&)>;
+		using MouseBindingFunction	= std::function<InputResponse(const clv::Mouse::Event&)>;
+
 		//VARIABLES
-	//private:
-	public: //Temp
-		std::unordered_map<clv::Key, std::vector<std::function<InputResponse(const clv::Keyboard::Event&)>>> keyBindings;
-		std::unordered_map<clv::MouseButton, std::vector<std::function<InputResponse(const clv::Mouse::Event&)>>> mouseButtonBindings;
+	private:
+		static BindingId nextId;
+
+		std::unordered_map<clv::Key, std::unordered_map<BindingId, size_t>> keyIdToIndexMap;
+		std::unordered_map<clv::Key, std::vector<KeyBindingFunction>> keyBindings;
+
+		std::unordered_map<clv::MouseButton, std::unordered_map<BindingId, size_t>> buttonIdToIndexMap;
+		std::unordered_map<clv::MouseButton, std::vector<MouseBindingFunction>> mouseButtonBindings;
 
 		//FUNCTIONS
 	public:
@@ -34,7 +44,19 @@ namespace blb::ecs {
 
 		~InputComponent();
 
-		//template<typename FunctionType>
+		template<typename FunctionType, typename ObjectType>
+		[nodiscard] BindingId bind(clv::Key key, FunctionType&& function, ObjectType* object);
+		template<typename FunctionType, typename ObjectType>
+		[nodiscard] BindingId bind(clv::MouseButton button, FunctionType&& function, ObjectType* object);
+		
+		template<typename FunctionType>
+		[nodiscard] BindingId bind(clv::Key key, FunctionType&& function);
+		template<typename FunctionType>
+		[nodiscard] BindingId bind(clv::MouseButton button, FunctionType&& function);
 
+		void unbind(clv::Key key, BindingId id);
+		void unbind(clv::MouseButton button, BindingId id);
 	};
 }
+
+#include "InputComponent.inl"
---
+++ Bulb/include/Bulb/ECS/Components/InputComponent.inl
@@ -0,0 +1,49 @@
+namespace blb::ecs {
+	template<typename FunctionType, typename ObjectType>
+	InputComponent::BindingId InputComponent::bind(clv::Key key, FunctionType&& function, ObjectType* object) {
+		const BindingId id = nextId++;
+
+		keyBindings[key].emplace_back(std::bind(function, object, std::placeholders::_1));
+		const size_t index = keyBindings[key].size() - 1;
+
+		keyIdToIndexMap[id] = index;
+
+		return id;
+	}
+
+	template<typename FunctionType, typename ObjectType>
+	InputComponent::BindingId InputComponent::bind(clv::MouseButton button, FunctionType&& function, ObjectType* object) {
+		const BindingId id = nextId++;
+
+		mouseButtonBindings[button].emplace_back(std::bind(function, object, std::placeholders::_1));
+		const size_t index = mouseButtonBindings[button].size() - 1;
+
+		buttonIdToIndexMap[id] = index;
+
+		return id;
+	}
+
+	template<typename FunctionType>
+	InputComponent::BindingId InputComponent::bind(clv::Key key, FunctionType&& function) {
+		const BindingId id = nextId++;
+
+		keyBindings[key].emplace_back(function);
+		const size_t index = keyBindings[key].size() - 1;
+
+		keyIdToIndexMap[key][id] = index;
+
+		return id;
+	}
+
+	template<typename FunctionType>
+	InputComponent::BindingId InputComponent::bind(clv::MouseButton button, FunctionType&& function) {
+		const BindingId id = nextId++;
+
+		mouseButtonBindings[button].emplace_back(function);
+		const size_t index = mouseButtonBindings[button].size() - 1;
+
+		buttonIdToIndexMap[button][id] = index;
+
+		return id;
+	}
+}
