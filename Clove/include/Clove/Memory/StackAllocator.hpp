fb2a9631cce1dcf508beea9d8ab7944306387ba9
Alex <wolfplayeral@yahoo.co.uk>
2020-04-27 19:06:22 +0100

    Set up the basic functionality of the StackAllocator

--- Clove/include/Clove/Memory/StackAllocator.hpp
+++ Clove/include/Clove/Memory/StackAllocator.hpp
@@ -2,5 +2,33 @@
 
 namespace clv::mem {
 	class StackAllocator {
+		//TYPES
+	public:
+		using Marker = size_t;
+
+		//VARIABLES
+	private:
+		std::unique_ptr<char[]> stack;
+		char* top = nullptr;
+
+		//FUNCTIONS
+	public:
+		StackAllocator() = delete;
+		StackAllocator(size_t sizeBytes);
+		
+		StackAllocator(const StackAllocator& other) = delete;
+		StackAllocator(StackAllocator&& other) noexcept;
+
+		StackAllocator& operator=(const StackAllocator& other) = delete;
+		StackAllocator& operator=(StackAllocator&& other) noexcept;
+
+		~StackAllocator();
+
+		Marker markPosition();
+
+		void* alloc(size_t bytes);
+
+		void free();
+		void free(Marker marker);
 	};
 }
--- Clove/src/Memory/StackAllocator.cpp
+++ Clove/src/Memory/StackAllocator.cpp
@@ -1,4 +1,32 @@
 #include "Clove/Memory/StackAllocator.hpp"
 
 namespace clv::mem {
+	StackAllocator::StackAllocator(size_t sizeBytes) {
+		stack = std::unique_ptr<char[]>{ new char[sizeBytes] };
+		top = &stack[0];
+	}
+
+	StackAllocator::StackAllocator(StackAllocator&& other) noexcept = default;
+
+	StackAllocator& StackAllocator::operator=(StackAllocator&& other) noexcept = default;
+
+	StackAllocator::~StackAllocator() = default;
+
+	StackAllocator::Marker StackAllocator::markPosition() {
+		return top - &stack[0];
+	}
+
+	void* StackAllocator::alloc(size_t bytes) {
+		void* element = top;
+		top += bytes;
+		return element;
+	}
+
+	void StackAllocator::free() {
+		top = &stack[0];
+	}
+
+	void StackAllocator::free(Marker marker) {
+		top = &stack[0] + marker;
+	}
 }
