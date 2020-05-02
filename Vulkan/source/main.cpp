f97e8a31cca5dd43bf6e12fa39b7010734fef4b1
Alex <wolfplayeral@yahoo.co.uk>
2020-05-02 13:15:02 +0100

    Picked a physical device

--- Vulkan/source/main.cpp
+++ Vulkan/source/main.cpp
@@ -43,8 +43,9 @@ private:
 	GLFWwindow* window = nullptr;
 
 	//Vulkan
-	VkInstance instance = nullptr;
-	VkDebugUtilsMessengerEXT debugMessenger = nullptr;
+	VkInstance instance = VK_NULL_HANDLE;
+	VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE;
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
 	//FUNCTIONS
 public:
@@ -68,6 +69,7 @@ private:
 	void initVulkan() {
 		createInstance();
 		setupDebugMessenger();
+		pickPhysicalDevice();
 	}
 
 	void mainLoop() {
@@ -207,6 +209,49 @@ private:
 		if(createDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
 			throw std::runtime_error("failed to set up debug messenger!");
 		}
+	}
+
+	//Physical devices are GPUs
+	void pickPhysicalDevice(){
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+		if(deviceCount == 0) {
+			throw std::runtime_error("failed to find GPUs with Vulkan support!");
+		}
+
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+		for(const auto&device : devices) {
+			if(isDeviceSuitable(physicalDevice)) {
+				physicalDevice = device;
+				break;
+			}
+		}
+
+		if(physicalDevice == VK_NULL_HANDLE) {
+			throw std::runtime_error("failed to find a suitable GPU!");
+		}
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device){
+		//Basic properties (name, type, supported vk version)
+		VkPhysicalDeviceProperties devicePoperties;
+		vkGetPhysicalDeviceProperties(device, &devicePoperties);
+
+		//Feature (texture compression, 64 bit floats, multi viewport rendering)
+		VkPhysicalDeviceFeatures deviceFeatures;
+		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+		//Here we could either check for specific features:
+#if 0
+		//Only want a dedicated GPU that supports gemoetry shader
+		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && deviceFeatures.geometryShader;
+#endif
+		//Or we could give a score to each physicalDevice and then return the highest score
+		//For now, any GPU will do
+		return true;
 	}
 
 	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
