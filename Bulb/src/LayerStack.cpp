4c77df2eb117f1ede2df9ea1e3a424f321f100de
Alex <wolfplayeral@yahoo.co.uk>
2020-05-04 07:12:53 +0100

    Fixed potential crashes when popping layers off of the layer stack

--- Bulb/src/LayerStack.cpp
+++ Bulb/src/LayerStack.cpp
@@ -12,40 +12,37 @@ namespace blb{
 	LayerStack::~LayerStack(){
 		CLV_LOG_TRACE("Deconstructing layer stack...");
 		for(auto& layer : layers){
-			layer->onDetach();
 			CLV_LOG_DEBUG("{0} detached", layer->getName());
+			layer->onDetach();
 		}
 	}
 
 	void LayerStack::pushLayer(std::shared_ptr<Layer> layer){
-		layer->onAttach();
 		CLV_LOG_DEBUG("Attached layer: {0}", layer->getName());
-		layers.emplace(layers.begin() + layerInsertIndex, std::move(layer));
-		++layerInsertIndex;
+		layer->onAttach();
+		layers.emplace(layers.begin() + layerInsertIndex++, std::move(layer));
 	}
 
 	void LayerStack::popLayer(const std::shared_ptr<Layer>& layer){
-		auto it = std::find(layers.begin(), layers.end(), layer);
-		if(it != layers.end()){
-			layers.erase(it);
-			--layerInsertIndex;
+		if(auto it = std::find(layers.begin(), layers.end(), layer); it != layers.end()) {
 			CLV_LOG_DEBUG("Popped layer: {0}", (*it)->getName());
 			(*it)->onDetach();
+			layers.erase(it);
+			--layerInsertIndex;
 		}
 	}
 
 	void LayerStack::pushOverlay(std::shared_ptr<Layer> overlay){
-		overlay->onAttach();
 		CLV_LOG_DEBUG("Attached overlay: {0}", overlay->getName());
+		overlay->onAttach();
 		layers.emplace_back(std::move(overlay));
 	}
 
 	void LayerStack::popOverlay(const std::shared_ptr<Layer>& overlay){
-		auto it = std::find(layers.begin(), layers.end(), overlay);
-		if(it != layers.end()){
-			layers.erase(it);
+		if(auto it = std::find(layers.begin(), layers.end(), overlay); it != layers.end()) {
 			CLV_LOG_DEBUG("Popped overlay: {0}", (*it)->getName());
 			(*it)->onDetach();
+			layers.erase(it);
 		}
 	}
 
