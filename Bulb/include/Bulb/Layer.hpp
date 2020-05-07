448309890b1d4305448fe1c70d790ebfb0c5297c
Alex <wolfplayeral@yahoo.co.uk>
2020-05-07 07:20:40 +0100

    Added an onInputEvent function to Layer

--- Bulb/include/Bulb/Layer.hpp
+++ Bulb/include/Bulb/Layer.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
-namespace blb{
-	class Layer{
+namespace clv{
+	struct InputEvent;
+}
+
+namespace blb {
+	class Layer {
 		//VARIABLES
 	protected:
 		std::string debugName;
@@ -11,9 +15,12 @@ namespace blb{
 		Layer(std::string name = "Layer");
 		virtual ~Layer() = default;
 
-		virtual void onAttach(){}
-		virtual void onUpdate(clv::utl::DeltaTime deltaTime){}
-		virtual void onDetach(){}
+		virtual void onAttach() {}
+
+		virtual void onInputEvent(clv::InputEvent& event) {}
+		virtual void onUpdate(clv::utl::DeltaTime deltaTime) {}
+
+		virtual void onDetach() {}
 
 		const std::string& getName() const;
 	};
