0140c8317fe6dbc3eb8187affc815071ae8aa888
Alex <wolfplayeral@yahoo.co.uk>
2020-05-09 11:37:51 +0100

    Added the skeleton for the UIFrame
    This will be responsible for composing and drawing all UIElements

---
+++ Bulb/include/Bulb/UI/UIFrame.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace blb::ui {
+	class UIElement;
+}
+
+namespace blb::ui {
+	class UIFrame {
+		//VARIABLES
+	private:
+		std::vector<UIElement> elements;
+
+		//FUNCTIONS
+	public:
+		
+	};
+}
---
+++ Bulb/src/UI/UIFrame.cpp
@@ -0,0 +1,7 @@
+#include "Bulb/UI/UIFrame.hpp"
+
+#include "Bulb/UI/UIElement.hpp"
+
+namespace blb::ui {
+	
+}
