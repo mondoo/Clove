eec7e6d06eacdada7e5eb660baa0bc36caffdb19
Alex <wolfplayeral@yahoo.co.uk>
2020-05-09 11:20:33 +0100

    Added a UIElement class to create an interface for simple elements

---
+++ Bulb/include/Bulb/UI/UIElement.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Bulb/InputResponse.hpp"
+
+#include <Clove/Input/Keyboard.hpp>
+#include <Clove/Input/Mouse.hpp>
+
+namespace blb::rnd {
+	class Renderer2D;
+}
+
+namespace blb::ui {
+	class UIElement {
+		//FUNCTIONS
+	public:
+		virtual ~UIElement() = default;
+
+		virtual InputResponse onKeyEvent(const clv::Keyboard::Event& event) = 0;
+		virtual InputResponse onMouseEvent(const clv::Mouse::Event& event) = 0;
+
+		virtual void draw(rnd::Renderer2D& renderer, const clv::mth::vec2f& drawSpace) = 0;
+	};
+}
