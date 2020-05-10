28823ec21882c24344cb2aa2cd70ab238a8fad27
Alex <wolfplayeral@yahoo.co.uk>
2020-05-10 10:37:59 +0100

    Set up the basic structure of the UIFrame

--- Bulb/include/Bulb/UI/UIFrame.hpp
+++ Bulb/include/Bulb/UI/UIFrame.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "Bulb/InputResponse.hpp"
+#include <Clove/Input/InputEvent.hpp>
+
+namespace blb::rnd {
+	class Renderer3D;
+}
+
 namespace blb::ui {
 	class UIElement;
 }
@@ -8,10 +15,18 @@ namespace blb::ui {
 	class UIFrame {
 		//VARIABLES
 	private:
-		std::vector<UIElement> elements;
+		//SharedPtr for now, good candidate for an allocator to get cache friendly
+		std::vector<std::shared_ptr<UIElement>> elements;
+
+		clv::mth::vec2i size;
 
 		//FUNCTIONS
 	public:
-		
+		UIFrame() = delete;
+		UIFrame(clv::mth::vec2i size);
+
+		InputResponse propogateInput(const clv::InputEvent& event);
+
+		void drawChildren(rnd::Renderer3D& renderer);
 	};
 }
--- Bulb/src/UI/UIFrame.cpp
+++ Bulb/src/UI/UIFrame.cpp
@@ -3,5 +3,33 @@
 #include "Bulb/UI/UIElement.hpp"
 
 namespace blb::ui {
-	
+	UIFrame::UIFrame(clv::mth::vec2i size) 
+		: size(std::move(size)){
+	}
+
+	InputResponse UIFrame::propogateInput(const clv::InputEvent& event) {
+		for(auto& element : elements) {
+			//Key
+			if(event.keyboardEvent) {
+				if(element->onKeyEvent(*event.keyboardEvent) == InputResponse::Consumed) {
+					return InputResponse::Consumed;
+				}
+			}
+
+			//Mouse
+			if(event.mouseEvent) {
+				if(element->onMouseEvent(*event.mouseEvent) == InputResponse::Consumed) {
+					return InputResponse::Consumed;
+				}
+			}
+		}
+
+		return InputResponse::Ignored;
+	}
+
+	void UIFrame::drawChildren(rnd::Renderer3D& renderer) {
+		for(auto& element : elements) {
+			element->draw(renderer, size);
+		}
+	}
 }
