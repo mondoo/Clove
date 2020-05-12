520b7d95a5b83c93bb891e200d550a553dec7711
Alex <wolfplayeral@yahoo.co.uk>
2020-05-12 11:39:42 +0100

    Added profiling sessions to Stem's entry point

--- Stem/include/Stem/EntryPoint.hpp
+++ Stem/include/Stem/EntryPoint.hpp
@@ -3,12 +3,17 @@
 #include "Stem/Application.hpp"
 
 int main() {
+	CLV_PROFILE_BEGIN_SESSION("Startup", "GarlicProfile-Startup.json");
 	clv::Log::init();
 
 	stm::Application app{};
 
 	app.start();
+	CLV_PROFILE_END_SESSION();
+
+	CLV_PROFILE_BEGIN_SESSION("Runtime", "GarlicProfile-Runtime.json");
 	app.run();
+	CLV_PROFILE_END_SESSION();
 
 	return 0;
 }
