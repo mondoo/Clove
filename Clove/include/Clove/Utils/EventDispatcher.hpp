54a3c9e8359bb5af1b505e6fcf13e5eb62b12ee8
Alex <wolfplayeral@yahoo.co.uk>
2020-05-15 10:50:16 +0100

    Added event containers to abstract out the storage of the events

--- Clove/include/Clove/Utils/EventDispatcher.hpp
+++ Clove/include/Clove/Utils/EventDispatcher.hpp
@@ -1,20 +1,58 @@
 #pragma once
 
+#include <unordered_map>
+#include <cinttypes>
+
 namespace clv::utl {
+	class EventContainerInterface{
+
+	};
+
+	template<typename EventType>
+	class EventContainer : public EventContainerInterface{
+	public:
+		using FunctionType = std::function<void()>;
+
+		std::vector<std::function<void()>> listeners; //TODO: have the listeners take the event that's been called (will contain the data)
+	};
+
+	class EventManager{
+	private:
+		std::unordered_map<size_t, std::unique_ptr<EventContainerInterface>> containers;
+
+	public:
+		template<typename EventType>
+		EventContainer<EventType>& getEventContainer() {
+			const size_t eventId = typeid(EventType).hash_code();
+
+			if(containers.find(eventId) == containers.end()) {
+				containers[eventId] = std::make_unique<EventContainer<EventType>>();
+			}
+
+			return static_cast<EventContainer<EventType>&>(*containers[eventId]);
+		}
+	};
+
 	class EventDispatcher {
 		//VARIABLES
 	private:
+		EventManager manager;
 
 		//FUNCTIONS
 	public:
 		//TODO: Ctors
 
+		//TODO: return some type of ID
 		template<typename EventType>
-		void bindToEvent() {
+		void bindToEvent(typename EventContainer<EventType>::FunctionType function) {
+			manager.getEventContainer<EventType>().listeners.push_back(function);
 		}
 
 		template<typename EventType>
 		void broadCastEvent(/* params */) {
+			for(auto&& func : manager.getEventContainer<EventType>().listeners) {
+				func();
+			}
 		}
 	};
 }
