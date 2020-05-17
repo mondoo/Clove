0cbe065bf639bd30027d06221854d1d64775c063
Alex <wolfplayeral@yahoo.co.uk>
2020-05-17 11:26:26 +0100

    Moved the EventContainer to be a type inside EventManager

--- Clove/include/Clove/Utils/EventDispatcher.hpp
+++ Clove/include/Clove/Utils/EventDispatcher.hpp
@@ -3,80 +3,94 @@
 namespace clv::utl {
 	using ListenerId = size_t;
 
-	class EventContainerInterface {
-	public:
-	protected:
-		//In base class so it's not a static element per template class
-		inline static ListenerId nextId = 0;
-
-	public:
-		virtual void removeListener(ListenerId id) = 0;
-	};
-
 	template<typename EventType>
-	class EventContainer : public EventContainerInterface {
-	public:
-		using FunctionType = std::function<void(EventType&)>;
-
-	private:
-		std::unordered_map<ListenerId, size_t> listenerIdToIndexMap;
-		std::vector<FunctionType> listeners;
-
-	public:
-		//TODO: rather than an ID should it return a struct that automatically unbinds when out of scope?
-		ListenerId addListener(FunctionType function) {
-			listeners.push_back(function);
-			
-			ListenerId id = nextId++;
-			listenerIdToIndexMap[id] = listeners.size() - 1;
-
-			return id;
-		}
-		void removeListener(ListenerId id) final{
-			if(auto iter = listenerIdToIndexMap.find(id); iter != listenerIdToIndexMap.end()) {
-				const size_t index = iter->second;
-				const size_t lastIndex = listeners.size() - 1;
-
-				std::optional<ListenerId> movedEvent;
-				if(index < lastIndex) {
-					for(auto&& idToIndexPair : listenerIdToIndexMap) {
-						if(idToIndexPair.second == lastIndex) {
-							movedEvent = idToIndexPair.first;
-							break;
-						}
-					}
-
-					listeners[iter->second] = listeners.back();
-				}
-				listeners.pop_back();
-				listenerIdToIndexMap.erase(id);
-
-				if(movedEvent) {
-					listenerIdToIndexMap[*movedEvent] = index;
-				}
-			}
-		}
-
-		typename std::vector<FunctionType>::iterator begin() {
-			return listeners.begin();
-		}
-		typename std::vector<FunctionType>::const_iterator begin() const {
-			return listeners.begin();
-		}
-
-		typename std::vector<FunctionType>::iterator end() {
-			return listeners.end();
-		}
-		typename std::vector<FunctionType>::const_iterator end() const {
-			return listeners.end();
-		}
-	};
+	using ListenerFunction = std::function<void(EventType&)>;
 
 	class EventManager {
+		//TYPES
+	private:
+		class EventContainerInterface {
+			//VARIABLES
+		protected:
+			inline static ListenerId nextId = 0;
+
+			//FUNCTI0NS
+		public:
+			virtual ~EventContainerInterface() = default;
+
+			virtual void removeListener(ListenerId id) = 0;
+		};
+
+		template<typename EventType>
+		class EventContainer : public EventContainerInterface {
+			//TYPES
+		private:
+			using ContainerFunctionType = ListenerFunction<EventType>;
+			
+			//VARIABLES
+		private:
+			std::unordered_map<ListenerId, size_t> listenerIdToIndexMap;
+			std::vector<ContainerFunctionType> listeners;
+
+			//FUNCTIONS
+		public:
+			//TODO: rather than an ID should it return a struct that automatically unbinds when out of scope?
+			ListenerId addListener(ContainerFunctionType function) {
+				listeners.push_back(function);
+
+				ListenerId id = nextId++;
+				listenerIdToIndexMap[id] = listeners.size() - 1;
+
+				return id;
+			}
+			void removeListener(ListenerId id) final {
+				if(auto iter = listenerIdToIndexMap.find(id); iter != listenerIdToIndexMap.end()) {
+					const size_t index = iter->second;
+					const size_t lastIndex = listeners.size() - 1;
+
+					std::optional<ListenerId> movedEvent;
+					if(index < lastIndex) {
+						for(auto&& idToIndexPair : listenerIdToIndexMap) {
+							if(idToIndexPair.second == lastIndex) {
+								movedEvent = idToIndexPair.first;
+								break;
+							}
+						}
+
+						listeners[iter->second] = listeners.back();
+					}
+					listeners.pop_back();
+					listenerIdToIndexMap.erase(id);
+
+					if(movedEvent) {
+						listenerIdToIndexMap[*movedEvent] = index;
+					}
+				}
+			}
+
+			typename std::vector<ContainerFunctionType>::iterator begin() {
+				return listeners.begin();
+			}
+			typename std::vector<ContainerFunctionType>::const_iterator begin() const {
+				return listeners.begin();
+			}
+
+			typename std::vector<ContainerFunctionType>::iterator end() {
+				return listeners.end();
+			}
+			typename std::vector<ContainerFunctionType>::const_iterator end() const {
+				return listeners.end();
+			}
+		};
+
+		//VARIABLES
 	private:
 		std::unordered_map<size_t, std::unique_ptr<EventContainerInterface>> containers;
 
+		//FUNCTIONS
 	public:
+		//TODO: Ctors
+
 		template<typename EventType>
 		EventContainer<EventType>& getEventContainer() {
 			const size_t eventId = typeid(EventType).hash_code();
@@ -99,8 +113,8 @@ namespace clv::utl {
 		//TODO: Ctors
 
 		template<typename EventType>
-		ListenerId bindToEvent(typename EventContainer<EventType>::FunctionType&& function) {
-			return manager.getEventContainer<EventType>().addListener(std::forward<typename EventContainer<EventType>::FunctionType>(function));
+		ListenerId bindToEvent(ListenerFunction<EventType>&& function) {
+			return manager.getEventContainer<EventType>().addListener(std::forward<ListenerFunction<EventType>>(function));
 		}
 
 		template<typename EventType>
