5157cd3b0262da79e9ec168a332c3a5f860373af
Alex <wolfplayeral@yahoo.co.uk>
2020-05-20 07:42:49 +0100

    Set up a rough API for the classes used in the state machine

--- Bulb/include/Bulb/StateMachine/StateMachine.hpp
+++ Bulb/include/Bulb/StateMachine/StateMachine.hpp
@@ -1,15 +1,75 @@
 #pragma once
 
 namespace blb {
-	class State{
+	class Transition;
+	class State;
 
+	class Action {
+		//VARIABLES
+	private:
+		//FUNCTIONS
+	public:
 	};
 
-	class Transition{
+	class Condition {
+		//VARIABLES
+	private:
+		//FUNCTIONS
+	public:
+		virtual ~Condition() = 0;
 
+		virtual bool test() const = 0;
 	};
 
-	class StateMachine{
+	class State {
+		//VARIABLES
+	private:
+		std::vector<std::unique_ptr<Transition>> transitions;
 
+		//FUNCTIONS
+	public:
+		//TODO: Ctors
+		//virtual ~State() = default;
+
+		std::vector<Action*> getEntryActions() {
+		}
+		std::vector<Action*> getActions() {
+		}
+		std::vector<Action*> getExitActions() {
+		}
+
+		std::vector<Transition*> getTransitions() {
+		}
+	};
+
+	class Transition {
+		//VARIABLES
+	private:
+		std::unique_ptr<Condition> condition;
+
+		//FUNCTIONS
+	public:
+		//virtual ~Transition() = default;
+
+		bool isTriggered() const {
+			condition->test();
+		}
+
+		State* getState() const {
+		}
+
+		std::vector<Action*> getActions() const {
+		}
+	};
+
+	class StateMachine {
+		//VARIABLES
+	private:
+		//FUNCTIONS
+	public:
+		//TODO: Ctors
+
+		void update() {
+		}
 	};
 }
