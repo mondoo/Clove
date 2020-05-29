560e152bcafc42d12be1b4a47dfcfd29d59caec3
Alex <wolfplayeral@yahoo.co.uk>
2020-05-29 07:22:03 +0100

    Set up a rough API for the blackboard

--- Bulb/include/Bulb/AI/BlackBoard.hpp
+++ Bulb/include/Bulb/AI/BlackBoard.hpp
@@ -1,12 +1,41 @@
 #pragma once
 
 namespace blb::ai {
+	using BlackBoardKey = size_t;
+
+	struct BlackBoardDataHeader {
+		void* prevItem = nullptr;
+		void* nextItem = nullptr;
+
+		size_t key = 0;
+		size_t size = 0;
+	};
+
 	class BlackBoard {
 		//VARIABLES
 	private:
+		//Idea is that we store data in here with a header value so we know the memory layout
+		void* data = nullptr;
+		void* pointer = nullptr; //Points to current free space
 
 		//FUNCTIONS
 	public:
 		//TODO: Ctors
+		BlackBoard() {
+			data = malloc(1024 * 1024); //Lets just give ourselves a meg at the moment
+		}
+
+		template<typename DataType>
+		BlackBoardKey addItem(DataType item) {
+		}
+
+		template<typename DataType>
+		DataType& getItem(BlackBoardKey key) {
+		}
+
+		void removeItem(BlackBoardKey key) {
+		}
+
+	private:
 	};
 }
