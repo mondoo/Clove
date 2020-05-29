180b41564df15cf4e48a91953695f7640e073669
Alex <wolfplayeral@yahoo.co.uk>
2020-05-29 07:32:02 +0100

    Implemented BlackBoard::addItem

--- Bulb/include/Bulb/AI/BlackBoard.hpp
+++ Bulb/include/Bulb/AI/BlackBoard.hpp
@@ -16,17 +16,42 @@ namespace blb::ai {
 	private:
 		//Idea is that we store data in here with a header value so we know the memory layout
 		void* data = nullptr;
+		size_t dataSize = 0;
 		void* pointer = nullptr; //Points to current free space
+
+		size_t nextKey = 1;
 
 		//FUNCTIONS
 	public:
 		//TODO: Ctors
 		BlackBoard() {
-			data = malloc(1024 * 1024); //Lets just give ourselves a meg at the moment
+			dataSize = 1024 * 1024; //Lets just give ourselves a meg at the moment
+			data = malloc(dataSize); 
+			pointer = data;
 		}
 
 		template<typename DataType>
 		BlackBoardKey addItem(DataType item) {
+			const size_t headerSize = sizeof(BlackBoardDataHeader);
+			const size_t itemSize = sizeof(DataType);
+			const size_t totalSize = headerSize + itemSize;
+			CLV_ASSERT(pointer + totalSize > data + dataSize, "Not enough space!"); //TODO: Remove assertion?
+
+			BlackBoardDataHeader header{};
+			if(pointer != data) {
+				header.prevItem = pointer;
+				//TODO: Update nextItem of previous
+			}
+			header.key = nextKey++;
+			header.size = itemSize;
+
+			//TODO: Doing memcpy for simplicity, try placement new
+			memcpy(pointer, &header, headerSize);
+			pointer += headerSize;
+			memcpy(pointer, &item, itemSize);
+			pointer += itemSize;
+
+			return header.key;
 		}
 
 		template<typename DataType>
