3864d71240382211aedf158885f20e6045e2f2b7
Alex <wolfplayeral@yahoo.co.uk>
2020-06-01 21:06:51 +0100

    Stripped down the logger
    There is now a single define (in-case logging wants to be compiled out). A Context which contains app info and a single log function

--- Clove/include/Clove/Log.hpp
+++ Clove/include/Clove/Log.hpp
@@ -1,49 +1,31 @@
 #pragma once
 
-#if !_M_CEE //Log is excluded from CLR builds until a better work around is found
-
-#include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
+#include <spdlog/spdlog.h>
 
-namespace clv{
-	class Log{
-		//VARIABLES
-	private:
-		static std::shared_ptr<spdlog::logger> coreLogger;
-		static std::shared_ptr<spdlog::logger> clientLogger;
+namespace clv {
+	namespace Log {
+		//TYPES
+		enum class Level {
+			Trace,
+			Debug,
+			Warning,
+			Error,
+			Critical
+		};
 
-		//FUNCTIONS
-	public:
-		static std::shared_ptr<spdlog::logger>& getCoreLogger();
-		static std::shared_ptr<spdlog::logger>& getClientLogger();
+		struct Context {
+		private:
+			spdlog::logger logger;
+		};
 
-		static void init();
+		Context createContext(std::string_view name) {
+		}
+
+		template<typename... Args>
+		void log(const Context& context, Level level, Args&&... args) {
+		}
 	};
 }
 
-#if CLV_ENGINE
-	//Core log macros
-	#define CLV_LOG_TRACE(...)		::clv::Log::getCoreLogger()->trace(__VA_ARGS__)
-	#define CLV_LOG_DEBUG(...)		::clv::Log::getCoreLogger()->debug(__VA_ARGS__)
-	#define CLV_LOG_INFO(...)		::clv::Log::getCoreLogger()->info(__VA_ARGS__)
-	#define CLV_LOG_WARN(...)		::clv::Log::getCoreLogger()->warn(__VA_ARGS__)
-	#define CLV_LOG_ERROR(...)		::clv::Log::getCoreLogger()->error(__VA_ARGS__)
-	#define CLV_LOG_CRITICAL(...)	::clv::Log::getCoreLogger()->critical(__VA_ARGS__)
-#else
-	//Client log macros
-	#define CLV_LOG_TRACE(...)		::clv::Log::getClientLogger()->trace(__VA_ARGS__)
-	#define CLV_LOG_DEBUG(...)		::clv::Log::getClientLogger()->debug(__VA_ARGS__)
-	#define CLV_LOG_INFO(...)		::clv::Log::getClientLogger()->info(__VA_ARGS__)
-	#define CLV_LOG_WARN(...)		::clv::Log::getClientLogger()->warn(__VA_ARGS__)
-	#define CLV_LOG_ERROR(...)		::clv::Log::getClientLogger()->error(__VA_ARGS__)
-	#define CLV_LOG_CRITICAL(...)	::clv::Log::getClientLogger()->critical(__VA_ARGS__)
-#endif
-	
-#else
-	#define CLV_LOG_TRACE(...)	
-	#define CLV_LOG_DEBUG(...)	
-	#define CLV_LOG_INFO(...)	
-	#define CLV_LOG_WARN(...)	
-	#define CLV_LOG_ERROR(...)	
-	#define CLV_LOG_CRITICAL(...)
-#endif
+#define GARLIC_LOG(context, level, ...) ::clv::Log::log(contest, level, __VA_ARGS__);
