b1434d6bbf9497fe9591a81e3871955fba323e4d
Alex <wolfplayeral@yahoo.co.uk>
2020-06-05 07:21:07 +0100

    Moved over the function that help choosing a swapchain

--- Clove/src/Graphics/Vulkan/VKSwapchain.cpp
+++ Clove/src/Graphics/Vulkan/VKSwapchain.cpp
@@ -1,13 +1,51 @@
 #include "Clove/Graphics/Vulkan/VKSwapChain.hpp"
 
 namespace clv::gfx::vk {
+	static VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+		for(const auto& availableFormat : availableFormats) {
+			if(availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+				return availableFormat;
+			}
+		}
+
+		//Fall back to the first one if we can't find a surface format we want
+		return availableFormats[0];
+	}
+
+	static VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
+		for(const auto& availablePresentMode : availablePresentModes) {
+			if(availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+				return availablePresentMode;
+			}
+		}
+
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
+	static VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+		if(capabilities.currentExtent.width != UINT32_MAX) {
+			return capabilities.currentExtent;
+		} else {
+			int width;
+			int height;
+			glfwGetFramebufferSize(window, &width, &height);
+
+			VkExtent2D actualExtent{ width, height };
+
+			actualExtent.width = std::max(capabilities.minImageExtent.width, std::min(capabilities.maxImageExtent.width, actualExtent.width));
+			actualExtent.height = std::max(capabilities.minImageExtent.height, std::min(capabilities.maxImageExtent.height, actualExtent.height));
+
+			return actualExtent;
+		}
+	}
+
 	VKSwapchain::VKSwapchain(VkDevice device, VkPhysicalDevice physicalDevice, VkSurfaceKHR surface, const QueueFamilyIndices& familyIndices, SwapChainDescriptor descriptor) 
 		: device(device){
 		SwapChainSupportDetails swapChainSuport = querySwapChainSupport(physicalDevice);
 
-		VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSuport.formats);
-		VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSuport.presentModes);
-		VkExtent2D extent = chooseSwapExtent(swapChainSuport.capabilities);
+		VkSurfaceFormatKHR surfaceFormat	= chooseSwapSurfaceFormat(swapChainSuport.formats);
+		VkPresentModeKHR presentMode		= chooseSwapPresentMode(swapChainSuport.presentModes);
+		VkExtent2D extent					= chooseSwapExtent(swapChainSuport.capabilities);
 
 		uint32_t queueFamilyIndices[] = { *familyIndices.graphicsFamily, *familyIndices.presentFamily };
 
@@ -18,28 +56,28 @@ namespace clv::gfx::vk {
 		}
 
 		VkSwapchainCreateInfoKHR createInfo{};
-		createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
-		createInfo.surface = surface;
-		createInfo.minImageCount = imageCount;
-		createInfo.imageFormat = surfaceFormat.format;
-		createInfo.imageColorSpace = surfaceFormat.colorSpace;
-		createInfo.imageExtent = extent;
+		createInfo.sType			= VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		createInfo.surface			= surface;
+		createInfo.minImageCount	= imageCount;
+		createInfo.imageFormat		= surfaceFormat.format;
+		createInfo.imageColorSpace	= surfaceFormat.colorSpace;
+		createInfo.imageExtent		= extent;
 		createInfo.imageArrayLayers = 1;
-		createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+		createInfo.imageUsage		= VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
 		if(familyIndices.graphicsFamily != familyIndices.presentFamily) {
-			createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
-			createInfo.queueFamilyIndexCount = 2;
-			createInfo.pQueueFamilyIndices = queueFamilyIndices;
+			createInfo.imageSharingMode			= VK_SHARING_MODE_CONCURRENT;
+			createInfo.queueFamilyIndexCount	= 2;
+			createInfo.pQueueFamilyIndices		= queueFamilyIndices;
 		} else {
-			createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
-			createInfo.queueFamilyIndexCount = 0;
-			createInfo.pQueueFamilyIndices = nullptr;
+			createInfo.imageSharingMode			= VK_SHARING_MODE_EXCLUSIVE;
+			createInfo.queueFamilyIndexCount	= 0;
+			createInfo.pQueueFamilyIndices		= nullptr;
 		}
-		createInfo.preTransform = swapChainSuport.capabilities.currentTransform;
-		createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
-		createInfo.presentMode = presentMode;
-		createInfo.clipped = VK_TRUE;
-		createInfo.oldSwapchain = VK_NULL_HANDLE;
+		createInfo.preTransform		= swapChainSuport.capabilities.currentTransform;
+		createInfo.compositeAlpha	= VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+		createInfo.presentMode		= presentMode;
+		createInfo.clipped			= VK_TRUE;
+		createInfo.oldSwapchain		= VK_NULL_HANDLE;
 
 		if(vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS) {
 			throw std::runtime_error("failed to create swap chain!");
