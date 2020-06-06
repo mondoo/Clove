b6f7f99ecb54707e846a34f077492a1ff6356461
Alex <wolfplayeral@yahoo.co.uk>
2020-06-06 16:50:25 +0100

    Added missing colour blend state create info

--- Clove/src/Graphics/Vulkan/VKPipelineObject.cpp
+++ Clove/src/Graphics/Vulkan/VKPipelineObject.cpp
@@ -82,6 +82,17 @@ namespace clv::gfx::vk {
 		colourBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
 		colourBlendAttachment.blendEnable	 = VK_FALSE;//TODO: Enable blending
 
+		VkPipelineColorBlendStateCreateInfo colorBlending{};
+		colorBlending.sType				= VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+		colorBlending.logicOpEnable		= VK_FALSE;
+		colorBlending.logicOp			= VK_LOGIC_OP_COPY;
+		colorBlending.attachmentCount	= 1;
+		colorBlending.pAttachments		= &colourBlendAttachment;
+		colorBlending.blendConstants[0] = 0.0f;
+		colorBlending.blendConstants[1] = 0.0f;
+		colorBlending.blendConstants[2] = 0.0f;
+		colorBlending.blendConstants[3] = 0.0f;
+
 		//Pipeline Layout
 		//TODO: pipeline layout
 		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
