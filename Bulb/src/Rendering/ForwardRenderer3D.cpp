2208b2359aff4b9076d0246da9f3b8ebbcfc77d3
Alex <wolfplayeral@yahoo.co.uk>
2020-06-06 17:04:31 +0100

    Forward renderer now creates a pipeline

--- Bulb/src/Rendering/ForwardRenderer3D.cpp
+++ Bulb/src/Rendering/ForwardRenderer3D.cpp
@@ -6,18 +6,29 @@
 #include <Clove/Platform/Window.hpp>
 
 namespace blb::rnd {
-	ForwardRenderer3D::ForwardRenderer3D(clv::plt::Window& window, const clv::gfx::API){
+	ForwardRenderer3D::ForwardRenderer3D(clv::plt::Window& window, const clv::gfx::API) {
 		graphicsFactory = std::make_shared<clv::gfx::vk::VKGraphicsFactory>(window.getNativeWindow());
 
 		//Retrieving data as a test
 		auto graphicsQueue = graphicsFactory->createGraphicsQueue({ clv::gfx::QueueFlags::None });
-		auto presentQueue = graphicsFactory->createPresentQueue();
+		auto presentQueue  = graphicsFactory->createPresentQueue();
 		auto transferQueue = graphicsFactory->createTransferQueue({ clv::gfx::QueueFlags::Transient });
 
 		auto swapchain = graphicsFactory->createSwapChain({});
 
-		auto vertShader = graphicsFactory->createShader("vert.spirv");
-		auto fragShader = graphicsFactory->createShader("frag.spirv");
+		std::shared_ptr<clv::gfx::vk::VKShader> vertShader = graphicsFactory->createShader("vert.spirv");
+		std::shared_ptr<clv::gfx::vk::VKShader> fragShader = graphicsFactory->createShader("frag.spirv");
+
+		std::shared_ptr<clv::gfx::vk::VKRenderPass> renderPass = graphicsFactory->createRenderPass(*swapchain, {});
+
+		clv::gfx::PiplineObjectDescriptor pipelineDescriptor;
+		pipelineDescriptor.vertexShader			   = vertShader;
+		pipelineDescriptor.fragmentShader		   = fragShader;
+		pipelineDescriptor.viewportDescriptor.size = { swapchain->getExtent().width, swapchain->getExtent().height };
+		pipelineDescriptor.scissorDescriptor.size  = { swapchain->getExtent().width, swapchain->getExtent().height };
+		pipelineDescriptor.renderPass			   = renderPass;
+
+		auto pipelineObject = graphicsFactory->createPipelineObject(pipelineDescriptor);
 	}
 
 	ForwardRenderer3D::~ForwardRenderer3D() = default;
