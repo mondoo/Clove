d951f9717e09ad68d1ffc2ce1288af6ea7de1162
Alex <wolfplayeral@yahoo.co.uk>
2020-06-11 18:35:21 +0100

    ALFactory now creates the AL device / context

--- Clove/include/Clove/Audio/OpenAL/ALFactory.hpp
+++ Clove/include/Clove/Audio/OpenAL/ALFactory.hpp
@@ -2,11 +2,26 @@
 
 #include "Clove/Audio/AudioFactory.hpp"
 
+#include <AL/alc.h>
+
 namespace clv {
     class ALFactory : public AudioFactory {
+        //VARIABLES
+    private:
+        ALCdevice* alDevice = nullptr;
+        ALCcontext* alContext = nullptr;
+
         //FUNCTIONS
     public:
-        //TODO: Ctors
+        ALFactory();
+
+        ALFactory(const ALFactory& other) = delete;
+        ALFactory(ALFactory&& other) noexcept;
+
+        ALFactory& operator=(const ALFactory& other) = delete;
+        ALFactory& operator=(ALFactory&& other) noexcept;
+
+        ~ALFactory();
 
         std::unique_ptr<AudioBuffer> createAudioBuffer(AudioBufferDescriptor descriptor, const void* data, size_t dataSize) override;
     };
--- Clove/src/Audio/OpenAL/ALFactory.cpp
+++ Clove/src/Audio/OpenAL/ALFactory.cpp
@@ -3,6 +3,28 @@
 #include "Clove/Audio/OpenAL/ALBuffer.hpp"
 
 namespace clv {
+    ALFactory::ALFactory(){
+        alDevice = alcOpenDevice(nullptr);
+        if(!alDevice) {
+            GARLIC_LOG(garlicLogContext, Log::Level::Error, "Failed to create OpenAL device");
+            return;
+        }
+
+        alContext = alcCreateContext(alDevice, nullptr);
+        alcMakeContextCurrent(alContext);
+    }
+
+    ALFactory::ALFactory(ALFactory&& other) noexcept = default;
+
+    ALFactory& ALFactory::operator=(ALFactory&& other) noexcept = default;
+
+    ALFactory::~ALFactory(){
+        alcMakeContextCurrent(nullptr);
+        alcDestroyContext(alContext);
+
+        alcCloseDevice(alDevice);
+    }
+
     std::unique_ptr<AudioBuffer> ALFactory::createAudioBuffer(AudioBufferDescriptor descriptor, const void* data, size_t dataSize) {
         return std::make_unique<ALBuffer>(descriptor, data, dataSize);
     }
