e86a60b06f330237dd2a3f2db9ceef05af21ee03
Alex <wolfplayeral@yahoo.co.uk>
2020-06-13 12:31:08 +0100

    Added types for the pose data

--- Bulb/include/Bulb/Rendering/Renderables/SkeletalMesh.hpp
+++ Bulb/include/Bulb/Rendering/Renderables/SkeletalMesh.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 namespace blb::rnd {
+    //Skeleton Data
     struct Joint {
         clv::mth::mat4f inverseBindPose;
         uint8_t parentIndex;
@@ -8,6 +9,20 @@ namespace blb::rnd {
 
     struct Skeleton {
         std::vector<Joint> joints;
+    };
+
+    //Pose Data
+    struct JointPose {
+        clv::mth::quatf rotation;
+        clv::mth::vec4f position;
+        clv::mth::vec4f scale;
+    };
+
+    struct SkeletonPose {
+        Skeleton* skeleton;
+        //One item for every joint in the skeleton
+        std::vector<JointPose> localPoses;
+        std::vector<clv::mth::mat4f> globalPoses;
     };
 }
 
