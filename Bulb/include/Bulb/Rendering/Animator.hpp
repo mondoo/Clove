aae4b65804710ef23e9b244faca5ccefd45ef809
Alex <wolfplayeral@yahoo.co.uk>
2020-06-13 13:11:54 +0100

    Set up the basic types that the animator will need

--- Bulb/include/Bulb/Rendering/Animator.hpp
+++ Bulb/include/Bulb/Rendering/Animator.hpp
@@ -1,5 +1,20 @@
 #pragma once
 
+//Temp:
+#include "Bulb/Rendering/Renderables/SkeletalMesh.hpp"
+
+namespace blb::rnd {
+    struct AnimationPose {
+        std::vector<JointPose> poses;
+    };
+
+    struct AnimationClip {
+        Skeleton* skeleton;
+        float duration;
+        std::vector<AnimationPose> poses;
+    };
+}
+
 namespace blb::rnd {
     class Animator {
     };
