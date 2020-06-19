b3adab937387be5b39cdacdd950324bf9bd586c3
Alex <wolfplayeral@yahoo.co.uk>
2020-06-19 08:07:59 +0100

    Quickly set up the rendering of the model in the scene

--- Bulb/Examples/SkeletalAnimation/main.cpp
+++ Bulb/Examples/SkeletalAnimation/main.cpp
@@ -11,6 +11,24 @@ int main() {
     //Cache of the 'previous frame time' that'll be used to calculate the delta time
     auto prevFrameTime = std::chrono::system_clock::now();
 
+    auto model    = blb::ModelLoader::loadModel(SOURCE_DIR "/CharacterRunning.fbx", window->getGraphicsFactory());
+    auto renderer = std::make_shared<blb::rnd::Renderer3D>(*window);
+    auto camera   = blb::rnd::Camera{ *window, blb::rnd::ProjectionMode::Perspective };
+
+    static constexpr float size     = 50.0f;
+    static constexpr float nearDist = 0.5f;
+    static constexpr float farDist  = 1000.0f;
+
+    blb::rnd::DirectionalLightData dirLightData;
+    dirLightData.direction = { 0.0f, -1.0f, 1.0f };
+    dirLightData.ambient   = { 0.01f, 0.01f, 0.01f };
+    dirLightData.diffuse   = { 0.75f, 0.75f, 0.75f };
+    dirLightData.specular  = { 1.0f, 1.0f, 1.0f };
+
+    blb::rnd::DirectionalLight dirLight;
+    dirLight.data            = std::move(dirLightData);
+    dirLight.shadowTransform = clv::mth::createOrthographicMatrix(-size, size, -size, size, nearDist, farDist);
+
     //Run our program while the window is open
     while(window->isOpen()) {
         //Calculate the delta time
@@ -21,7 +39,33 @@ int main() {
         //Tell our window we're beginning a frame so it can handle any input events
         window->beginFrame();
 
-        //TODO...
+        renderer->begin();
+
+        //Camera
+        const clv::mth::vec3f position{ 0.0f, 5.0f, -10.0f };
+        const clv::mth::vec3f camFront{ 0.0f, 0.0f, 1.0f };
+        const clv::mth::vec3f camUp{ 0.0f, 1.0f, 0.0f };
+        const clv::mth::mat4f lookAt = clv::mth::lookAt(position, position + camFront, camUp);
+        camera.setView(lookAt);
+        blb::rnd::CameraRenderData renderData{};
+        renderData.lookAt     = lookAt;
+        renderData.position   = position;
+        renderData.projection = camera.getProjection();
+        renderer->submitCamera({ camera.getViewport(), std::move(renderData), camera.getRenderTarget() });
+
+        //Light
+        renderer->submitLight(dirLight);
+
+        //Mesh
+        for(auto& mesh : model.getMeshes()) {
+            auto rot = clv::mth::rotate(clv::mth::mat4f{ 1.0f }, clv::mth::asRadians(-90.0f), { 1.0f, 0.0f, 0.0f });
+            rot = clv::mth::rotate(rot, clv::mth::asRadians(90.0f), { 0.0f, 0.0f, 1.0f });
+
+            mesh->getMaterialInstance().setData(clv::gfx::BBP_ModelData, blb::rnd::VertexData{ rot, clv::mth::transpose(clv::mth::inverse(rot)) }, clv::gfx::ShaderStage::Vertex);
+            renderer->submitMesh(mesh);
+        }
+
+        renderer->end();
 
         //Tell our window we've finished frame so it can present it's back buffer
         window->endFrame();
