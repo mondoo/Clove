4d82b5f74c1e5875e675a4ec3c8905eaf3ffdc40
Alex <wolfplayeral@yahoo.co.uk>
2020-06-20 18:45:53 +0100

    Added function definition for loading an animated model

--- Bulb/include/Bulb/ModelLoader.hpp
+++ Bulb/include/Bulb/ModelLoader.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Bulb/Rendering/Renderables/Model.hpp"
+#include "Bulb/Rendering/Renderables/SkeletalMesh.hpp"
 
 namespace clv::gfx{
 	class GraphicsFactory;
@@ -8,4 +9,5 @@ namespace clv::gfx{
 
 namespace blb::ModelLoader {
 	rnd::Model loadModel(std::string_view modelFilePath, const std::shared_ptr<clv::gfx::GraphicsFactory>& graphicsFactory);
+    rnd::SkeletalMesh loadAnimatedModel(std::string_view modelFilePath, const std::shared_ptr<clv::gfx::GraphicsFactory>& graphicsFactory);
 }
