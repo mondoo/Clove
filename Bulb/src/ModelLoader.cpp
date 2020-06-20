279bfd5fd7d6aa2a4f4b2d3be2378e70fc3325a8
Alex <wolfplayeral@yahoo.co.uk>
2020-06-20 18:45:10 +0100

    Loaded the inverse bind pose from assimp

--- Bulb/src/ModelLoader.cpp
+++ Bulb/src/ModelLoader.cpp
@@ -12,6 +12,32 @@
 using namespace clv;
 
 namespace blb::ModelLoader {
+	static mth::mat4f convertToGarlicMatrix(const aiMatrix4x4& aiMat){
+        mth::mat4f garlicMat;
+
+        garlicMat[0][0] = aiMat.a1;
+        garlicMat[1][0] = aiMat.b1;
+        garlicMat[2][0] = aiMat.c1;
+        garlicMat[3][0] = aiMat.d1;
+
+        garlicMat[0][1] = aiMat.a2;
+        garlicMat[1][1] = aiMat.b2;
+        garlicMat[2][1] = aiMat.c2;
+        garlicMat[3][1] = aiMat.d2;
+
+        garlicMat[0][2] = aiMat.a3;
+        garlicMat[1][2] = aiMat.b3;
+        garlicMat[2][2] = aiMat.c3;
+        garlicMat[3][2] = aiMat.d3;
+
+        garlicMat[0][3] = aiMat.a4;
+        garlicMat[1][3] = aiMat.b4;
+        garlicMat[2][3] = aiMat.c4;
+        garlicMat[3][3] = aiMat.d4;
+
+        return garlicMat;
+	}
+
 	static std::shared_ptr<gfx::Texture> loadMaterialTexture(aiMaterial* material, aiTextureType type, const std::shared_ptr<clv::gfx::GraphicsFactory>& graphicsFactory) {
 		std::shared_ptr<gfx::Texture> texture;
 
@@ -120,5 +146,51 @@ namespace blb::ModelLoader {
         }
 
 		return { meshes };
+	}
+
+	rnd::SkeletalMesh loadAnimatedModel(std::string_view modelFilePath, const std::shared_ptr<clv::gfx::GraphicsFactory>& graphicsFactory){
+        CLV_PROFILE_FUNCTION();
+
+        std::vector<std::shared_ptr<rnd::Mesh>> meshes;
+
+        Assimp::Importer importer;
+        const aiScene* scene = importer.ReadFile(modelFilePath.data(), aiProcess_Triangulate | aiProcess_FlipUVs);
+        if(scene == nullptr || (scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE) || scene->mRootNode == nullptr) {
+            GARLIC_LOG(garlicLogContext, Log::Level::Error, "Assimp Error: {0}", importer.GetErrorString());
+            return { meshes };
+        }
+
+		for(size_t i = 0; i < scene->mNumMeshes; ++i) {
+            aiMesh* mesh = scene->mMeshes[i];
+            meshes.emplace_back(processMesh(mesh, scene, graphicsFactory));
+        }
+
+		rnd::SkeletalMesh animatedModel{ meshes };
+
+		//TODO: Support multiple skeletons?
+		rnd::Skeleton skeleton;
+
+		//Go through bones
+        for(size_t i = 0; i < scene->mNumMeshes; ++i) {
+            aiMesh* mesh = scene->mMeshes[i];
+            if(mesh->mNumBones <= 0) {
+                continue;
+			}
+
+            skeleton.joints.resize(mesh->mNumBones);
+            for(size_t i = 0; i < mesh->mNumBones; i++) {
+                aiBone* bone = mesh->mBones[i];
+
+				//TODO: Parent
+				skeleton.joints[i].inverseBindPose = convertToGarlicMatrix(bone->mOffsetMatrix);
+
+				//TODO: Get weights, have to index back into the vertex buffer
+			}
+
+			//Just doing one skeleton for now
+			break;
+        }
+
+        return animatedModel;
 	}
 }
