7e748972c3d767a3a79d37a93a979fe4460056d3
Alex <wolfplayeral@yahoo.co.uk>
2020-06-20 18:45:32 +0100

    SkeletalMesh inherits from model

--- Bulb/include/Bulb/Rendering/Renderables/SkeletalMesh.hpp
+++ Bulb/include/Bulb/Rendering/Renderables/SkeletalMesh.hpp
@@ -1,22 +1,17 @@
 #pragma once
 
-#include "Bulb/Rendering/Renderables/Mesh.hpp"
+#include "Bulb/Rendering/Renderables/Model.hpp"
 #include "Bulb/Rendering/AnimationTypes.hpp"
 #include "Bulb/Rendering/Animator.hpp"
 
-//TODO: Support submeshes (probably convert this into a model)
-
 namespace blb::rnd {
-    class SkeletalMesh {
+    class SkeletalMesh : public Model { // TODO: Rename to AnimatedModel
         //VARIABLES
    // private:
     public:
         //Temp, owning the skeleton for now but when using submeshes it will make sense to have this stored somewhere else. Which also allow skeletons to be reused
         Skeleton skeleton;
         Animator animator; //TODO: not sure if this should live here
-
-        //TEMP: just storing a mesh now to reduce code duplication
-        std::shared_ptr<Mesh> mesh;
 
         //FUNCTIONS
     public:
