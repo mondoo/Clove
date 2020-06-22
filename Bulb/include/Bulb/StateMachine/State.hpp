c095f26ecbb9c7bfa7d18c16dd8741c0d7614a54
Alex <wolfplayeral@yahoo.co.uk>
2020-06-22 22:00:43 +0100

    Added Doxygen comments for the StateMachine

--- Bulb/include/Bulb/StateMachine/State.hpp
+++ Bulb/include/Bulb/StateMachine/State.hpp
@@ -2,6 +2,9 @@ namespace blb {
 	template<typename Action>
 	class Transition;
 
+	/**
+	 * @brief A helper struct to avoid cyclic dependencies when initialising States
+	 */
 	template<typename Action>
 	struct StateInitialiser {
 		std::vector<std::unique_ptr<Action>> entryActions;
@@ -13,6 +16,17 @@ namespace blb {
 }
 
 namespace blb {
+	/**
+	 * @brief A State is a State that a StateMachine can currently be in.
+	 *
+	 * A State is comprised of entry Actions, Actions and exit Actions. These are certain behaviours
+	 * that are to be executed when entering this State, updating this State and exiting this State.
+	 * How this is done is governed by a list of Transitions
+	 *
+	 * @tparam Action The type of Action this State deals with
+	 *
+	 * @see Transition
+	 */
 	template<typename Action>
 	class State {
 		//VARIABLES
--- Bulb/include/Bulb/StateMachine/StateMachine.hpp
+++ Bulb/include/Bulb/StateMachine/StateMachine.hpp
@@ -6,6 +6,11 @@ namespace blb{
 }
 
 namespace blb {
+	/**
+	 * @brief A StateMachine manages States and Transitions. Determining what Action to execute.
+	 *
+	 * @tparam Action The type of Action this StateMachine handles
+	 */
 	template<typename Action>
 	class StateMachine {
 		//VARIABLES
@@ -25,6 +30,11 @@ namespace blb {
 
 		~StateMachine();
 
+		/**
+		 * @brief Runs through the current Action to test for any Transitions that were activated
+		 *
+		 * @return A vector of Actions to perform
+		 */
 		std::vector<Action*> update();
 	};
 }
