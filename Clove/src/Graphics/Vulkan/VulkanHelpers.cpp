2b94ced23f7b8fac0a4a4110adef4aeb984d0f06
Alex <wolfplayeral@yahoo.co.uk>
2020-06-22 07:51:34 +0100

    Unkown formats will now assert

--- Clove/src/Graphics/Vulkan/VulkanHelpers.cpp
+++ Clove/src/Graphics/Vulkan/VulkanHelpers.cpp
@@ -30,7 +30,7 @@ namespace clv::gfx::vk {
 			case VK_FORMAT_B8G8R8A8_UNORM:
 				return ImageFormat::B8G8R8A8_UNORM;
 			default:
-				GARLIC_LOG(garlicLogContext, Log::Level::Error, "Format not supported by garlic");
+				CLV_ASSERT(false, "Format not supported by garlic");
 				return ImageFormat::Unkown;
 		}
 	}
