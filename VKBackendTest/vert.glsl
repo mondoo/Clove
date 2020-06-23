b0163e4258e1649a9725a5b80634189f067ed780
Alex <wolfplayeral@yahoo.co.uk>
2020-06-23 07:31:08 +0100

    Changed the vertex shader to expect vertex attributes

--- VKBackendTest/vert.glsl
+++ VKBackendTest/vert.glsl
@@ -1,20 +1,11 @@
 #version 450
 
+layout(location = 0) in vec2 position;
+layout(location = 1) in vec3 colour;
+
 layout(location = 0) out vec3 fragColour;
 
-vec2 positions[3] = vec2[](
-	vec2(0.0f, -0.5f),
-	vec2(0.5f, 0.5f),
-	vec2(-0.5f, 0.5f)
-);
-
-vec3 colours[3] = vec3[](
-	vec3(1.0f, 0.0f, 0.0f),
-	vec3(0.0f, 1.0f, 0.0f),
-	vec3(0.0f, 0.0f, 1.0f)
-);
-
 void main(){
-	gl_Position = vec4(positions[gl_VertexIndex], 0.0f, 1.0f);
-	fragColour = colours[gl_VertexIndex];
+	gl_Position = vec4(position, 0.0f, 1.0f);
+	fragColour = colour;
 }
