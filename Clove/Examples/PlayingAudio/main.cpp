0e070be4f52e7fc993c5c0a4130e4ae5187ecc86
Alex <wolfplayeral@yahoo.co.uk>
2020-06-24 07:44:13 +0100

    Can now play mono sounds

--- Clove/Examples/PlayingAudio/main.cpp
+++ Clove/Examples/PlayingAudio/main.cpp
@@ -1,9 +1,30 @@
 #include <Clove/Clove.hpp>
 
-#include "LoadWav.hpp"
+#include "AudioFile.h"
 
 //An example on how to play audio through Clove
 
 int main(){
+	auto audioFactory = clv::createAudioFactory(clv::AudioAPI::OpenAl);
 
+	AudioFile<double> file;
+	file.load(SOURCE_DIR "/beep.wav");
+
+	clv::AudioBufferDescriptor descriptor{};
+	if(file.getNumChannels() == 1){
+		descriptor.format = file.getBitDepth() == 8 ? clv::BufferFormat::Mono8 : clv::BufferFormat::Mono16;
+	}else{
+		descriptor.format = file.getBitDepth() == 8 ? clv::BufferFormat::Stereo8 : clv::BufferFormat::Stereo16;
+	}
+	descriptor.sampleRate = file.getSampleRate();
+
+	auto audioBuffer = audioFactory->createAudioBuffer(descriptor, file.samples[0].data(), file.samples[0].size()); //TODO: Use both channels
+	auto audioSource = audioFactory->createAudioSource();
+
+	audioSource->setBuffer(*audioBuffer);
+
+	audioSource->setLooping(true);
+	audioSource->play();
+
+	while(true);
 }
