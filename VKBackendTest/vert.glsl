87d9906ae4d7355c8606f0f96c060ea59513c0b0
Alex <wolfplayeral@yahoo.co.uk>
2020-07-06 07:44:46 +0100

    Updated the vertex shader to expect a UBO

--- VKBackendTest/vert.glsl
+++ VKBackendTest/vert.glsl
@@ -1,11 +1,17 @@
 #version 450
 
+layout(binding = 0) uniform ModelViewProj{
+	mat4 model;
+	mat4 view;
+	mat4 proj;
+} mvp;
+
 layout(location = 0) in vec2 position;
 layout(location = 1) in vec3 colour;
 
 layout(location = 0) out vec3 fragColour;
 
 void main(){
-	gl_Position = vec4(position, 0.0f, 1.0f);
+	gl_Position = mvp.proj * mvp.view * mvp.model * vec4(position, 0.0f, 1.0f);
 	fragColour = colour;
 }
