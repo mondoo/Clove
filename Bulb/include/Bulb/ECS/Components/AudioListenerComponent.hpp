a29ea039d6a6ace79e9049ab1ee77f5a0da834fc
Alex <wolfplayeral@yahoo.co.uk>
2020-07-09 07:15:06 +0100

    Listener component is now defined

--- Bulb/include/Bulb/ECS/Components/AudioListenerComponent.hpp
+++ Bulb/include/Bulb/ECS/Components/AudioListenerComponent.hpp
@@ -1,9 +1,31 @@
 #pragma once
 
-#include "Bulb/ECS/Component.hpp
+#include "Bulb/ECS/Component.hpp"
+
+namespace clv {
+    class AudioFactory;
+    class AudioListener;
+}
 
 namespace blb::ecs {
     class AudioListenerComponent : public Component<AudioListenerComponent> {
-        //TODO
+        friend class AudioSystem;
+
+        //VARIABLES
+    private:
+        std::unique_ptr<clv::AudioListener> listener;
+
+        //FUNCTIONS
+    public:
+        AudioListenerComponent() = delete;
+        AudioListenerComponent(clv::AudioFactory& factory);
+
+        AudioListenerComponent(const AudioListenerComponent& other) = delete;
+        AudioListenerComponent(AudioListenerComponent&& other) noexcept;
+
+        AudioListenerComponent& operator=(const AudioListenerComponent& other) = delete;
+        AudioListenerComponent& operator=(AudioListenerComponent&& other) noexcept;
+
+        ~AudioListenerComponent();
     };
 }
