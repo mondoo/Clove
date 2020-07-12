81787b9ec69cfdc51ac089691391dbeb98fa7690
Alex <wolfplayeral@yahoo.co.uk>
2020-07-12 21:13:08 +0100

    Commented the examples for playing audio

--- Bulb/Examples/PlayingAudio/main.cpp
+++ Bulb/Examples/PlayingAudio/main.cpp
@@ -1,5 +1,8 @@
 #include <Bulb/Bulb.hpp>
 
+//Demonstrates how to play audio in Bulb / Clove
+
+//Helper funcntion for converting a SoundFile format into an AudioBuffer format
 static clv::AudioBuffer::Format getBufferFormat(const blb::aud::SoundFile& file) {
     const bool is16 = file.getFormat() == blb::aud::SoundFile::Format::S16;
 
@@ -11,26 +14,35 @@ static clv::AudioBuffer::Format getBufferFormat(const blb::aud::SoundFile& file)
 }
 
 int main() {
+	//Load our file to play audio from
     blb::aud::SoundFile file{ SOURCE_DIR "/bounce.wav" };
 
-    auto audioFactory = clv::createAudioFactory(clv::AudioAPI::OpenAl);
+	//Create an AudioFactory that uses OpenAL
+	//An AudioFactory creates all of the components needed to play audio
+    std::unique_ptr<clv::AudioFactory> audioFactory = clv::createAudioFactory(clv::AudioAPI::OpenAl);
 
+	//Read the entire file contents into a buffer
     auto [rawBuffer, bufferSize] = file.read(file.getFrames());
 
-    clv::AudioBuffer::DataInfo descriptor{};
-    descriptor.format     = getBufferFormat(file);
-    descriptor.sampleRate = file.getSampleRate();
-    descriptor.data       = rawBuffer;
-    descriptor.dataSize   = bufferSize;
-    auto audioBuffer      = audioFactory->createAudioBuffer(std::move(descriptor));
+	//Pass that data along to the AudioFactory to create an AudioBuffer from
+    clv::AudioBuffer::DataInfo info{};
+    info.format     = getBufferFormat(file);
+    info.sampleRate = file.getSampleRate();
+    info.data       = rawBuffer;
+    info.dataSize   = bufferSize;
+	
+	std::unique_ptr<clv::AudioBuffer> audioBuffer = audioFactory->createAudioBuffer(std::move(info));
 
+	//Delete the rawBuffer obtained from the file
     delete rawBuffer;
 
-    auto audioSource = audioFactory->createAudioSource();
+	//Create an AudioSource to configure where audio is played from
+    std::unique_ptr<clv::AudioSource> audioSource = audioFactory->createAudioSource();
     audioSource->setBuffer(*audioBuffer);
     audioSource->setLooping(true);
     audioSource->play();
 
+	//Get previous frame time to calculate the delta between frames
     auto prevFrameTime = std::chrono::system_clock::now();
     while(true) {
         auto currFrameTime                        = std::chrono::system_clock::now();
@@ -40,6 +52,7 @@ int main() {
         static float total = 0.0f;
         total += deltaSeconds.count();
 
+		//Move the audio source right to left
         float x = sin(total) * 10.0f;
         audioSource->setPosition({ x, 0.0f, 3.0f });
 
--- Bulb/Examples/StreamingAudio/main.cpp
+++ Bulb/Examples/StreamingAudio/main.cpp
@@ -1,5 +1,8 @@
 #include <Bulb/Bulb.hpp>
 
+//Demonstrates how to stream audio in Bulb / Clove
+
+//Helper funcntion for converting a SoundFile format into an AudioBuffer format
 static clv::AudioBuffer::Format getBufferFormat(const blb::aud::SoundFile& file) {
     const bool is16 = file.getFormat() == blb::aud::SoundFile::Format::S16;
 
@@ -11,21 +14,27 @@ static clv::AudioBuffer::Format getBufferFormat(const blb::aud::SoundFile& file)
 }
 
 int main() {
+	//Load our file to play audio from
     blb::aud::SoundFile file{ SOURCE_DIR "/countdown.wav" };
 
-    auto audioFactory = clv::createAudioFactory(clv::AudioAPI::OpenAl);
+	//Create an AudioFactory that uses OpenAL
+	//An AudioFactory creates all of the components needed to play audio
+    std::unique_ptr<clv::AudioFactory> audioFactory = clv::createAudioFactory(clv::AudioAPI::OpenAl);
 
+	//Create 2 empty buffers. One will be playing audio and the other will
+	//have the next section of audio data read into
     std::shared_ptr<clv::AudioBuffer> bufferA = audioFactory->createAudioBuffer();
     std::shared_ptr<clv::AudioBuffer> bufferB = audioFactory->createAudioBuffer();
 
-    auto audioSource = audioFactory->createAudioSource();
+	//Create the AudioSource to control where the sound is played from
+    std::unique_ptr<clv::AudioSource> audioSource = audioFactory->createAudioSource();
     
     //Stream with 2 buffers each having a quater of the file
     const uint32_t framesPerBuffer = file.getFrames() / 4;
 
     //Get some data set up initially
     {
-        //Reading also seeks the file's cursor along along
+        //Reading also seeks the file's cursor along
         auto [bufferAData, bufferASize] = file.read(framesPerBuffer);
         auto [bufferBData, bufferBSize] = file.read(framesPerBuffer);
 
@@ -47,12 +56,14 @@ int main() {
             bufferB->map(std::move(bufferData));
         }
 
+		//Clean up raw data after usage
         delete bufferAData;
         delete bufferBData;
     }
 
+	//Put our buffers into the AudioSource's internal queue
     audioSource->queueBuffers({ bufferA, bufferB });
-
+	//Start plating the queue
     audioSource->play();
 
     auto prevFrameTime = std::chrono::system_clock::now();
@@ -69,7 +80,8 @@ int main() {
             break;
         }
 
-        //Update buffers
+        //While we have a buffer that has been processed. Load up the
+		//next chunk of data and then queue that buffer
         uint32_t numProcessed = audioSource->getNumBuffersProcessed();
         if(numProcessed > 0) {
             while(numProcessed-- > 0) {
