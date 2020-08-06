cdcf39a21ed963d4cf672b9a30f17bd26a5ba4ff
Alex <wolfplayeral@yahoo.co.uk>
2020-08-06 21:36:58 +0100

    Fixed the platform prefered types

--- Clove/include/Clove/Platform/PlatformTypes.hpp
+++ Clove/include/Clove/Platform/PlatformTypes.hpp
@@ -5,11 +5,11 @@
 namespace clv::plt{
 	constexpr clv::gfx::API getPlatformPreferedAPI(){
 	#if GARLIC_PLATFORM_WINDOWS
-		return clv::gfx::API::Direct3D11;
+		return clv::gfx::API::Vulkan;
 	#elif GARLIC_PLATFORM_LINUX
-		return clv::gfx::API::OpenGL4;
+        return clv::gfx::API::Vulkan;
 	#elif GARLIC_PLATFORM_MACOS
-		return clv::gfx::API::Metal1;
+		return clv::gfx::API::None;
 	#endif
 	}
 
