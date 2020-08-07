35bddfab3e81e10b73b878fd0f16924c189e74a0
Alex <wolfplayeral@yahoo.co.uk>
2020-08-07 07:06:06 +0100

    Turned the Renderer class into an interface

--- Bulb/include/Bulb/Rendering/Renderer3D.hpp
+++ Bulb/include/Bulb/Rendering/Renderer3D.hpp
@@ -3,127 +3,43 @@
 #include "Bulb/Rendering/ShaderBufferTypes.hpp"
 
 #include <Clove/Graphics/GraphicsTypes.hpp>
-//#include <Clove/Graphics/VertexLayout.hpp>
 
 namespace clv::gfx {
-	class GraphicsFactory;
-	class CommandBuffer;
-	class Surface;
-	class RenderTarget;
-	class PipelineObject;
-	class Texture;
-	class Buffer;
-}
-
-namespace clv::plt {
-	class Window;
+    class GraphicsBuffer;
+    class GraphicsImageView;
 }
 
 namespace blb::rnd {
-	class Sprite;
-	class Mesh;
-    class AnimatedModel;
+    class Mesh;
+    class Sprite;
 }
 
 namespace blb::rnd {
-	class Renderer3D {
-		//TYPES
-	public:
-		struct ComposedCameraData {
-			clv::gfx::Viewport viewport;
-			CameraRenderData bufferData;
-			std::shared_ptr<clv::gfx::RenderTarget> target;
-		};
+    class Renderer3D {
+        //TYPES
+    public:
+        struct ComposedCameraData {
+            clv::gfx::Viewport viewport;
+            CameraRenderData bufferData;
+            //std::shared_ptr<clv::gfx::RenderTarget> target;
+        };
 
-	private:
-		struct SceneData {
-			//VARIABLES
-		public:
-			std::vector<ComposedCameraData> cameras;
+        //FUNCTIONS
+    public:
+        virtual void begin() = 0;
 
-			std::vector<std::shared_ptr<rnd::Mesh>> meshes;
-            std::vector<std::shared_ptr<rnd::Mesh>> animatedMeshes;
+        virtual void submitPrimitive(const std::shared_ptr<clv::gfx::GraphicsBuffer>& vertexBuffer, const std::shared_ptr<clv::gfx::GraphicsBuffer>& indexBuffer, const clv::mth::mat4f& transform) = 0;
+        virtual void submitQuad(const clv::mth::mat4f& transform, const clv::mth::vec4f& colour)                                                                                                    = 0;
+        virtual void submitQuad(const clv::mth::mat4f& transform, const std::shared_ptr<clv::gfx::GraphicsImageView>& texture)                                                                      = 0;
 
-			int32_t numDirectionalLights = 0u;
-			int32_t numPointLights = 0u;
+        virtual void submitMesh(const std::shared_ptr<Mesh>& mesh) = 0;
+        virtual void submitLight(const DirectionalLight& light)         = 0;
+        virtual void submitLight(const PointLight& light)               = 0;
+        virtual void submitCamera(const ComposedCameraData& camera)     = 0;
 
-			LightDataArray lightDataArray;
+        virtual void submitWidget(const std::shared_ptr<Sprite>& widget) = 0;
+        virtual void submitText(const std::shared_ptr<Sprite>& text)     = 0;
 
-			std::array<std::array<clv::mth::mat4f, 6>, MAX_LIGHTS> pointShadowTransformArray{};
-			DirectionalShadowTransformArray directionalShadowTransformArray{};
-
-			std::vector<std::shared_ptr<Sprite>> widgetsToRender;
-			std::vector<std::shared_ptr<Sprite>> textToRender;
-
-			//FUNCTIONS
-		public:
-			SceneData();
-
-			~SceneData();
-		};
-
-		//VARIABLES
-	private:
-		std::shared_ptr<clv::gfx::RenderTarget> directionalShadowRenderTarget;
-		std::shared_ptr<clv::gfx::RenderTarget> pointShadowRenderTarget;
-
-		std::shared_ptr<clv::gfx::PipelineObject> directionalShadowPipelineObject;
-        std::shared_ptr<clv::gfx::PipelineObject> animatedDirectionalShadowPipelineObject;
-
-		std::shared_ptr<clv::gfx::PipelineObject> pointShadowPipelineObject;
-        std::shared_ptr<clv::gfx::PipelineObject> animatedPointShadowPipelineObject;
-
-        std::shared_ptr<clv::gfx::PipelineObject> meshPipelineObject;
-        std::shared_ptr<clv::gfx::PipelineObject> animatedMeshPipelineObject;
-
-		std::shared_ptr<clv::gfx::PipelineObject> widgetPipelineObject;
-		std::shared_ptr<clv::gfx::PipelineObject> textPipelineObject;
-
-		std::shared_ptr<clv::gfx::CommandBuffer> directionalShadowCommandBuffer;
-		std::shared_ptr<clv::gfx::CommandBuffer> pointShadowCommandBuffer;
-		std::shared_ptr<clv::gfx::CommandBuffer> meshCommandBuffer;
-		std::shared_ptr<clv::gfx::CommandBuffer> uiCommandBuffer;
-
-		std::shared_ptr<clv::gfx::Texture> directionalShadowMapTexture;
-		std::shared_ptr<clv::gfx::Texture> pointShadowMapTexture;
-
-		std::shared_ptr<clv::gfx::Buffer> viewBuffer;
-		std::shared_ptr<clv::gfx::Buffer> viewPosition;
-
-		std::shared_ptr<clv::gfx::Buffer> widgetVB;
-		std::shared_ptr<clv::gfx::Buffer> textVB;
-		std::shared_ptr<clv::gfx::Buffer> uiIndexBuffer;
-
-		std::shared_ptr<clv::gfx::Buffer> lightArrayBuffer;
-		std::shared_ptr<clv::gfx::Buffer> lightDepthBuffer;
-		std::shared_ptr<clv::gfx::Buffer> lightNumBuffer;
-
-		std::shared_ptr<clv::gfx::Buffer> directionalShadowTransformBuffer;
-		std::shared_ptr<clv::gfx::Buffer> directionalShadowTransformArrayBuffer;
-		std::shared_ptr<clv::gfx::Buffer> pointShadowTransformBuffer;
-		std::shared_ptr<clv::gfx::Buffer> lightIndexBuffer;
-		std::shared_ptr<clv::gfx::Buffer> shadowLightPosBuffer;
-
-		/*clv::gfx::VertexLayout uiVbLayout;*/
-
-		SceneData scene;
-
-		//FUNCTIONS
-	public:
-		Renderer3D(clv::plt::Window& window);
-		//TODO: ctors
-
-		void begin();
-
-		void submitStaticMesh(const std::shared_ptr<rnd::Mesh>& mesh);
-        void submitAnimatedMesh(const std::shared_ptr<rnd::Mesh>& mesh);
-		void submitLight(const DirectionalLight& light);
-		void submitLight(const PointLight& light);
-		void submitCamera(const ComposedCameraData& camera);
-
-		void submitWidget(const std::shared_ptr<Sprite>& widget);
-		void submitText(const std::shared_ptr<Sprite>& text);
-
-		void end();
-	};
+        virtual void end() = 0;
+    };
 }
