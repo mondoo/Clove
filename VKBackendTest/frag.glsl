eca32985e15a4f37c5af38cbd1141b74d27c92cd
Alex <wolfplayeral@yahoo.co.uk>
2020-08-10 07:35:15 +0100

    Test shader now renderers the texture normally

--- VKBackendTest/frag.glsl
+++ VKBackendTest/frag.glsl
@@ -8,5 +8,5 @@ layout(location = 1) in vec2 fragTexCoord;
 layout(location = 0) out vec4 outColour;
 
 void main(){
-	outColour = vec4(fragColour * texture(texSampler, fragTexCoord * 2.0f).rbg, 1.0f);
+	outColour = vec4(texture(texSampler, fragTexCoord));
 }
