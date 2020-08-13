78f2a83dd1d3a582689bbbd5948c82a1ba53c472
Alex <wolfplayeral@yahoo.co.uk>
2020-08-13 07:02:59 +0100

    Removed commented code

--- Clove/src/Graphics/Vulkan/VulkanHelpers.cpp
+++ Clove/src/Graphics/Vulkan/VulkanHelpers.cpp
@@ -263,30 +263,4 @@ namespace clv::gfx::vk {
                 return VK_INDEX_TYPE_UINT16;
         }
     }
-
-    /*std::pair<VkAccessFlags, VkAccessFlags> getAccessFlags(VkImageLayout oldLayout, VkImageLayout newLayout) {
-        if(oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
-            return { 0, VK_ACCESS_TRANSFER_WRITE_BIT };
-        } else if(oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
-            return { VK_ACCESS_TRANSFER_WRITE_BIT, VK_ACCESS_SHADER_READ_BIT };
-        } else if(oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
-            return { 0, VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT };
-        } else {
-            GARLIC_ASSERT(false, "{0}: Unhandled layout transition", GARLIC_FUNCTION_NAME);
-            return { 0, 0 };
-        }
-    }
-
-    std::pair<VkPipelineStageFlags, VkPipelineStageFlags> getStageFlags(VkImageLayout oldLayout, VkImageLayout newLayout) {
-        if(oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
-            return { VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT };
-        } else if(oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
-            return { VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT };
-        } else if(oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
-            return { VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT };
-        } else {
-            GARLIC_ASSERT(false, "{0}: Unhandled layout transition", GARLIC_FUNCTION_NAME);
-            return { 0, 0 };
-        }
-    }*/
 }
