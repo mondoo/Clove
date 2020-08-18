5dc2b518f85fc5a6f8bf2642db11bcbf973140a6
Alex <wolfplayeral@yahoo.co.uk>
2020-08-18 14:38:05 +0100

    Moved the glsl shaders into Bulb

---
+++ Bulb/res/Shaders/vert.glsl
@@ -0,0 +1,20 @@
+#version 450
+
+layout(binding = 0) uniform ModelViewProj{
+	mat4 model;
+	mat4 view;
+	mat4 proj;
+} mvp;
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 colour;
+layout(location = 2) in vec2 texCoord;
+
+layout(location = 0) out vec3 fragColour;
+layout(location = 1) out vec2 fragTexCoord;
+
+void main(){
+	gl_Position = mvp.proj * mvp.view * mvp.model * vec4(position, 1.0f);
+	fragColour = colour;
+	fragTexCoord = texCoord;
+}
