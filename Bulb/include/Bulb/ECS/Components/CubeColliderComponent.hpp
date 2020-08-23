d4c4ef20786fc91d1889230ee94553455df60355
Alex <wolfplayeral@yahoo.co.uk>
2020-08-23 19:49:39 +0100

    CubeColliderComponent now initialises it's collision shape / object

--- Bulb/include/Bulb/ECS/Components/CubeColliderComponent.hpp
+++ Bulb/include/Bulb/ECS/Components/CubeColliderComponent.hpp
@@ -2,7 +2,7 @@
 
 #include "Bulb/ECS/Component.hpp"
 
-class btCollisionShape;
+class btBoxShape;
 class btCollisionObject;
 
 namespace blb::ecs {
@@ -10,15 +10,16 @@ namespace blb::ecs {
      * @brief Provides a cube collision shape for an entity.
      * @details On it's own a CubeColliderComponent can act like a trigger.
      * If an Entity also has a RigidBodyComponent then the Entity can respond
-     * to gravity and other physics events.
+     * to gravity and other physics events using this component as it's collision
+     * shape.
      */
     class CubeColliderComponent : public Component<CubeColliderComponent> {
         friend class PhysicsSystem;
 
         //VARIABLES
     private:
-        std::unique_ptr<btCollisionShape> collisionShape;
-        std::shared_ptr<btCollisionObject> collisionObject;
+        std::unique_ptr<btBoxShape> collisionShape;
+        std::shared_ptr<btCollisionObject> collisionObject; /**< Is either a btGhostObject or if there's a RigidBodyComponent a btRigidBody. */
 
         //FUNCTIONS
     public:
--- Bulb/src/ECS/Components/CubeColliderComponent.cpp
+++ Bulb/src/ECS/Components/CubeColliderComponent.cpp
@@ -1,9 +1,13 @@
 #include "Bulb/ECS/Components/CubeColliderComponent.hpp"
 
 #include <btBulletDynamicsCommon.h>
+#include <BulletCollision/CollisionDispatch/btGhostObject.h>
 
 namespace blb::ecs {
     CubeColliderComponent::CubeColliderComponent(const clv::mth::vec3f& cubeSize) {
-        //TODO
+        collisionShape = std::make_unique<btBoxShape>(btVector3{ cubeSize.x, cubeSize.y, cubeSize.z });
+        collisionObject = std::make_shared<btGhostObject>();
+
+        collisionObject->setCollisionShape(collisionShape.get());
     }
 }
