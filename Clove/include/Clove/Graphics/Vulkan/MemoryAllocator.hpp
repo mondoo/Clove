b01b4ef40cf313db77e79716ee530911de6bea6c
Alex <wolfplayeral@yahoo.co.uk>
2020-08-25 07:35:34 +0100

    Set up the class for how blocks of memory will be represented

--- Clove/include/Clove/Graphics/Vulkan/MemoryAllocator.hpp
+++ Clove/include/Clove/Graphics/Vulkan/MemoryAllocator.hpp
@@ -1,21 +1,48 @@
 #pragma once
 
+#include "Clove/Graphics/Vulkan/DevicePointer.hpp"
+
 #include <vulkan/vulkan.h>
 
 namespace clv::gfx::vk {
     class MemoryAllocator {
+        //TYPES
+    private:
+        using MemoryType = uint32_t;
+        class Block {
+            //VARIABLES
+        private:
+            VkDevice device{ VK_NULL_HANDLE };
+
+            VkDeviceMemory memory{ VK_NULL_HANDLE };
+            VkDeviceSize size{ 0 };
+
+            //FUNCTIONS
+        public:
+            Block() = delete;
+            Block(VkDevice device, VkDeviceSize size, MemoryType memoryTypeIndex);
+
+            //TODO: other ctors
+
+            ~Block();
+
+            //TODO: Move to .inl
+            inline VkDeviceSize getSize() const{
+                return size;
+            }
+        };
+
         //VARIABLES
     private:
-        /*
-        - Would probably allocate 256mb blocks at a time.
-        - Might need separate device memorys for video / system.
-        - Type of memory is chosen by buffer / image requirements so might need several of these
-        */
+        static constexpr size_t memorySize = 256 * 1024 * 1024;
 
-        VkDeviceMemory bufferMemory{ VK_NULL_HANDLE };
-        VkDeviceMemory imageMemory{ VK_NULL_HANDLE };
+        DevicePointer device;
+
+        std::unordered_map<MemoryType, Block> memoryBlocks;
 
         //FUNCTIONS
     public:
+        MemoryAllocator(DevicePointer device);
+        //TODO: ctors
     };
 }
--- Clove/src/Graphics/Vulkan/MemoryAllocator.cpp
+++ Clove/src/Graphics/Vulkan/MemoryAllocator.cpp
@@ -1,5 +1,23 @@
 #include "Clove/Graphics/Vulkan/MemoryAllocator.hpp"
 
 namespace clv::gfx::vk {
+    MemoryAllocator::Block::Block(VkDevice device, VkDeviceSize size, MemoryType memoryTypeIndex)
+        : device(device)
+        , size(size) {
+        VkMemoryAllocateInfo info{};
+        info.sType           = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        info.pNext           = nullptr;
+        info.allocationSize  = size;
+        info.memoryTypeIndex = memoryTypeIndex;
 
+        vkAllocateMemory(device, &info, nullptr, &memory);
+    }
+
+    MemoryAllocator::Block::~Block() {
+        vkFreeMemory(device, memory, nullptr);
+    }
+
+    MemoryAllocator::MemoryAllocator(DevicePointer device)
+        : device(std::move(device)) {
+    }
 }
