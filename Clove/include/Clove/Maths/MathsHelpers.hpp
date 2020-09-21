7584756e116f955a4d8aa827503f4a8a53a108e1
Alex <wolfplayeral@yahoo.co.uk>
2020-09-21 20:33:04 +0100

    Formatted the maths files

--- Clove/include/Clove/Maths/MathsHelpers.hpp
+++ Clove/include/Clove/Maths/MathsHelpers.hpp
@@ -1,56 +1,56 @@
 #pragma once
 
-namespace clv::mth{
-	template<typename T>
-	const typename T::value_type* valuePtr(const T& v);
+namespace clv::mth {
+    template<typename T>
+    const typename T::value_type* valuePtr(const T& v);
 
-	template<typename T>
-	typename T::value_type* valuePtr(T& v);
+    template<typename T>
+    typename T::value_type* valuePtr(T& v);
 
-	template<typename T>
-	constexpr T asRadians(T degrees);
+    template<typename T>
+    constexpr T asRadians(T degrees);
 
-	template<typename T>
-	constexpr T asDegrees(T radians);
+    template<typename T>
+    constexpr T asDegrees(T radians);
 
-	template<typename T>
-	constexpr T pi = glm::pi<T>();
+    template<typename T>
+    constexpr T pi = glm::pi<T>();
 
-	template<typename T>
-	mat<4, 4, T, qualifier::defaultp> createOrthographicMatrix(T left, T right, T bottom, T top);
+    template<typename T>
+    mat<4, 4, T, qualifier::defaultp> createOrthographicMatrix(T left, T right, T bottom, T top);
 
-	template<typename T>
-	mat<4, 4, T, qualifier::defaultp> createOrthographicMatrix(T left, T right, T bottom, T top, T near, T far);
+    template<typename T>
+    mat<4, 4, T, qualifier::defaultp> createOrthographicMatrix(T left, T right, T bottom, T top, T near, T far);
 
-	template<typename T>
-	mat<4, 4, T, qualifier::defaultp> createPerspectiveMatrix(T fovy, T aspect, T zNear, T zFar);
+    template<typename T>
+    mat<4, 4, T, qualifier::defaultp> createPerspectiveMatrix(T fovy, T aspect, T zNear, T zFar);
 
-	template<typename T, qualifier Q>
-	quat<T, Q> eulerToQuaternion(const vec<3, T, Q>& euler);
+    template<typename T, qualifier Q>
+    quat<T, Q> eulerToQuaternion(const vec<3, T, Q>& euler);
 
-	template<typename T, qualifier Q>
-	vec<3, T, Q> matrixToEuler(const mat<4, 4, T, Q>& mat);
+    template<typename T, qualifier Q>
+    vec<3, T, Q> matrixToEuler(const mat<4, 4, T, Q>& mat);
 
-	template<typename T, qualifier Q>
-	quat<T, Q> matrixToQuaternion(const mat<3, 3, T, Q>& mat);
+    template<typename T, qualifier Q>
+    quat<T, Q> matrixToQuaternion(const mat<3, 3, T, Q>& mat);
 
-	template<typename T, qualifier Q>
-	quat<T, Q> matrixToQuaternion(const mat<4, 4, T, Q>& mat);
+    template<typename T, qualifier Q>
+    quat<T, Q> matrixToQuaternion(const mat<4, 4, T, Q>& mat);
 
-	template<typename T, qualifier Q>
-	quat<T, Q> asQuaternion(T angle, const vec<3, T, Q>& axis);
+    template<typename T, qualifier Q>
+    quat<T, Q> asQuaternion(T angle, const vec<3, T, Q>& axis);
 
-	template<typename T, qualifier Q>
-	vec<3, T, Q> quaternionToEuler(const quat<T, Q>& quat);
+    template<typename T, qualifier Q>
+    vec<3, T, Q> quaternionToEuler(const quat<T, Q>& quat);
 
-	template<typename T, qualifier Q>
-	mat<3, 3, T, Q> quaternionToMatrix3(const quat<T, Q>& quat);
+    template<typename T, qualifier Q>
+    mat<3, 3, T, Q> quaternionToMatrix3(const quat<T, Q>& quat);
 
-	template<typename T, qualifier Q>
-	mat<4, 4, T, Q> quaternionToMatrix4(const quat<T, Q>& quat);
+    template<typename T, qualifier Q>
+    mat<4, 4, T, Q> quaternionToMatrix4(const quat<T, Q>& quat);
 
-	template<typename T, qualifier Q>
-	vec<3, T, Q> screenToWorld(const vec<2, T, Q>& screenPos, T screenDepth, const vec<2, T, Q>& screenSize, const mat<4, 4, T, Q>& viewMatrix, const mat<4, 4, T, Q>& projectionMatrix);
+    template<typename T, qualifier Q>
+    vec<3, T, Q> screenToWorld(const vec<2, T, Q>& screenPos, T screenDepth, const vec<2, T, Q>& screenSize, const mat<4, 4, T, Q>& viewMatrix, const mat<4, 4, T, Q>& projectionMatrix);
 }
 
 #include "MathsHelpers.inl"
--- Clove/include/Clove/Maths/Matrix.hpp
+++ Clove/include/Clove/Maths/Matrix.hpp
@@ -6,18 +6,18 @@
 
 #include <glm/glm.hpp>
 
-namespace clv::mth{
-	template<length_t C, length_t R, typename T, qualifier Q>
-	using mat = glm::mat<C, R, T, Q>;
+namespace clv::mth {
+    template<length_t C, length_t R, typename T, qualifier Q>
+    using mat = glm::mat<C, R, T, Q>;
 
-	using mat2x2f	= mat<2, 2, float, qualifier::defaultp>;
-	using mat2f		= mat2x2f;
+    using mat2x2f = mat<2, 2, float, qualifier::defaultp>;
+    using mat2f   = mat2x2f;
 
-	using mat3x3f	= mat<3, 3, float, qualifier::defaultp>;
-	using mat3f		= mat3x3f;
+    using mat3x3f = mat<3, 3, float, qualifier::defaultp>;
+    using mat3f   = mat3x3f;
 
-	using mat4x4f	= mat<4, 4, float, qualifier::defaultp>;
-	using mat4f		= mat4x4f;
+    using mat4x4f = mat<4, 4, float, qualifier::defaultp>;
+    using mat4f   = mat4x4f;
 
-	//TODO: All the other ps
+    //TODO: All the other ps
 }
