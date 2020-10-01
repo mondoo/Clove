58dfd1c8a25304407e4f101b06d4e658eed5268a
Alex <wolfplayeral@yahoo.co.uk>
2020-10-01 07:25:43 +0100

    Added a bit more detail to AudioSource::unQueueBuffers

--- Clove/include/Clove/Audio/AudioSource.hpp
+++ Clove/include/Clove/Audio/AudioSource.hpp
@@ -5,7 +5,7 @@ namespace clv {
 }
 
 namespace clv {
-	/**
+    /**
 	 * @brief An AudioSource allows sound to played from a cetain point in space
 	 */
     class AudioSource {
@@ -23,14 +23,16 @@ namespace clv {
 		 * @details Buffers are appended to the end of the current queue.
 		 * @param buffers Vector of buffers to add to the queue.
 		 */
-        virtual void queueBuffers(std::vector<std::shared_ptr<AudioBuffer>> buffers)   = 0;
+        virtual void queueBuffers(std::vector<std::shared_ptr<AudioBuffer>> buffers) = 0;
         /**
-		 * @brief Removes buffers from the queue.
-		 * @details numToUnQueue must be <= getNumBuffersProcessed.
-		 * @param numToQueue The number of buffers to be removed from the queue.
+		 * @brief Removes processed buffers from the queue.
+		 * @details Processed buffers can be removed from the queue, allowing their data 
+         * to be changed and then added back in. Useful when double or triple buffering
+         * to stream large audio files.
+		 * @param numToQueue The number of buffers to remove from the queue. must be <= getNumBuffersProcessed.
 		 * @returns A vector of AudioBuffers removed from the queue.
 		 */
-		virtual std::vector<std::shared_ptr<AudioBuffer>> unQueueBuffers(const uint32_t numToUnqueue) = 0;
+        virtual std::vector<std::shared_ptr<AudioBuffer>> unQueueBuffers(const uint32_t numToUnqueue) = 0;
 
         virtual void setPitch(float pitch)      = 0;
         virtual void setLooping(bool isLooping) = 0;
@@ -41,11 +43,11 @@ namespace clv {
         virtual clv::mth::vec3f getPosition() const = 0;
         virtual clv::mth::vec3f getVelocity() const = 0;
 
-		/**
+        /**
 		 * @brief How many buffers are in the queue (not played or playing)
 		 */
-        virtual uint32_t getNumBuffersQueued() const    = 0;
-		/**
+        virtual uint32_t getNumBuffersQueued() const = 0;
+        /**
 		 * @brief How many buffers have been played
 		 */
         virtual uint32_t getNumBuffersProcessed() const = 0;
