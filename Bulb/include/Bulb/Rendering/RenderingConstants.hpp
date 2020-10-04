87f946076b0c03dfb41400841ff963c67f40f566
Alex <wolfplayeral@yahoo.co.uk>
2020-10-04 13:12:05 +0100

    Increased shadow map resolution

--- Bulb/include/Bulb/Rendering/RenderingConstants.hpp
+++ Bulb/include/Bulb/Rendering/RenderingConstants.hpp
@@ -10,7 +10,7 @@ namespace blb::rnd {
     inline constexpr uint8_t MAX_LIGHTS = 10u;
     inline constexpr uint8_t MAX_JOINTS = std::numeric_limits<uint8_t>::max();
 
-    inline constexpr uint32_t shadowMapSize = 1024u;
+    inline constexpr uint32_t shadowMapSize = 2048u;
 
     /**
 	 * @brief Defines a mapping between set slots
