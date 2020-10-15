0d790fe6b0086bb108feec4109d9072d71c17300
Alex <wolfplayeral@yahoo.co.uk>
2020-10-15 13:22:38 +0100

    Can construct Expected with a value

--- Root/Expected/include/Root/Expected.hpp
+++ Root/Expected/include/Root/Expected.hpp
@@ -1,7 +1,31 @@
 #pragma once
 
 namespace garlic::inline root {
+    /**
+     * @brief Wraps either a value of type T or an error of type E.
+     * @details Expected is used to return from functions either an
+     * expected value or an error if the function fails.
+     * @tparam T The type of the expected value.
+     * @tparam E The type of the error. For example, can be an error
+     * code or an exception.
+     */
+    template<typename T, typename E>
+    class Expected {
+        //VARIABLES
+    private:
+        T value;
 
+        //FUNCTIONS
+    public:
+        Expected() = default;
+        Expected(T value)
+            : value(std::move(value)) {
+        }
+
+        T getValue() {
+            return value;
+        }
+    };
 }
 
 #include "Expected.inl"
--- Root/Expected/tests/ExpectedTests.cpp
+++ Root/Expected/tests/ExpectedTests.cpp
@@ -2,3 +2,10 @@
 #include <gtest/gtest.h>
 
 using namespace garlic;
+
+TEST(ExpectedTests, CanConstructWithAValue) {
+    int32_t constexpr testValue = 50;
+    Expected<int32_t, std::exception> expectedValue(testValue);
+
+    EXPECT_EQ(testValue, expectedValue.getValue());
+}
