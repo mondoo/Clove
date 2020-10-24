9f5420366259f0bc31cfc68cae866586f546306d
Alex <wolfplayeral@yahoo.co.uk>
2020-10-24 19:17:45 +0100

    Membrane lin now gets copied to the output dir properly
    This even works when another project references Scape/Engine

--- Scape/Engine/Import.cs
+++ Scape/Engine/Import.cs
@@ -2,14 +2,12 @@ namespace Scape.Engine
 {
     class Import
     {
-        //TODO: Investigate a way to build and pull these files into the current working / build directory
-
 #if SCAPE_PLATFORM_WINDOWS
-        public const string MembraneDll = "../../build/lib/Debug/Scape.Membrane.dll";
+        public const string MembraneDll = "Scape.Membrane.dll";
 #elif SCAPE_PLATFORM_MACOS
         public const string MembraneDll = ""; //TODO
 #elif SCAPE_PLATFORM_LINUX
-        public const string MembraneDll = "../../build/Scape/Scape.Membrane/libScape.Membrane.so";
+        public const string MembraneDll = "libScape.Membrane.so";
 #endif
     }
 }
