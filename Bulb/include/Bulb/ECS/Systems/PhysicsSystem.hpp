4b03bbe74fdba55a94e43b550e64d6ea19e2a05e
Alex <wolfplayeral@yahoo.co.uk>
2020-10-25 16:01:29 +0000

    Can now cast rays against collision groups

--- Bulb/include/Bulb/ECS/Systems/PhysicsSystem.hpp
+++ Bulb/include/Bulb/ECS/Systems/PhysicsSystem.hpp
@@ -26,17 +26,17 @@ namespace blb::ecs {
             EntityID entityA;
             EntityID entityB;
 
-            constexpr friend bool operator==(const CollisionManifold& lhs, const CollisionManifold& rhs) {
+            constexpr friend bool operator==(CollisionManifold const &lhs, CollisionManifold const &rhs) {
                 return (lhs.entityA == rhs.entityA && lhs.entityB == rhs.entityB) ||
                     (lhs.entityA == rhs.entityB && lhs.entityB == rhs.entityA);
             }
-            constexpr friend bool operator!=(const CollisionManifold& lhs, const CollisionManifold& rhs) {
+            constexpr friend bool operator!=(CollisionManifold const &lhs, CollisionManifold const &rhs) {
                 return !(lhs == rhs);
             }
         };
 
         struct ManifoldHasher {
-            size_t operator()(const CollisionManifold& manifold) const {
+            size_t operator()(CollisionManifold const &manifold) const {
                 size_t a = std::hash<EntityID>{}(manifold.entityA);
                 size_t b = std::hash<EntityID>{}(manifold.entityB);
                 return a ^ (b << 1);
@@ -64,34 +64,41 @@ namespace blb::ecs {
     public:
         PhysicsSystem();
 
-        PhysicsSystem(const PhysicsSystem& other) = delete;
-        PhysicsSystem(PhysicsSystem&& other) noexcept;
+        PhysicsSystem(PhysicsSystem const &other) = delete;
+        PhysicsSystem(PhysicsSystem &&other) noexcept;
 
-        PhysicsSystem& operator=(const PhysicsSystem& other) = delete;
-        PhysicsSystem& operator=(PhysicsSystem&& other) noexcept;
+        PhysicsSystem &operator=(PhysicsSystem const &other) = delete;
+        PhysicsSystem &operator=(PhysicsSystem &&other) noexcept;
 
         ~PhysicsSystem();
 
-        void registerToEvents(garlic::EventDispatcher& dispatcher) override;
+        void registerToEvents(garlic::EventDispatcher &dispatcher) override;
 
-        void preUpdate(World& world) override;
-        void update(World& world, clv::utl::DeltaTime deltaTime) override;
-        void postUpdate(World& world) override;
+        void preUpdate(World &world) override;
+        void update(World &world, clv::utl::DeltaTime deltaTime) override;
+        void postUpdate(World &world) override;
 
         /**
          * @brief Fires a ray into the world.
          * @returns The ID of the first Entity hit.
          */
-        EntityID rayCast(const clv::mth::vec3f& begin, const clv::mth::vec3f& end);
+        EntityID rayCast(clv::mth::vec3f const &begin, clv::mth::vec3f const &end);
+        /**
+         * @brief Fires a ray into the world.
+         * @param collisionGroup Bit flag of the collision groups the ray belongs to.
+         * @param collisionMask Bit flag of the collision groups the ray collides with.
+         * @return The ID of the first Entity hit.
+         */
+        EntityID rayCast(clv::mth::vec3f const &begin, clv::mth::vec3f const &end, uint32_t const collisionGroup, uint32_t const collisionMask);
 
     private:
-        void onCubeColliderAdded(const ComponentAddedEvent<CubeColliderComponent>& event);
-        void onCubeColliderRemoved(const ComponentRemovedEvent<CubeColliderComponent>& event);
+        void onCubeColliderAdded(ComponentAddedEvent<CubeColliderComponent> const &event);
+        void onCubeColliderRemoved(ComponentRemovedEvent<CubeColliderComponent> const &event);
 
-        void onRigidBodyAdded(const ComponentAddedEvent<RigidBodyComponent>& event);
-        void onRigidBodyRemoved(const ComponentRemovedEvent<RigidBodyComponent>& event);
+        void onRigidBodyAdded(ComponentAddedEvent<RigidBodyComponent> const &event);
+        void onRigidBodyRemoved(ComponentRemovedEvent<RigidBodyComponent> const &event);
 
-        void addBodyToWorld(const RigidBodyComponent& rigidBodyComponent);
-        void addColliderToWorld(const CubeColliderComponent& colliderComponent);
+        void addBodyToWorld(RigidBodyComponent const &rigidBodyComponent);
+        void addColliderToWorld(CubeColliderComponent const &colliderComponent);
     };
 }
--- Bulb/src/ECS/Systems/PhysicsSystem.cpp
+++ Bulb/src/ECS/Systems/PhysicsSystem.cpp
@@ -22,7 +22,7 @@ namespace blb::ecs {
         dynamicsWorld = std::make_unique<btDiscreteDynamicsWorld>(dispatcher.get(), overlappingPairCache.get(), solver.get(), collisionConfiguration.get());
     }
 
-    PhysicsSystem::PhysicsSystem(PhysicsSystem&& other) noexcept {
+    PhysicsSystem::PhysicsSystem(PhysicsSystem &&other) noexcept {
         collisionConfiguration = std::move(other.collisionConfiguration);
         dispatcher             = std::move(other.dispatcher);
         overlappingPairCache   = std::move(other.overlappingPairCache);
@@ -39,33 +39,33 @@ namespace blb::ecs {
         currentCollisions = std::move(other.currentCollisions);
     }
 
-    PhysicsSystem& PhysicsSystem::operator=(PhysicsSystem&& other) noexcept = default;
+    PhysicsSystem &PhysicsSystem::operator=(PhysicsSystem &&other) noexcept = default;
 
     PhysicsSystem::~PhysicsSystem() = default;
 
-    void PhysicsSystem::registerToEvents(garlic::EventDispatcher& dispatcher) {
-        cubeColliderAddedHandle = dispatcher.bindToEvent<ComponentAddedEvent<CubeColliderComponent>>([this](const ComponentAddedEvent<CubeColliderComponent>& event) {
+    void PhysicsSystem::registerToEvents(garlic::EventDispatcher &dispatcher) {
+        cubeColliderAddedHandle = dispatcher.bindToEvent<ComponentAddedEvent<CubeColliderComponent>>([this](ComponentAddedEvent<CubeColliderComponent> const &event) {
             onCubeColliderAdded(event);
         });
 
-        cubeColliderRemovedHandle = dispatcher.bindToEvent<ComponentRemovedEvent<CubeColliderComponent>>([this](const ComponentRemovedEvent<CubeColliderComponent>& event) {
+        cubeColliderRemovedHandle = dispatcher.bindToEvent<ComponentRemovedEvent<CubeColliderComponent>>([this](ComponentRemovedEvent<CubeColliderComponent> const &event) {
             onCubeColliderRemoved(event);
         });
 
-        rigidBodyAddedHandle = dispatcher.bindToEvent<ComponentAddedEvent<RigidBodyComponent>>([this](const ComponentAddedEvent<RigidBodyComponent>& event) {
+        rigidBodyAddedHandle = dispatcher.bindToEvent<ComponentAddedEvent<RigidBodyComponent>>([this](ComponentAddedEvent<RigidBodyComponent> const &event) {
             onRigidBodyAdded(event);
         });
 
-        rigidBodyRemovedHandle = dispatcher.bindToEvent<ComponentRemovedEvent<RigidBodyComponent>>([this](const ComponentRemovedEvent<RigidBodyComponent>& event) {
+        rigidBodyRemovedHandle = dispatcher.bindToEvent<ComponentRemovedEvent<RigidBodyComponent>>([this](ComponentRemovedEvent<RigidBodyComponent> const &event) {
             onRigidBodyRemoved(event);
         });
     }
 
-    void PhysicsSystem::preUpdate(World& world) {
+    void PhysicsSystem::preUpdate(World &world) {
         CLV_PROFILE_FUNCTION();
 
         //Make sure any colliders are properly paired with their rigid body
-        for(auto&& [cubeCollider, rigidBody] : world.getComponentSets<CubeColliderComponent, RigidBodyComponent>()) {
+        for(auto &&[cubeCollider, rigidBody] : world.getComponentSets<CubeColliderComponent, RigidBodyComponent>()) {
             if(cubeCollider->collisionObject != nullptr) {
                 dynamicsWorld->removeCollisionObject(cubeCollider->collisionObject.get());
                 cubeCollider->collisionObject.reset();
@@ -81,7 +81,7 @@ namespace blb::ecs {
         }
 
         //Make sure any recently un-paired colliders are updated
-        for(auto&& [cubeCollider] : world.getComponentSets<CubeColliderComponent>()) {
+        for(auto &&[cubeCollider] : world.getComponentSets<CubeColliderComponent>()) {
             if(world.hasComponent<RigidBodyComponent>(cubeCollider->getEntityID())) {
                 continue;
             }
@@ -93,13 +93,13 @@ namespace blb::ecs {
         }
 
         //Make sure any recently un-paired rigid bodies are updated
-        for(auto&& [rigidBody] : world.getComponentSets<RigidBodyComponent>()) {
+        for(auto &&[rigidBody] : world.getComponentSets<RigidBodyComponent>()) {
             if(world.hasComponent<CubeColliderComponent>(rigidBody->getEntityID())) {
                 continue;
             }
 
             if(rigidBody->standInShape == nullptr) {
-                btRigidBody* body = rigidBody->body.get();
+                btRigidBody *body = rigidBody->body.get();
 
                 dynamicsWorld->removeCollisionObject(body);
                 rigidBody->standInShape = RigidBodyComponent::createStandInShape();
@@ -109,15 +109,15 @@ namespace blb::ecs {
         }
     }
 
-    void PhysicsSystem::update(World& world, utl::DeltaTime deltaTime) {
+    void PhysicsSystem::update(World &world, utl::DeltaTime deltaTime) {
         CLV_PROFILE_FUNCTION();
 
-        const auto cubeColliders = world.getComponentSets<TransformComponent, CubeColliderComponent>();
-        const auto rigidBodies   = world.getComponentSets<TransformComponent, RigidBodyComponent>();
+        auto const cubeColliders = world.getComponentSets<TransformComponent, CubeColliderComponent>();
+        auto const rigidBodies   = world.getComponentSets<TransformComponent, RigidBodyComponent>();
 
-        const auto updateCollider = [](const TransformComponent& transform, btCollisionObject& collisionObject) {
-            const auto pos = transform.getPosition(TransformSpace::World);
-            const auto rot = transform.getRotation(TransformSpace::World);
+        auto const updateCollider = [](TransformComponent const &transform, btCollisionObject &collisionObject) {
+            auto const pos = transform.getPosition(TransformSpace::World);
+            auto const rot = transform.getRotation(TransformSpace::World);
 
             btTransform btTrans = collisionObject.getWorldTransform();
             btTrans.setOrigin({ pos.x, pos.y, pos.z });
@@ -126,52 +126,52 @@ namespace blb::ecs {
         };
 
         //Notify Bullet of the location of the colliders
-        for(auto&& [transform, cubeCollider] : cubeColliders) {
+        for(auto &&[transform, cubeCollider] : cubeColliders) {
             if(world.hasComponent<RigidBodyComponent>(cubeCollider->getEntityID())) {
                 continue;
             }
 
             updateCollider(*transform, *cubeCollider->collisionObject);
         }
-        for(auto&& [transform, rigidBody] : rigidBodies) {
+        for(auto &&[transform, rigidBody] : rigidBodies) {
             updateCollider(*transform, *rigidBody->body);
         }
 
         //Step physics world
         dynamicsWorld->stepSimulation(deltaTime.getDeltaSeconds());
 
-        const auto updateTransform = [](TransformComponent& transform, const btCollisionObject& collisionObject) {
-            const btTransform& btTrans = collisionObject.getWorldTransform();
-            const btVector3& pos       = btTrans.getOrigin();
-            const btQuaternion& rot    = btTrans.getRotation();
+        auto const updateTransform = [](TransformComponent &transform, btCollisionObject const &collisionObject) {
+            btTransform const &btTrans = collisionObject.getWorldTransform();
+            btVector3 const &pos       = btTrans.getOrigin();
+            btQuaternion const &rot    = btTrans.getRotation();
 
             transform.setPosition({ pos.x(), pos.y(), pos.z() }, TransformSpace::World);
             transform.setRotation({ rot.getW(), rot.getX(), rot.getY(), rot.getZ() }, TransformSpace::World);
         };
 
         //Apply any simulation updates
-        for(auto&& [transform, cubeCollider] : cubeColliders) {
+        for(auto &&[transform, cubeCollider] : cubeColliders) {
             if(world.hasComponent<RigidBodyComponent>(cubeCollider->getEntityID())) {
                 continue;
             }
 
             updateTransform(*transform, *cubeCollider->collisionObject);
         }
-        for(auto&& [transform, rigidBody] : rigidBodies) {
+        for(auto &&[transform, rigidBody] : rigidBodies) {
             updateTransform(*transform, *rigidBody->body);
         }
     }
 
-    void PhysicsSystem::postUpdate(World& world) {
+    void PhysicsSystem::postUpdate(World &world) {
         //Gather collision manifolds
-        const int numManifolds = dispatcher->getNumManifolds();
+        int const numManifolds = dispatcher->getNumManifolds();
         std::unordered_set<CollisionManifold, ManifoldHasher> newCollisions;
 
         for(int i = 0; i < numManifolds; ++i) {
-            btPersistentManifold* manifold = dispatcher->getManifoldByIndexInternal(i);
+            btPersistentManifold *manifold = dispatcher->getManifoldByIndexInternal(i);
             if(manifold->getNumContacts() > 0) {
-                const btCollisionObject* obA = manifold->getBody0();
-                const btCollisionObject* obB = manifold->getBody1();
+                btCollisionObject const *obA = manifold->getBody0();
+                btCollisionObject const *obB = manifold->getBody1();
 
                 EntityID entityA = obA->getUserIndex();
                 EntityID entityB = obB->getUserIndex();
@@ -185,18 +185,18 @@ namespace blb::ecs {
 
         //Copy any elements not in our cached set
         collisionBeginManifolds.reserve(std::size(newCollisions));
-        std::copy_if(std::begin(newCollisions), std::end(newCollisions), std::back_inserter(collisionBeginManifolds), [this](const CollisionManifold& manifold) {
+        std::copy_if(std::begin(newCollisions), std::end(newCollisions), std::back_inserter(collisionBeginManifolds), [this](CollisionManifold const &manifold) {
             return !currentCollisions.contains(manifold);
         });
 
         //Copy any elements not in our new array
         collisionEndManifolds.reserve(std::size(currentCollisions));
-        std::copy_if(std::begin(currentCollisions), std::end(currentCollisions), std::back_inserter(collisionEndManifolds), [&](const CollisionManifold& manifold) {
+        std::copy_if(std::begin(currentCollisions), std::end(currentCollisions), std::back_inserter(collisionEndManifolds), [&](CollisionManifold const &manifold) {
             return !newCollisions.contains(manifold);
         });
 
         //Broadcast collision begin events
-        for(const auto& manifold : collisionBeginManifolds) {
+        for(auto const &manifold : collisionBeginManifolds) {
             Entity entityA = world.getEntity(manifold.entityA);
             Entity entityB = world.getEntity(manifold.entityB);
 
@@ -212,7 +212,7 @@ namespace blb::ecs {
         }
 
         //Broadcast collision end events
-        for(const auto& manifold : collisionEndManifolds) {
+        for(auto const &manifold : collisionEndManifolds) {
             Entity entityA = world.getEntity(manifold.entityA);
             Entity entityB = world.getEntity(manifold.entityB);
 
@@ -228,11 +228,17 @@ namespace blb::ecs {
         }
     }
 
-    EntityID PhysicsSystem::rayCast(const clv::mth::vec3f& begin, const clv::mth::vec3f& end) {
-        btVector3 btBegin{ begin.x, begin.y, begin.z };
-        btVector3 btEnd{ end.x, end.y, end.z };
+    EntityID PhysicsSystem::rayCast(clv::mth::vec3f const &begin, clv::mth::vec3f const &end) {
+        return rayCast(begin, end, ~0, ~0);
+    }
+
+    EntityID PhysicsSystem::rayCast(clv::mth::vec3f const &begin, clv::mth::vec3f const &end, uint32_t const collisionGroup, uint32_t const collisionMask) {
+        btVector3 const btBegin{ begin.x, begin.y, begin.z };
+        btVector3 const btEnd{ end.x, end.y, end.z };
 
         btCollisionWorld::ClosestRayResultCallback callBack{ btBegin, btEnd };
+        callBack.m_collisionFilterGroup = collisionGroup;
+        callBack.m_collisionFilterMask  = collisionMask;
         dynamicsWorld->rayTest(btBegin, btEnd, callBack);
 
         if(callBack.m_collisionObject != nullptr) {
@@ -242,30 +248,30 @@ namespace blb::ecs {
         }
     }
 
-    void PhysicsSystem::onCubeColliderAdded(const ComponentAddedEvent<CubeColliderComponent>& event) {
+    void PhysicsSystem::onCubeColliderAdded(ComponentAddedEvent<CubeColliderComponent> const &event) {
         addColliderToWorld(*event.component);
     }
 
-    void PhysicsSystem::onCubeColliderRemoved(const ComponentRemovedEvent<CubeColliderComponent>& event) {
-        if(btCollisionObject* object = event.component->collisionObject.get()) {
+    void PhysicsSystem::onCubeColliderRemoved(ComponentRemovedEvent<CubeColliderComponent> const &event) {
+        if(btCollisionObject *object = event.component->collisionObject.get()) {
             dynamicsWorld->removeCollisionObject(object);
         }
     }
 
-    void PhysicsSystem::onRigidBodyAdded(const ComponentAddedEvent<RigidBodyComponent>& event) {
+    void PhysicsSystem::onRigidBodyAdded(ComponentAddedEvent<RigidBodyComponent> const &event) {
         addBodyToWorld(*event.component);
     }
 
-    void PhysicsSystem::onRigidBodyRemoved(const ComponentRemovedEvent<RigidBodyComponent>& event) {
+    void PhysicsSystem::onRigidBodyRemoved(ComponentRemovedEvent<RigidBodyComponent> const &event) {
         dynamicsWorld->removeCollisionObject(event.component->body.get());
     }
 
-    void PhysicsSystem::addBodyToWorld(const RigidBodyComponent& rigidBodyComponent) {
+    void PhysicsSystem::addBodyToWorld(RigidBodyComponent const &rigidBodyComponent) {
         dynamicsWorld->addRigidBody(rigidBodyComponent.body.get(), rigidBodyComponent.descriptor.collisionGroup, rigidBodyComponent.descriptor.collisionMask);
         rigidBodyComponent.body->setUserIndex(rigidBodyComponent.getEntityID());
     }
 
-    void PhysicsSystem::addColliderToWorld(const CubeColliderComponent& colliderComponent) {
+    void PhysicsSystem::addColliderToWorld(CubeColliderComponent const &colliderComponent) {
         dynamicsWorld->addCollisionObject(colliderComponent.collisionObject.get(), ~0, ~0);//Add the collider to every group and collide with every other group
         colliderComponent.collisionObject->setUserIndex(colliderComponent.getEntityID());
     }
