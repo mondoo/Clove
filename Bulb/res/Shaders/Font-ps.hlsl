7bab4a4cc43be8a3a7d20bd0892cee1eb125ba3a
Alex <wolfplayeral@yahoo.co.uk>
2020-11-13 21:45:44 +0000

    Removed old HLSL shaders

--- Bulb/res/Shaders/Font-ps.hlsl
+++
@@ -1,6 +0,0 @@
-Texture2D albedoTexture : register(t1);
-SamplerState albedoSampler : register(s1);
-
-float4 main(float2 texCoord : TexCoord) : SV_Target{
-	return float4(1.0, 1.0, 1.0, albedoTexture.Sample(albedoSampler, texCoord).r);
-}
