5993750b422f6718a1b846f6f662da8c8fcf1b64
Alex <wolfplayeral@yahoo.co.uk>
2020-11-19 13:15:45 +0000

    MainWindow now runs Application in another thread

--- Scape/Editor/MainWindow.xaml.cs
+++ Scape/Editor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Threading;
 using System.Windows;
 
 namespace Editor
@@ -8,11 +10,45 @@ namespace Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Garlic.Application app;
+
+        private Thread appThread;
+        private object appThreadLock = new object();
+
+        private bool exit = false;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Garlic.Application app = new Garlic.Application();
+            app = new Garlic.Application();
+
+            Closing += (object sender, CancelEventArgs e) =>
+            {
+                lock (appThreadLock)
+                {
+                    exit = true;
+                }
+                appThread.Join();
+            };
+
+            appThread = new Thread(new ThreadStart(TickApplication));
+            appThread.Name = "Garlic application thread";
+            appThread.Start();
+        }
+
+        private void TickApplication()
+        {
+            while (app.tick())
+            {
+                lock (appThreadLock)
+                {
+                    if (exit)
+                    {
+                        app.shutdown();
+                    }
+                }
+            }
         }
     }
 }
