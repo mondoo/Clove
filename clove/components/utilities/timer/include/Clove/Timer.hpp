8d1ce14e2c1a54e20d58346fc2c16e390e8be6de
Alex <wolfplayeral@yahoo.co.uk>
2020-11-23 22:18:09 +0000

    Timer is now it's own library

---
+++ clove/components/utilities/timer/include/Clove/Timer.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <chrono>
+#include <fstream>
+
+#if CLV_ENABLE_PROFILING
+namespace garlic::clove {
+    struct ProfileResult {
+        std::string name;
+
+        int64_t start{ 0 };
+        int64_t end{ 0 };
+
+        uint32_t threadID;
+    };
+
+    struct TimerSession {
+        std::string name;
+    };
+}
+
+namespace garlic::clove {
+    class Timer {
+        //VARIABLES
+    private:
+        TimerSession *currentSession = nullptr;
+        std::ofstream outputStream;
+        uint32_t profileCount = 0;
+
+        //FUNCTIONS
+    public:
+        Timer();
+
+        ~Timer();
+
+        static Timer &get();
+
+        void beginSession(std::string const &name, std::string const &filepath = "results.json");
+        void endSession();
+
+        void writeHeader();
+        void writeProfile(ProfileResult const &result);
+        void writeFooter();
+    };
+}
+
+namespace garlic::clove {
+    class ScopedTimerClock {
+        //VARIABLES
+    private:
+        std::string name;
+        std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
+
+        //FUNCTIONS
+    public:
+        ScopedTimerClock(std::string const &name);
+
+        ~ScopedTimerClock();
+    };
+}
+#endif
+
+#if CLV_ENABLE_PROFILING
+    #define CLV_PROFILE_BEGIN_SESSION(name, filepath) ::garlic::clove::Timer::get().beginSession(name, filepath)
+    #define CLV_PROFILE_END_SESSION() ::garlic::clove::Timer::get().endSession()
+    #define CLV_PROFILE_SCOPE(name) ::garlic::clove::ScopedTimerClock timer##__LINE__(name)
+    #define CLV_PROFILE_FUNCTION() CLV_PROFILE_SCOPE(GARLIC_FUNCTION_NAME_PRETTY)
+#else
+    #define CLV_PROFILE_BEGIN_SESSION(name, filepath)
+    #define CLV_PROFILE_END_SESSION()
+    #define CLV_PROFILE_SCOPE(name)
+    #define CLV_PROFILE_FUNCTION()
+#endif
