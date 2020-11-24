37d701a7c917b5b8a1d58a2e927e3f0df645de82
Alex <wolfplayeral@yahoo.co.uk>
2020-11-24 18:26:16 +0000

    CloveStateMachine is now it's own library

---
+++ clove/components/systems/state_machine/include/Clove/StateMachine/StateMachine.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+namespace garlic::clove {
+    template<typename Action>
+    class State;
+}
+
+namespace garlic::clove {
+    /**
+	 * @brief A StateMachine manages States and Transitions. Determining what Action to execute.
+	 * @tparam Action The type of Action this StateMachine handles
+	 */
+    template<typename Action>
+    class StateMachine {
+        //VARIABLES
+    private:
+        State<Action> *currentState = nullptr;
+
+        //FUNCTIONS
+    public:
+        StateMachine() = delete;
+        StateMachine(State<Action> *initialState);
+
+        StateMachine(StateMachine const &other) = delete;
+        StateMachine(StateMachine &&other) noexcept;
+
+        StateMachine &operator=(StateMachine const &other) = delete;
+        StateMachine &operator=(StateMachine &&other) noexcept;
+
+        ~StateMachine();
+
+        /**
+		 * @brief Runs through the current State to test for any Transitions that were activated
+		 * @return A vector of Actions to perform
+		 */
+        std::vector<Action *> update();
+    };
+}
+
+#include "StateMachine.inl"
---
+++ clove/components/systems/state_machine/include/Clove/StateMachine/Transition.hpp
@@ -0,0 +1,47 @@
+#include <functional>
+#include <memory>
+#include <vector>
+
+namespace garlic::clove {
+    template<typename Action>
+    class State;
+}
+
+namespace garlic::clove {
+    /**
+	 * @brief A Transition defines a condition on how to transition from one State to another
+	 * @details Transitions can also perform actions while transitioning between States
+	 * @tparam Action The type of Action this Transition deals with
+	 */
+    template<typename Action>
+    class Transition {
+        //VARIABLES
+    private:
+        //TODO: custom deleter for unique ptrs so allocators can be used
+        std::function<bool()> condition;
+        State<Action> *state;
+        std::vector<std::unique_ptr<Action>> actions;
+
+        //FUNCTIONS
+    public:
+        Transition();
+
+        Transition(Transition const &other) = delete;
+        Transition(Transition &&other) noexcept;
+
+        Transition &operator=(Transition const &other) = delete;
+        Transition &operator=(Transition &&other) noexcept;
+
+        ~Transition();
+
+        void init(std::function<bool()> condition, State<Action> *state);
+        void init(std::function<bool()> condition, State<Action> *state, std::vector<std::unique_ptr<Action>> actions);
+
+        bool isTriggered() const;
+
+        State<Action> *getState() const;
+        std::vector<Action *> getActions() const;
+    };
+}
+
+#include "Transition.inl"
