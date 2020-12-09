06a61814df30796b85b1e49ccb9cdb419f2f56ad
Alex <wolfplayeral@yahoo.co.uk>
2020-12-09 21:37:51 +0000

    Set up default values on the WindowDesciptor

--- clove/components/core/platform/include/Clove/Platform/PlatformTypes.hpp
+++ clove/components/core/platform/include/Clove/Platform/PlatformTypes.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <string>
 #include <cinttypes>
+#include <string>
 
 namespace garlic::clove {
     struct WindowDescriptor {
         std::string title;
-        int32_t width;
-        int32_t height;
+        int32_t width{ 800 };
+        int32_t height{ 600 };
     };
 }
