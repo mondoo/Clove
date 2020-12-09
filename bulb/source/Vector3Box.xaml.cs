cd087ab091a55a9e898e8b6828d43b7e90b02ed0
Alex <wolfplayeral@yahoo.co.uk>
2020-12-09 07:01:23 +0000

    Created a UserControl to view 3 component vectors

---
+++ bulb/source/Vector3Box.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace Garlic.Bulb
+{
+    /// <summary>
+    /// A UserControl for displaying 3 component vectors
+    /// </summary>
+    public partial class Vector3Box : UserControl
+    {
+        public Vector3Box(){
+            InitializeComponent();
+        }
+    }
+}
