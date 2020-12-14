ed5724e97b7a47cca057e2a7f6df78545cace00e
Alex <wolfplayeral@yahoo.co.uk>
2020-12-14 06:33:00 +0000

    Set up the bindings for the Transform ViewModel -> View

--- bulb/source/ComponentViewModel.cs
+++ bulb/source/ComponentViewModel.cs
@@ -5,11 +5,34 @@ namespace Garlic.Bulb
     /// </summary>
     public class ComponentViewModel : ViewModel
     {
-        public string Name{ get; set; }
+        public string Name { get; set; }
     }
 
     //TEMP
-    public class TransformComponentViewModel : ComponentViewModel {
+    public class TransformComponentViewModel : ComponentViewModel
+    {
+        public string XText { get; } = "X";
+        public string XValue
+        {
+            get { return xValue; }
+            set { xValue = value; OnPropertyChanged(nameof(xValue)); }
+        }
+        private string xValue = "0";
 
-	}
+        public string YText { get; } = "Y";
+        public string YValue
+        {
+            get { return yValue; }
+            set { yValue = value; OnPropertyChanged(nameof(yValue)); }
+        }
+        private string yValue = "0";
+
+        public string ZText { get; } = "Z";
+        public string ZValue
+        {
+            get { return zValue; }
+            set { zValue = value; OnPropertyChanged(nameof(zValue)); }
+        }
+        private string zValue = "0";
+    }
 }
