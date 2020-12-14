588c4a13c45bfc4d0c4479bb62c6709200f72d70
Alex <wolfplayeral@yahoo.co.uk>
2020-12-14 07:11:51 +0000

    Vector3Box properly references the dependency properties

--- bulb/source/Vector3Box.xaml.cs
+++ bulb/source/Vector3Box.xaml.cs
@@ -1,5 +1,7 @@
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Garlic.Bulb
 {
@@ -8,23 +10,47 @@ namespace Garlic.Bulb
     /// </summary>
     public partial class Vector3Box : UserControl
     {
-        public string LeftFieldText { get; set; }
-        public string LeftFieldValue { get; set; }
+        public string LeftFieldText
+        {
+            get { return (string)GetValue(LeftTextProperty); }
+            set { SetValue(LeftTextProperty, value); }
+        }
+        public string LeftFieldValue
+        {
+            get { return (string)GetValue(LeftValueProperty); }
+            set { SetValue(LeftValueProperty, value); }
+        }
 
-        public string CenterFieldText { get; set; }
-        public string CenterFieldValue { get; set; }
+        public string CenterFieldText
+        {
+            get { return (string)GetValue(CenterTextProperty); }
+            set { SetValue(CenterTextProperty, value); }
+        }
+        public string CenterFieldValue
+        {
+            get { return (string)GetValue(CenterValueProperty); }
+            set { SetValue(CenterValueProperty, value); }
+        }
 
-        public string RightFieldText { get; set; }
-        public string RightFieldValue { get; set; }
+        public string RightFieldText
+        {
+            get { return (string)GetValue(RightTextProperty); }
+            set { SetValue(RightTextProperty, value); }
+        }
+        public string RightFieldValue
+        {
+            get { return (string)GetValue(RightValueProperty); }
+            set { SetValue(RightValueProperty, value); }
+        }
 
-        public static DependencyProperty XTextProperty = DependencyProperty.Register(nameof(LeftFieldText), typeof(object), typeof(Vector3Box), new PropertyMetadata(null));
-        public static DependencyProperty XValueProperty = DependencyProperty.Register(nameof(LeftFieldValue), typeof(object), typeof(Vector3Box), new PropertyMetadata(null));
+        public static DependencyProperty LeftTextProperty = DependencyProperty.Register(nameof(LeftFieldText), typeof(string), typeof(Vector3Box), new PropertyMetadata(null));
+        public static DependencyProperty LeftValueProperty = DependencyProperty.Register(nameof(LeftFieldValue), typeof(string), typeof(Vector3Box), new PropertyMetadata(null));
 
-        public static DependencyProperty YTextProperty = DependencyProperty.Register(nameof(CenterFieldText), typeof(object), typeof(Vector3Box), new PropertyMetadata(null));
-        public static DependencyProperty YValueProperty = DependencyProperty.Register(nameof(CenterFieldValue), typeof(object), typeof(Vector3Box), new PropertyMetadata(null));
+        public static DependencyProperty CenterTextProperty = DependencyProperty.Register(nameof(CenterFieldText), typeof(string), typeof(Vector3Box), new PropertyMetadata(null));
+        public static DependencyProperty CenterValueProperty = DependencyProperty.Register(nameof(CenterFieldValue), typeof(string), typeof(Vector3Box), new PropertyMetadata(null));
 
-        public static DependencyProperty ZTextProperty = DependencyProperty.Register(nameof(RightFieldText), typeof(object), typeof(Vector3Box), new PropertyMetadata(null));
-        public static DependencyProperty ZValueProperty = DependencyProperty.Register(nameof(RightFieldValue), typeof(object), typeof(Vector3Box), new PropertyMetadata(null));
+        public static DependencyProperty RightTextProperty = DependencyProperty.Register(nameof(RightFieldText), typeof(string), typeof(Vector3Box), new PropertyMetadata(null));
+        public static DependencyProperty RightValueProperty = DependencyProperty.Register(nameof(RightFieldValue), typeof(string), typeof(Vector3Box), new PropertyMetadata(null));
 
         public Vector3Box()
         {
