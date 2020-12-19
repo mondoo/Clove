e759d2be065a1313d6d3226d2ed7cbc1e4cf535c
Alex <wolfplayeral@yahoo.co.uk>
2020-12-19 22:56:23 +0000

    Created the TransformLayer
    This layer has two systems inside of it that generate the world transform for a given TransformComponent

---
+++ clove/source/Layers/TransformLayer.cpp
@@ -0,0 +1,53 @@
+#include "Clove/Layers/TransformLayer.hpp"
+
+#include "Clove/Application.hpp"
+#include "Clove/Components/ParentComponent.hpp"
+#include "Clove/Components/TransformComponent.hpp"
+
+#include <Clove/ECS/EntityManager.hpp>
+#include <Clove/Maths/Maths.hpp>
+#include <Clove/Maths/MathsHelpers.hpp>
+
+namespace garlic::clove {
+    namespace {
+        mat4f calculateWorldMatrix(EntityManager *entityManager, TransformComponent &transform, Entity parent) {
+            if(entityManager->hasComponent<TransformComponent>(parent)) {
+                auto parentTransform{ entityManager->getComponent<TransformComponent>(parent) };
+                auto parentParent{ entityManager->getComponent<ParentComponent>(parent) };
+
+                return calculateWorldMatrix(entityManager, *parentTransform, parentParent ? parentParent->parent : NullEntity) * transform.worldMatrix;
+            } else {
+                return transform.worldMatrix;
+            }
+        }
+    }
+
+    TransformLayer::TransformLayer()
+        : Layer("Audio")
+        , entityManager{ Application::get().getEntityManager() } {
+    }
+
+    TransformLayer::TransformLayer(TransformLayer &&other) noexcept = default;
+
+    TransformLayer &TransformLayer::operator=(TransformLayer &&other) noexcept = default;
+
+    TransformLayer::~TransformLayer() = default;
+
+    void TransformLayer::onUpdate(DeltaTime const deltaTime) {
+        CLOVE_PROFILE_FUNCTION();
+
+        //Calculate the world matrix of every single transform
+        entityManager->forEach([](TransformComponent &transform) {
+            mat4f const translationMatrix{ translate(mat4f{ 1.0f }, transform.position) };
+            mat4f const rotationMatrix{ quaternionToMatrix4(transform.rotation) };
+            mat4f const scaleMatrix{ scale(mat4f{ 1.0f }, transform.scale) };
+
+            transform.worldMatrix = translationMatrix * rotationMatrix * scaleMatrix;
+        });
+
+        //Re calculate the world matrix for all transforms with parents
+        entityManager->forEach([entityMan = entityManager](TransformComponent &transform, ParentComponent const &parent) {
+            transform.worldMatrix = calculateWorldMatrix(entityMan, transform, parent.parent);
+        });
+    }
+}
