7291ffce3ac6dcccfa13f9b63bf509de3f378f4e
Alex <wolfplayeral@yahoo.co.uk>
2020-12-23 09:34:05 +0000

    Rotation and Scale can now be set through the UI

--- bulb/source/TransformComponentViewModel.cs
+++ bulb/source/TransformComponentViewModel.cs
@@ -7,58 +7,125 @@ namespace Garlic.Bulb
     /// </summary>
     public class TransformComponentViewModel : ComponentViewModel
     {
-        public string XValue
+        //Position
+        public string PositionXValue
         {
             get { return position.x.ToString(); }
             set
             {
                 float number;
                 float.TryParse(value, out number);
-                var temp = position;
-                temp.x = number;
-                OnTransformChanged?.Invoke(temp, rotation, scale);
+                OnTransformChanged?.Invoke(new Membrane.Vector3(number, position.y, position.z), rotation, scale);
             }
         }
-
-        public string YValue
+        public string PositionYValue
         {
             get { return position.y.ToString(); }
             set
             {
                 float number;
                 float.TryParse(value, out number);
-                var temp = position;
-                temp.y = number;
-                OnTransformChanged?.Invoke(temp, rotation, scale);
+                OnTransformChanged?.Invoke(new Membrane.Vector3(position.x, number, position.z), rotation, scale);
             }
         }
-
-        public string ZValue
+        public string PositionZValue
         {
             get { return position.z.ToString(); }
             set
             {
                 float number;
                 float.TryParse(value, out number);
-                var temp = position;
-                temp.z = number;
-                OnTransformChanged?.Invoke(temp, rotation, scale);
+                OnTransformChanged?.Invoke(new Membrane.Vector3(position.x, position.y, number), rotation, scale);
             }
         }
 
-        Membrane.Vector3 position = new Membrane.Vector3();
-        Membrane.Vector3 rotation = new Membrane.Vector3();
-        Membrane.Vector3 scale = new Membrane.Vector3(1, 1, 1);
+        //Rotation
+        public string RotationXValue
+        {
+            get { return rotation.x.ToString(); }
+            set
+            {
+                float number;
+                float.TryParse(value, out number);
+                OnTransformChanged?.Invoke(position, new Membrane.Vector3(number, rotation.y, rotation.z), scale);
+            }
+        }
+        public string RotationYValue
+        {
+            get { return rotation.y.ToString(); }
+            set
+            {
+                float number;
+                float.TryParse(value, out number);
+                OnTransformChanged?.Invoke(position, new Membrane.Vector3(rotation.x, number, rotation.z), scale);
+            }
+        }
+        public string RotationZValue
+        {
+            get { return rotation.z.ToString(); }
+            set
+            {
+                float number;
+                float.TryParse(value, out number);
+                OnTransformChanged?.Invoke(position, new Membrane.Vector3(rotation.x, rotation.y, number), scale);
+            }
+        }
+
+        //Scale
+        public string ScaleXValue
+        {
+            get { return scale.x.ToString(); }
+            set
+            {
+                float number;
+                float.TryParse(value, out number);
+                OnTransformChanged?.Invoke(position, rotation, new Membrane.Vector3(number, scale.y, scale.z));
+            }
+        }
+        public string ScaleYValue
+        {
+            get { return scale.y.ToString(); }
+            set
+            {
+                float number;
+                float.TryParse(value, out number);
+                OnTransformChanged?.Invoke(position, rotation, new Membrane.Vector3(scale.x, number, scale.z));
+            }
+        }
+        public string ScaleZValue
+        {
+            get { return scale.z.ToString(); }
+            set
+            {
+                float number;
+                float.TryParse(value, out number);
+                OnTransformChanged?.Invoke(position, rotation, new Membrane.Vector3(scale.x, scale.y, number));
+            }
+        }
+
+        private Membrane.Vector3 position = new Membrane.Vector3();
+        private Membrane.Vector3 rotation = new Membrane.Vector3();
+        private Membrane.Vector3 scale = new Membrane.Vector3(1, 1, 1);
 
         public delegate void TransformChangedHandler(Membrane.Vector3 position, Membrane.Vector3 rotation, Membrane.Vector3 scale);
         public TransformChangedHandler OnTransformChanged;
 
         public void Update(Membrane.Vector3 position, Membrane.Vector3 rotation, Membrane.Vector3 scale){
             this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
 
-            OnPropertyChanged(nameof(XValue));
-            OnPropertyChanged(nameof(YValue));
-            OnPropertyChanged(nameof(ZValue));
+            OnPropertyChanged(nameof(PositionXValue));
+            OnPropertyChanged(nameof(PositionYValue));
+            OnPropertyChanged(nameof(PositionZValue));
+
+            OnPropertyChanged(nameof(RotationXValue));
+            OnPropertyChanged(nameof(RotationYValue));
+            OnPropertyChanged(nameof(RotationZValue));
+
+            OnPropertyChanged(nameof(ScaleXValue));
+            OnPropertyChanged(nameof(ScaleYValue));
+            OnPropertyChanged(nameof(ScaleZValue));
         }
     }
 }
