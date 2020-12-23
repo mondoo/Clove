2c2dcc6122a40a81b42183e769a55d09fdd531e7
Alex <wolfplayeral@yahoo.co.uk>
2020-12-23 11:25:44 +0000

    Vector3Box binding is now broadcast on every key press

--- bulb/source/Vector3Box.xaml.cs
+++ bulb/source/Vector3Box.xaml.cs
@@ -66,20 +66,18 @@ namespace Garlic.Bulb
             InitializeComponent();
         }
 
-        /// <summary>
-        /// Allows the Enter key to update a TextBox's text binding.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnKeyUp(object sender, KeyEventArgs e) {
-            if(e.Key == Key.Enter) {
-                TextBox box = (TextBox)sender;
-                DependencyProperty textProperty = TextBox.TextProperty;
+		/// <summary>
+		/// Allows the Enter key to update a TextBox's text binding.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OnKeyUp(object sender, KeyEventArgs e) {
+			TextBox box = (TextBox)sender;
+			DependencyProperty textProperty = TextBox.TextProperty;
 
-                BindingExpression binding = BindingOperations.GetBindingExpression(box, textProperty);
-                if(binding != null) {
-                    binding.UpdateSource();
-				}
+			BindingExpression binding = BindingOperations.GetBindingExpression(box, textProperty);
+			if(binding != null) {
+				binding.UpdateSource();
 			}
 		}
 	}
