d2d33484c91ac8288f031524535a3f14b2e04fd1
Alex <wolfplayeral@yahoo.co.uk>
2020-12-28 22:02:37 +0000

    Added more test conditions for entities

--- clove/components/systems/ecs/tests/EntityTests.cpp
+++ clove/components/systems/ecs/tests/EntityTests.cpp
@@ -27,8 +27,63 @@ TEST(ECSEntityTests, CreateEntity) {
     }
 }
 
-//TODO: isValid
-//TODO: clone
-//TODO: destroy
-//TODO: destroyAll
-//TODO: Create an entity in one manager and access it in another
+TEST(ECSEntityTests, CheckEntityValidity) {
+    EntityManager manager{};
+
+    EXPECT_FALSE(manager.isValid(NullEntity));
+
+    Entity entity{ manager.create() };
+    ASSERT_TRUE(manager.isValid(entity));
+
+    Entity clone{ manager.clone(entity) };
+    EXPECT_TRUE(manager.isValid(clone));
+
+    Entity badEntity{ 99999999 };
+    EXPECT_FALSE(manager.isValid(badEntity));
+
+    EntityManager otherManager{};
+
+    Entity wrongEntity{ otherManager.create() };
+
+    EXPECT_FALSE(manager.isValid(wrongEntity));
+    EXPECT_DEATH(manager.clone(wrongEntity), "");
+}
+
+TEST(ECSEntityTests, DestroySingleEntity){
+    EntityManager manager{};
+    Entity entity{ manager.create() };
+    manager.addComponent<uint32_t>(entity);
+
+    ASSERT_TRUE(manager.isValid(entity));
+    ASSERT_TRUE(manager.hasComponent<uint32_t>(entity));
+
+    manager.destroy(entity);
+
+    EXPECT_FALSE(manager.isValid(entity));
+    EXPECT_FALSE(manager.hasComponent<uint32_t>(entity));
+
+    ASSERT_TRUE(CLOVE_ENABLE_ASSERTIONS);
+
+    EXPECT_DEATH(manager.getComponent<uint32_t>(entity), "");
+    EXPECT_DEATH(manager.addComponent<float>(entity), "");
+}
+
+TEST(ECSEntityTests, DestroyAllEntities) {
+    EntityManager manager{};
+    Entity entityA{ manager.create() };
+    Entity entityB{ manager.create() };
+    Entity entityC{ manager.create() };
+    Entity entityD{ manager.create() };
+
+    ASSERT_TRUE(manager.isValid(entityA));
+    ASSERT_TRUE(manager.isValid(entityB));
+    ASSERT_TRUE(manager.isValid(entityC));
+    ASSERT_TRUE(manager.isValid(entityD));
+
+    manager.destroyAll();
+
+    EXPECT_FALSE(manager.isValid(entityA));
+    EXPECT_FALSE(manager.isValid(entityB));
+    EXPECT_FALSE(manager.isValid(entityC));
+    EXPECT_FALSE(manager.isValid(entityD));
+}
