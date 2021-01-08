2673ad26ce877dbbeee2705aacd3d04568db5920
Alex <wolfplayeral@yahoo.co.uk>
2021-01-08 07:05:57 +0000

    Capturing keyboard focus and key events on the RenderArea

--- bulb/source/MainWindow.xaml.cs
+++ bulb/source/MainWindow.xaml.cs
@@ -1,15 +1,26 @@
 ﻿using System.Windows;
 
-namespace Garlic.Bulb
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+using Membrane = garlic.membrane;
+
+namespace Garlic.Bulb {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void UserControl_KeyUp(object sender, System.Windows.Input.KeyEventArgs e) {
+			Membrane.Log.write(Membrane.LogLevel.Debug, $"Key {e.Key} up on render area called.");
+		}
+
+		private void UserControl_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+			Membrane.Log.write(Membrane.LogLevel.Debug, "Render area clicked");
+		}
+
+		private void UserControl_GotFocus(object sender, RoutedEventArgs e) {
+			Membrane.Log.write(Membrane.LogLevel.Debug, "Render area focus recieved");
+		}
+	}
 }
