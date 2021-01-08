4523086e032772644bf67e963074cb58ca91057e
Alex <wolfplayeral@yahoo.co.uk>
2021-01-08 07:25:41 +0000

    Added ViewportViewModel stub

--- bulb/source/EditorSessionViewModel.cs
+++ bulb/source/EditorSessionViewModel.cs
@@ -1,29 +1,24 @@
 using System;
-using garlic.membrane;
 
-namespace Garlic.Bulb
-{
-    /// <summary>
-    /// The View Model for the entire editor's session. Used to manage the viewmodels that make up the editor itself.
-    /// </summary>
-    public class EditorSessionViewModel : ViewModel
-    {
-        public SceneViewModel Scene
-        {
-            get { return scene; }
-            set
-            {
-                scene = value;
-                OnPropertyChanged(nameof(Scene));
-            }
-        }
-        private SceneViewModel scene;
+namespace Garlic.Bulb {
+	/// <summary>
+	/// The View Model for the entire editor's session. Used to manage the viewmodels that make up the editor itself.
+	/// </summary>
+	public class EditorSessionViewModel : ViewModel {
+		public SceneViewModel Scene {
+			get { return scene; }
+			set {
+				scene = value;
+				OnPropertyChanged(nameof(Scene));
+			}
+		}
+		private SceneViewModel scene;
 
-        public LogViewModel Log { get; } = new LogViewModel();
+		public ViewportViewModel editorViewport { get; } = new ViewportViewModel();
+		public LogViewModel Log { get; } = new LogViewModel();
 
-        public EditorSessionViewModel()
-        {
-            Scene = new SceneViewModel();
-        }
-    }
+		public EditorSessionViewModel() {
+			Scene = new SceneViewModel();
+		}
+	}
 }
---
+++ bulb/source/ViewportViewModel.cs
@@ -0,0 +1,10 @@
+using Membrane = garlic.membrane;
+
+namespace Garlic.Bulb {
+	/// <summary>
+	/// Handles viewport input and correctly dispatches it to the engine.
+	/// </summary>
+	public class ViewportViewModel : ViewModel {
+
+	}
+}
