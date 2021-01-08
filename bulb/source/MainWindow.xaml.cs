aaed83e24a71fa563504384c11b16fe29ebb14d3
Alex <wolfplayeral@yahoo.co.uk>
2021-01-08 07:11:35 +0000

    Responding to mouse events on the render area

--- bulb/source/MainWindow.xaml.cs
+++ bulb/source/MainWindow.xaml.cs
@@ -11,16 +11,20 @@ namespace Garlic.Bulb {
 			InitializeComponent();
 		}
 
+		private void UserControl_GotFocus(object sender, RoutedEventArgs e) {
+			Membrane.Log.write(Membrane.LogLevel.Debug, "Render area focus recieved");
+		}
+
 		private void UserControl_KeyUp(object sender, System.Windows.Input.KeyEventArgs e) {
 			Membrane.Log.write(Membrane.LogLevel.Debug, $"Key {e.Key} up on render area called.");
 		}
 
-		private void UserControl_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-			Membrane.Log.write(Membrane.LogLevel.Debug, "Render area clicked");
+		private void RenderAreaFocusHandler_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e) {
+			Membrane.Log.write(Membrane.LogLevel.Debug, "Render area mouse enter");
 		}
 
-		private void UserControl_GotFocus(object sender, RoutedEventArgs e) {
-			Membrane.Log.write(Membrane.LogLevel.Debug, "Render area focus recieved");
+		private void RenderAreaFocusHandler_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e) {
+			Membrane.Log.write(Membrane.LogLevel.Debug, "Render area mouse leave");
 		}
 	}
 }
