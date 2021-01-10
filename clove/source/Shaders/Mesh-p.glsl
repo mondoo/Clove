b1b380f030f4a3a397bfff3ea3ae5fed287dced8
Alex <wolfplayeral@yahoo.co.uk>
2021-01-10 11:39:26 +0000

    Lowered the max bias slightly
    This is just to be a bit more strict with the directional shadows

--- clove/source/Shaders/Mesh-p.glsl
+++ clove/source/Shaders/Mesh-p.glsl
@@ -69,7 +69,7 @@ void main(){
 	const float shiniess =  32.0f; //TODO: Add shiniess as a material param
 
 	const float minBias = 0.0005f;
-	const float maxBias = 0.05f;
+	const float maxBias = 0.025f;
 
 	float shadow = 0.0f;
 
