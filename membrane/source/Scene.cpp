6bbea16503fef0d68a185dfb1376e675cad7592a
Alex <wolfplayeral@yahoo.co.uk>
2021-01-11 08:20:40 +0000

    Put together a rough layout of how the yaml file should be

--- membrane/source/Scene.cpp
+++ membrane/source/Scene.cpp
@@ -1,9 +1,9 @@
 #include "Membrane/Scene.hpp"
 
-#include <Clove/ECS/EntityManager.hpp>
 #include <Clove/Application.hpp>
-#include <yaml-cpp/yaml.h>
+#include <Clove/ECS/EntityManager.hpp>
 #include <fstream>
+#include <yaml-cpp/yaml.h>
 
 namespace garlic::membrane {
     Scene::Scene(std::filesystem::path scenefile) {
@@ -11,7 +11,7 @@ namespace garlic::membrane {
     }
 
     Scene::~Scene() {
-        //Save???
+        //Save??? Maybe like scene_backup instead of overwriting main file.
     }
 
     clove::Entity Scene::createEntity() {
@@ -20,15 +20,30 @@ namespace garlic::membrane {
         return entity;
     }
 
+    //TODO: Nodes should be built when created. This should just emitt to file.
     void Scene::save() {
         //NOTE: this node will be loaded from a file.
-        YAML::Node node;
+        YAML::Node rootNode;
+
+        rootNode["Version"] = 0;
+        rootNode["SceneId"] = 1234567890;
+
         for(auto entity : entities) {
-            node["Entities"].push_back(entity);
+            YAML::Node entityData;
+            entityData["id"]   = entity;
+            entityData["name"] = "Test Entity Name";
+
+            rootNode["Entities"].push_back(entityData);
+
+            YAML::Node components;
+            components["TestComponent_1"] = std::vector<float>{ 100.3f, 29.1f, 999.0f };
+            components["TestComponent_2"] = "Name: XYZ";
+
+            entityData["components"].push_back(components);
         }
 
         YAML::Emitter emitter;
-        emitter << node;
+        emitter << rootNode;
 
         //TODO: Using just yaml for now but we'd probably want our own type.
         std::filesystem::path outPath{ std::filesystem::current_path() / "scene.yaml" };
