93bcb7db6154f10d88b63c9cee1d0d0085376ed5
Alex <wolfplayeral@yahoo.co.uk>
2021-01-16 17:45:59 +0000

    Added default values for all animation types

--- clove/include/Clove/Rendering/AnimationTypes.hpp
+++ clove/include/Clove/Rendering/AnimationTypes.hpp
@@ -13,31 +13,37 @@ namespace garlic::clove {
 
     struct Joint {
         std::string name;
-        mat4f inverseBindPose;//Bm->j
-        std::optional<JointIndexType> parentIndex;
+        mat4f inverseBindPose{ 1.0f }; /**< Bm->j */
+        std::optional<JointIndexType> parentIndex{};
     };
 
     struct Skeleton {
-        std::vector<Joint> joints;
+        std::vector<Joint> joints{};
     };
 
-    //The position that the joint will be in when this pose is applied to it (in joint space relative to parent)
+    /**
+     * @brief The position that the joint will be in when this pose is applied to it (in joint space relative to parent)
+     */
     struct JointPose {
         quatf rotation{ vec3f{ 0.0f } };
         vec3f position{ 0.0f };
         vec3f scale{ 1.0f };
     };
 
-    //The pose for an entire skeleton, a keyframe in the animation
+    /**
+     * @brief The pose for an entire skeleton, a keyframe in the animation
+     */
     struct AnimationPose {
-        float timeStamp;
-        std::vector<JointPose> poses;//one for each joint
+        float timeStamp{ 0.0f };
+        std::vector<JointPose> poses{}; /**< one for each joint */
     };
 
-    //An entire animation (like walk or run)
+    /**
+     * @brief An entire animation (like walk or run)
+     */
     struct AnimationClip {
-        Skeleton *skeleton;
-        float duration;
-        std::vector<AnimationPose> poses;//all the poses for this clip
+        Skeleton *skeleton{ nullptr };
+        float duration{ 0.0f };
+        std::vector<AnimationPose> poses{}; /**< all the poses for this clip */
     };
 }
