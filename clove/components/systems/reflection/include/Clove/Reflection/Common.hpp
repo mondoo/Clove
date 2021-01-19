204c065382c5e1814146b5778065491702833a9e
Alex <wolfplayeral@yahoo.co.uk>
2021-01-19 08:05:28 +0000

    Added the stub for getting type info on an object

--- clove/components/systems/reflection/include/Clove/Reflection/Common.hpp
+++ clove/components/systems/reflection/include/Clove/Reflection/Common.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <type_traits>
+
 namespace garlic::clove {
     template<typename T>
     struct TypeInfo {};
+
+    template<typename T>
+    TypeInfo<T> getTypeInfo(T const &object){
+        //TODO: Populate with data
+        return TypeInfo<T>{};
+    }
 }
--- clove/components/systems/reflection/tests/ReflectionTests.cpp
+++ clove/components/systems/reflection/tests/ReflectionTests.cpp
@@ -9,10 +9,19 @@ struct SimpleTestClass {
 CLOVE_REFLECT_TYPE(SimpleTestClass);
 CLOVE_REFLECT_END
 
-TEST(ReflectionTests, CanGetStaticBasicInformationOnAType) {
+TEST(ReflectionTests, CanGetBasicStaticInformationOnAType) {
     EXPECT_EQ(TypeInfo<SimpleTestClass>::name, "SimpleTestClass");
     EXPECT_EQ(TypeInfo<SimpleTestClass>::size, sizeof(SimpleTestClass));
     EXPECT_EQ(TypeInfo<SimpleTestClass>::memberCount, 0);
+}
+
+TEST(ReflectionTests, CanGetBasicObjectInformationOnAType) {
+    SimpleTestClass testClass{};
+    TypeInfo<SimpleTestClass> typeInfo{ getTypeInfo(testClass) };
+
+    EXPECT_EQ(typeInfo.name, "SimpleTestClass");
+    EXPECT_EQ(typeInfo.size, sizeof(SimpleTestClass));
+    EXPECT_EQ(typeInfo.memberCount, 0);
 }
 
 struct SimpleMemberTestClass {
@@ -24,12 +33,25 @@ CLOVE_REFLECT_MEMBER(intMember)
 CLOVE_REFLECT_MEMBER(floatMember)
 CLOVE_REFLECT_END
 
-TEST(ReflectionTests, CanGetStaticBasicMemberInformationOnAType) {
+TEST(ReflectionTests, CanGetBasicStaticMemberInformationOnAType) {
     ASSERT_EQ(TypeInfo<SimpleMemberTestClass>::memberCount, 2);
     EXPECT_EQ(TypeInfo<SimpleMemberTestClass>::MemberInfo<0>::name, "intMember");
     //EXPECT_EQ(testClassInfo.members[0].size, sizeof(SimpleMemberTestClass::intMember));
     //EXPECT_EQ(testClassInfo.members[0].offset, offsetof(SimpleMemberTestClass, intMember));
     EXPECT_EQ(TypeInfo<SimpleMemberTestClass>::MemberInfo<1>::name, "floatMember");
+    //EXPECT_EQ(testClassInfo.members[1].size, sizeof(SimpleMemberTestClass::floatMember));
+    //EXPECT_EQ(testClassInfo.members[1].offset, offsetof(SimpleMemberTestClass, floatMember));
+}
+
+TEST(ReflectionTests, CanGetBasicObjectMemberInformationOnAType) {
+    SimpleMemberTestClass testClass{};
+    TypeInfo<SimpleMemberTestClass> typeInfo{ getTypeInfo(testClass) };
+
+    ASSERT_EQ(typeInfo.memberCount, 2);
+    //EXPECT_EQ(typeInfo.name, "intMember");
+    //EXPECT_EQ(testClassInfo.members[0].size, sizeof(SimpleMemberTestClass::intMember));
+    //EXPECT_EQ(testClassInfo.members[0].offset, offsetof(SimpleMemberTestClass, intMember));
+    //EXPECT_EQ(typeInfo.name, "floatMember");
     //EXPECT_EQ(testClassInfo.members[1].size, sizeof(SimpleMemberTestClass::floatMember));
     //EXPECT_EQ(testClassInfo.members[1].offset, offsetof(SimpleMemberTestClass, floatMember));
 }
