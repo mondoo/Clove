144a4533e6eeb3f32fea29f880d72974c055f75f
Alex <wolfplayeral@yahoo.co.uk>
2021-01-22 21:09:29 +0000

    Updated TextureLoader to use filesystem and Expected

--- clove/source/TextureLoader.cpp
+++ clove/source/TextureLoader.cpp
@@ -3,21 +3,22 @@
 #include <stb_image.h>
 
 namespace garlic::clove::TextureLoader {
-    LoadedTextureData loadTexture(std::string_view texturePath) {
-        stbi_set_flip_vertically_on_load(1); //Note: Might need to change this when other APIs come into play
+    Expected<LoadedTextureData, std::runtime_error> loadTexture(std::filesystem::path const &path) {
+        stbi_set_flip_vertically_on_load(1);//Note: Might need to change this when other APIs come into play
 
         int width;
         int height;
         int channels;
-        void* localBuffer = stbi_load(texturePath.data(), &width, &height, &channels, STBI_rgb_alpha);
+        void *localBuffer{ stbi_load(path.string().c_str(), &width, &height, &channels, STBI_rgb_alpha) };
 
-        LoadedTextureData data{};
-        data.buffer     = std::unique_ptr<void, std::function<void(void*)>>(localBuffer, [](void* buffer) {
-            stbi_image_free(buffer);
-        });
-        data.dimensions = { static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
-        data.channels   = STBI_rgb_alpha;//Make sure the channels are what we forced it to be
+        if(localBuffer == nullptr){
+            return Unexpected{ std::runtime_error{ "Failed to load texture!" } };
+        }
 
-        return data;
+        return LoadedTextureData{
+            .buffer = std::unique_ptr<void, std::function<void(void *)>>(localBuffer, [](void *buffer) { stbi_image_free(buffer); }),
+            .dimensions = { static_cast<uint32_t>(width), static_cast<uint32_t>(height) },
+            .channels   = STBI_rgb_alpha,//Make sure the channels are what we forced it to be
+        };
     }
 }
