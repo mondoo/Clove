1ec15ace30531829ba737b2c7dfe1bce74772d35
Alex <wolfplayeral@yahoo.co.uk>
2021-01-23 10:25:48 +0000

    Added stub for the geometry pass

---
+++ clove/include/Clove/Rendering/RenderPasses/GeometryPass.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace garlic::clove{
+    /**
+     * @brief A GeometryPass performs graphics operations to render individual pieces of geometry
+     */
+    class GeometryPass{
+    };
+}
