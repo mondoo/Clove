aea0572ca759c0f17d101708fdb01e9a3af41dd4
Alex <wolfplayeral@yahoo.co.uk>
2021-02-06 10:45:15 +0000

    Added stubs for the compute command buffer / queue

---
+++ clove/components/core/graphics/include/Clove/Graphics/GhaComputeCommandBuffer.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace garlic::clove {
+    /**
+     * @brief A buffer for recording compute commands.
+     * @details Allocated from a GhaComputeQueue.
+     */
+    class GhaComputeCommandBuffer {
+    };
+}
---
+++ clove/components/core/graphics/include/Clove/Graphics/GhaComputeQueue.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace garlic::clove {
+
+}
+
+namespace garlic::clove {
+    /**
+     * @brief Creates buffers that can be recorded for compute commands and then submitted.
+     */
+    class GhaComputeQueue {
+    };
+}
