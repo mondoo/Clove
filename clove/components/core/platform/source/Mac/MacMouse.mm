546638c7ac9cfdfb13b098ba06268ad3f3cb1813
Alex <wolfplayeral@yahoo.co.uk>
2021-03-01 07:01:05 +0000

    Added stubs for mouse control on macos

---
+++ clove/components/core/platform/source/Mac/MacMouse.mm
@@ -0,0 +1,13 @@
+#include "Clove/Platform/Input/Mouse.hpp"
+
+namespace garlic::clove {
+	vec2i Mouse::getPosition() const {
+		return {};
+	}
+
+	void Mouse::setPosition(vec2i const &position) {
+	}
+
+	void Mouse::show(bool shouldShow) {
+	}
+}
