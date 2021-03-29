1ddb975dd4a51e65479f9f25b9eda67c1fd2892c
Alex <wolfplayeral@yahoo.co.uk>
2021-03-29 06:50:20 +0100

    Created the MetalFramebuffer class

---
+++ clove/components/core/graphics/include/Clove/Graphics/Metal/MetalFramebuffer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Clove/Graphics/GhaFramebuffer.hpp"
+
+#include <MetalKit/MetalKit.h>
+
+namespace garlic::clove {
+	class MetalFramebuffer : public GhaFramebuffer {
+		//VARIABLES
+	private:
+		MTLRenderPassDescriptor *renderPassDescriptor{ nullptr };
+		
+		//FUNCTIONS
+	public:
+		MetalFramebuffer() = delete;
+		MetalFramebuffer(MTLRenderPassDescriptor *renderPassDescriptor);
+		
+		MetalFramebuffer(MetalFramebuffer const &other) = delete;
+		MetalFramebuffer(MetalFramebuffer &&other) noexcept;
+		
+		MetalFramebuffer& operator=(MetalFramebuffer const &other) = delete;
+		MetalFramebuffer& operator=(MetalFramebuffer &&other) noexcept;
+		
+		~MetalFramebuffer();
+		
+		MTLRenderPassDescriptor *getRenderPassDescriptor() const;
+	};
+}
---
+++ clove/components/core/graphics/source/Metal/MetalFramebuffer.mm
@@ -0,0 +1,17 @@
+#include "Clove/Graphics/Metal/MetalFramebuffer.hpp"
+
+namespace garlic::clove {
+	MetalFramebuffer::MetalFramebuffer(MTLRenderPassDescriptor *renderPassDescriptor)
+		: renderPassDescriptor{ renderPassDescriptor } {
+	}
+	
+	MetalFramebuffer::MetalFramebuffer(MetalFramebuffer &&other) noexcept = default;
+	
+	MetalFramebuffer& MetalFramebuffer::operator=(MetalFramebuffer &&other) noexcept = default;
+	
+	MetalFramebuffer::~MetalFramebuffer() = default;
+	
+	MTLRenderPassDescriptor *MetalFramebuffer::getRenderPassDescriptor() const {
+		return renderPassDescriptor;
+	}
+}
