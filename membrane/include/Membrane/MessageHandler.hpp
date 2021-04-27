bdaeab3075a1654014fbd63d656e4c2740c3093c
Alex <wolfplayeral@yahoo.co.uk>
2021-04-27 20:06:40 +0100

    MessageHandler now separates engine and editor messages
    These messages are then dispatched at separate times to better prevent thread synchronisation issues

--- membrane/include/Membrane/MessageHandler.hpp
+++ membrane/include/Membrane/MessageHandler.hpp
@@ -1,33 +1,65 @@
 #pragma once
 
 namespace garlic::membrane {
+    ref class EditorMessage;
+    ref class EngineMessage;
+}
+
+namespace garlic::membrane {
     // clang-format off
     //Delegates
     generic<class MessageType> 
     public delegate void MessageSentHandler(MessageType message);
 
     //Helper types
-    private interface class IMessageDelegateWrapper{};
+    private interface class IMessageDelegateWrapper {
+    public:
+        virtual void tryInvoke(EditorMessage ^message) = 0;
+        virtual void tryInvoke(EngineMessage ^message) = 0;
+    };
 
     generic<class MessageType> 
     private ref class MessageDelegateWrapper : public IMessageDelegateWrapper {
+        //VARIABLES
     public:
         MessageSentHandler<MessageType> ^ handler;
+
+        //FUNCTIONS
+    public:
+        virtual void tryInvoke(EditorMessage ^message) {
+            tryInvoke_interal(message);
+        }
+
+        virtual void tryInvoke(EngineMessage ^message) {
+            tryInvoke_interal(message);
+        }
+
+    private:
+        generic<class InvokeMessageType>
+        void tryInvoke_interal(InvokeMessageType message) {
+            if (message->GetType() == MessageType::typeid) {
+                handler->Invoke((MessageType)message);
+            }
+        }
     };
 
     public ref class MessageHandler {
     private:
-        static int loopCount{0};
+        static System::Object^ editorLock{ gcnew System::Object() };
 
-        static System::Collections::Generic::List<IMessageDelegateWrapper^> ^ delegates = gcnew System::Collections::Generic::List<IMessageDelegateWrapper^>;
-        static System::Collections::Generic::List<IMessageDelegateWrapper^> ^ deferedDelegates = gcnew System::Collections::Generic::List<IMessageDelegateWrapper^>;
+        static System::Collections::Generic::List<IMessageDelegateWrapper ^> ^delegates{ gcnew System::Collections::Generic::List<IMessageDelegateWrapper^> };
 
+        static System::Collections::Generic::List<EditorMessage ^> ^editorMessages{ gcnew System::Collections::Generic::List<EditorMessage ^> };
+        static System::Collections::Generic::List<EngineMessage ^> ^engineMessages{ gcnew System::Collections::Generic::List<EngineMessage ^> };
     public:
         generic<class MessageType> 
-        static void bindToMessage(MessageSentHandler<MessageType> ^ handler);
+        static void bindToMessage(MessageSentHandler<MessageType> ^handler);
 
-        generic<class MessageType> 
+        generic<class MessageType>
         static void sendMessage(MessageType message);
+
+        static void flushEditor();
+        static void flushEngine();
     };
     // clang-format on
 }
--- membrane/source/MessageHandler.cpp
+++ membrane/source/MessageHandler.cpp
@@ -1,36 +1,58 @@
 #include "Membrane/MessageHandler.hpp"
 
+#include "Membrane/Messages.hpp"
+
+#include <Clove/Log/Log.hpp>
+#include <msclr/lock.h>
+#include <msclr/marshal_cppstd.h>
+
+CLOVE_DECLARE_LOG_CATEGORY(MEMBRANE)
+
 namespace garlic::membrane {
     // clang-format off
     generic<class MessageType> 
     void MessageHandler::bindToMessage(MessageSentHandler<MessageType> ^ handler) {
-        MessageDelegateWrapper<MessageType> ^ wrapper { gcnew MessageDelegateWrapper<MessageType> };
+        MessageDelegateWrapper<MessageType> ^wrapper{ gcnew MessageDelegateWrapper<MessageType> };
         wrapper->handler = handler;
 
-        //Make sure we don't add to the current list while looping
-        if(loopCount == 0){
-            delegates->Add(wrapper);
-        }else{
-            deferedDelegates->Add(wrapper);
+        delegates->Add(wrapper);
+    }
+
+    generic<class MessageType>
+    void MessageHandler::sendMessage(MessageType message){
+        System::Type ^const baseType{ message->GetType()->BaseType };        
+
+        if (baseType == EditorMessage::typeid) {
+            msclr::lock scopedLock{ editorLock };
+            editorMessages->Add((EditorMessage^)message);
+        } else if (baseType == EngineMessage::typeid) {
+            engineMessages->Add((EngineMessage^)message);
+        } else {
+            CLOVE_LOG(LOG_CATEGORY_MEMBRANE, clove::LogLevel::Warning, "{0} called with neither an Editor or Engine message! Base type is: {1}", CLOVE_FUNCTION_NAME_PRETTY, msclr::interop::marshal_as<std::string>(baseType->Name));
         }
     }
 
-    generic<class MessageType> 
-    void MessageHandler::sendMessage(MessageType message){
-        ++loopCount;
-        for each(IMessageDelegateWrapper^ wrapper in delegates){
-            if(wrapper->GetType() == MessageDelegateWrapper<MessageType>::typeid) {
-                ((MessageDelegateWrapper<MessageType>^)wrapper)->handler->Invoke(message);
-            }
-        }
-        --loopCount;
+    void MessageHandler::flushEditor() {
+        //All messages are dispatched from the engine thread so we need to lock the editor
+        msclr::lock scopedLock{ editorLock };
 
-        if(loopCount == 0){
-            for each(IMessageDelegateWrapper^ wrapper in deferedDelegates){
-                delegates->Add(wrapper);
+        for each(EditorMessage ^message in editorMessages) {
+            for each(IMessageDelegateWrapper^ wrapper in delegates){
+                wrapper->tryInvoke(message);
             }
-            deferedDelegates->Clear();
         }
+
+        editorMessages->Clear();
+    }
+
+    void MessageHandler::flushEngine() {
+        for each(EngineMessage ^message in engineMessages) {            
+            for each(IMessageDelegateWrapper^ wrapper in delegates){
+                wrapper->tryInvoke(message);
+            }
+        }
+
+        engineMessages->Clear();
     }
     // clang-format on
 }
