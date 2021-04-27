df82d1063743edbcdb65d371eadceb3991e2cd51
Alex <wolfplayeral@yahoo.co.uk>
2021-04-27 07:01:46 +0100

    Added a way to distinguish between editor / engine messages

--- membrane/include/Membrane/Messages.hpp
+++ membrane/include/Membrane/Messages.hpp
@@ -4,25 +4,23 @@
 
 namespace garlic::membrane {
     // clang-format off
-    public ref class Editor_CreateEntity {};
-    public ref class Engine_OnEntityCreated {
-    public:
-        System::UInt32 entity;
+    //Types
+    public ref struct Entity{
+        System::UInt32 id;
         System::String ^name;
+        System::Collections::Generic::List<ComponentType> ^components;
     };
 
-    public ref class Editor_CreateComponent {
+    //Messages sent from Bulb
+    public ref class EditorMessage{};
+
+    public ref class Editor_CreateEntity : public EditorMessage {};
+    public ref class Editor_CreateComponent : public EditorMessage {
     public:
         System::UInt32 entity;
         ComponentType componentType;
     };
-    public ref class Engine_OnComponentCreated {
-    public:
-        System::UInt32 entity;
-        ComponentType componentType;
-    };
-
-    public ref class Editor_UpdateTransform{
+    public ref class Editor_UpdateTransform : public EditorMessage {
     public:
         System::UInt32 entity;
 
@@ -30,23 +28,13 @@ namespace garlic::membrane {
         Vector3 rotation;
         Vector3 scale;
     };
-    public ref class Engine_OnTransformChanged{
-    public:
-        System::UInt32 entity;
-        
-        Vector3 position;
-        Vector3 rotation;
-        Vector3 scale;
-    };
-
-    public ref class Editor_UpdateName{
+    public ref class Editor_UpdateName : public EditorMessage {
     public:
         System::UInt32 entity;
 
         System::String^ name;
     };
-
-    public ref class Editor_ViewportKeyEvent{
+    public ref class Editor_ViewportKeyEvent : public EditorMessage {
     public:
         enum class Type{
             Pressed,
@@ -57,29 +45,41 @@ namespace garlic::membrane {
         System::Windows::Input::Key key;
         Type type;
     };
-
-    public ref class Editor_ViewportMouseButtonEvent{
+    public ref class Editor_ViewportMouseButtonEvent : public EditorMessage {
     public:
         System::Windows::Input::MouseButton button;
         System::Windows::Input::MouseButtonState state;
         System::Windows::Point position;
     };
-
-    public ref class Editor_ViewportMouseMoveEvent{
+    public ref class Editor_ViewportMouseMoveEvent : public EditorMessage {
     public:
         System::Windows::Point position;
     };
+    public ref class Editor_SaveScene : public EditorMessage {};
+    public ref class Editor_LoadScene : public EditorMessage {};
 
-    public ref class Editor_SaveScene{};
+    //Messages sent from Clove
+    public ref class EngineMessage{}; 
 
-    public ref struct Entity{
-        System::UInt32 id;
+    public ref class Engine_OnEntityCreated : public EngineMessage {
+    public:
+        System::UInt32 entity;
         System::String ^name;
-        System::Collections::Generic::List<ComponentType> ^components;
     };
-
-    public ref class Editor_LoadScene{};
-    public ref class Engine_OnSceneLoaded{
+    public ref class Engine_OnComponentCreated : public EngineMessage {
+    public:
+        System::UInt32 entity;
+        ComponentType componentType;
+    };
+    public ref class Engine_OnTransformChanged : public EngineMessage {
+    public:
+        System::UInt32 entity;
+        
+        Vector3 position;
+        Vector3 rotation;
+        Vector3 scale;
+    };
+    public ref class Engine_OnSceneLoaded : public EngineMessage {
     public:
         System::Collections::Generic::List<Entity^> ^entities;
     };
