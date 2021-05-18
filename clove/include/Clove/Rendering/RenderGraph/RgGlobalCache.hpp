fccf63fb73b874b748abbe6275ed53997245b9ca
Alex <wolfplayeral@yahoo.co.uk>
2021-05-18 07:45:22 +0100

    Global cache can allocate the descriptor set layouts

--- clove/include/Clove/Rendering/RenderGraph/RgGlobalCache.hpp
+++ clove/include/Clove/Rendering/RenderGraph/RgGlobalCache.hpp
@@ -11,14 +11,39 @@
 #include <unordered_map>
 
 namespace garlic::clove {
+    class GhaFactory;
+}
+
+namespace garlic::clove {
     /**
      * @brief Stores objects that will be used regardless of what frame is being rendered (shaders, pipelines, etc.)
      */
     class RgGlobalCache {
+        //TYPES
+    private:
+        using PoolId = uint64_t;
+
         //VARIABLES
     private:
+        std::shared_ptr<GhaFactory> factory{ nullptr };
+
+        std::unordered_map<PoolId, std::shared_ptr<GhaRenderPass>> renderPasses{};
+        std::unordered_map<PoolId, std::shared_ptr<GhaDescriptorSetLayout>> descriptorSetLayouts{};
+        std::unordered_map<PoolId, std::shared_ptr<GhaGraphicsPipelineObject>> graphicsPipelines{};
+
         //FUNCTIONS
     public:
+        RgGlobalCache() = delete;
+        RgGlobalCache(std::shared_ptr<GhaFactory> ghaFactory);
+
+        RgGlobalCache(RgGlobalCache const &other) = delete;
+        RgGlobalCache(RgGlobalCache &&other) noexcept;
+
+        RgGlobalCache &operator=(RgGlobalCache const &other) = delete;
+        RgGlobalCache &operator=(RgGlobalCache &&other) noexcept;
+
+        ~RgGlobalCache();
+
         std::shared_ptr<GhaShader> createShader(std::filesystem::path const &file, GhaShader::Stage shaderStage);
         std::shared_ptr<GhaShader> createShader(std::string_view source, std::unordered_map<std::string, std::string> includeSources, std::string_view shaderName, GhaShader::Stage shaderStage);
 
--- clove/source/Rendering/RenderGraph/RgGlobalCache.cpp
+++ clove/source/Rendering/RenderGraph/RgGlobalCache.cpp
@@ -1,7 +1,20 @@
 #include "Clove/Rendering/RenderGraph/RgGlobalCache.hpp"
 
 #include "Clove/Rendering/RenderGraph/CacheUtils.hpp"
+
+#include <Clove/Graphics/GhaFactory.hpp>
+
 namespace garlic::clove {
+     RgGlobalCache::RgGlobalCache(std::shared_ptr<GhaFactory> ghaFactory)
+        : factory{ std::move(ghaFactory) } {
+    }
+
+    RRgGlobalCache::gGlobalCache(RgGlobalCache &&other) noexcept = default;
+
+    RgGlobalCache &RgGlobalCache::operator=(RgGlobalCache &&other) noexcept = default;
+
+    RgGlobalCache::~RgGlobalCache() = default;
+
     std::shared_ptr<GhaShader> RgGlobalCache::createShader(std::filesystem::path const &file, GhaShader::Stage shaderStage) {
         //TODO
         return nullptr;
@@ -18,8 +31,19 @@ namespace garlic::clove {
     }
 
     std::shared_ptr<GhaDescriptorSetLayout> RgGlobalCache::createDescriptorSetLayout(GhaDescriptorSetLayout::Descriptor descriptor) {
-        //TODO
-        return nullptr;
+        PoolId layoutId{};
+        for(auto &binding : descriptor.bindings){
+            hashCombine(layoutId, binding.binding);
+            hashCombine(layoutId, binding.type);
+            hashCombine(layoutId, binding.arraySize);
+            hashCombine(layoutId, binding.stage);
+        }
+
+        if(!descriptorSetLayouts.contains(layoutId)){
+            descriptorSetLayouts[layoutId] = factory->createDescriptorSetLayout(std::move(descriptor)).getValue();
+        }
+
+        return descriptorSetLayouts.at(layoutId);
     }
 
     std::shared_ptr<GhaGraphicsPipelineObject> RgGlobalCache::createGraphicsPipelineObject(GhaGraphicsPipelineObject::Descriptor descriptor) {
