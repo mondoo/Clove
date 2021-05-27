11ac46a37a8a8b5dffa6b55d1f6a25502aa100f6
Alex <wolfplayeral@yahoo.co.uk>
2021-05-27 07:54:48 +0100

    Added get methods for both model types to the AssetManager

--- clove/include/Clove/FileSystem/AssetManager.hpp
+++ clove/include/Clove/FileSystem/AssetManager.hpp
@@ -1,6 +1,24 @@
 #pragma once
 
+#include "Clove/FileSystem/AssetPtr.hpp"
+#include "Clove/Rendering/Renderables/StaticModel.hpp"
+#include "Clove/Rendering/Renderables/AnimatedModel.hpp"
+
+#include <unordered_map>
+#include <filesystem>
+
 namespace garlic::clove {
     class AssetManager {
+        //VARIABLES
+    private:
+        std::unordered_map<std::string, AssetPtr<StaticModel>> staticModels{};
+        std::unordered_map<std::string, AssetPtr<AnimatedModel>> animatedModels{};
+
+        //FUNCTIONS
+    public:
+        //TODO: Ctors
+
+        AssetPtr<StaticModel> getStaticModel(std::filesystem::path const &filePath);
+        AssetPtr<AnimatedModel> getAnimatedModel(std::filesystem::path const &filePath);
     };
 }
--- clove/source/FileSystem/AssetManager.cpp
+++ clove/source/FileSystem/AssetManager.cpp
@@ -1,5 +1,25 @@
 #include "Clove/FileSystem/AssetManager.hpp"
 
+#include "Clove/ModelLoader.hpp"
+
 namespace garlic::clove {
-    
+    AssetPtr<StaticModel> AssetManager::getStaticModel(std::filesystem::path const &filePath) {
+        std::string const pathString{ filePath.string() };
+
+        if(!staticModels.contains(pathString)) {
+            staticModels[pathString] = AssetPtr<StaticModel>{ filePath, &ModelLoader::loadStaticModel };
+        }
+
+        return staticModels.at(pathString);
+    }
+
+    AssetPtr<AnimatedModel> AssetManager::getAnimatedModel(std::filesystem::path const &filePath) {
+        std::string const pathString{ filePath.string() };
+
+        if(!animatedModels.contains(pathString)) {
+            animatedModels[pathString] = AssetPtr<AnimatedModel>{ filePath, &ModelLoader::loadAnimatedModel };
+        }
+
+        return animatedModels.at(pathString);
+    }
 }
