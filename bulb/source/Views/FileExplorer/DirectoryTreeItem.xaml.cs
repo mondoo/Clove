73586bd7b19903154001bd24b7d0182d72d42486
Alex <wolfplayeral@yahoo.co.uk>
2021-06-18 16:30:31 +0100

    Added a simple directory tree view

---
+++ bulb/source/Views/FileExplorer/DirectoryTreeItem.xaml.cs
@@ -0,0 +1,41 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Garlic.Bulb {
+    /// <summary>
+    /// An idividual item inside of a directory tree.
+    /// </summary>
+    public partial class DirectoryTreeItem : UserControl {
+        private bool isExpanded = false;
+
+        public DirectoryTreeItem() {
+            InitializeComponent();
+            UpdateExpansionIcon();
+
+            ExpandIcon.Click += ToggleExpansion;
+        }
+
+        private void ToggleExpansion(object sender, RoutedEventArgs e) {
+            isExpanded = !isExpanded;
+
+            if (isExpanded) {
+                var viewModel = (DirectoryItemViewModel)DataContext;
+                foreach (var subDir in viewModel.SubDirectories) {
+                    var item = new DirectoryTreeItem();
+                    item.DataContext = subDir;
+
+                    ContentPanel.Children.Add(item);
+                }
+            } else {
+                ContentPanel.Children.Clear();
+            }
+
+            UpdateExpansionIcon();
+        }
+
+        private void UpdateExpansionIcon() {
+            ExpandIcon.Content = isExpanded ? "V" : ">";
+        }
+    }
+}
