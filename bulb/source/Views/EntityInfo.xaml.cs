314d30f77dbca29b2989ca3011ac1aad627e4eed
Alex <wolfplayeral@yahoo.co.uk>
2021-07-02 20:26:46 +0100

    Refactored out the entity info into it's own view

---
+++ bulb/source/Views/EntityInfo.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Garlic.Bulb {
+    /// <summary>
+    /// A UserControl that displays infomation about an entity (name, components, etc.)
+    /// </summary>
+    public partial class EntityInfo : UserControl {
+        public EntityInfo() {
+            InitializeComponent();
+        }
+    }
+}
