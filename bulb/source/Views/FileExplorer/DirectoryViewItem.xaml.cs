f3a75100421ec9f7c2233239fc4d1d9d36313cf8
Alex <wolfplayeral@yahoo.co.uk>
2021-07-03 09:23:45 +0100

    Moved the directory view items into their own view

---
+++ bulb/source/Views/FileExplorer/DirectoryViewItem.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace Garlic.Bulb {
+    public partial class DirectoryViewItem : UserControl {
+        public DirectoryViewItem() {
+            InitializeComponent();
+        }
+    }
+}
