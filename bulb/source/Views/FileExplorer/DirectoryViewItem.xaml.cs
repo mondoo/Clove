f91a8e39cc2a6f28605e2f98e52254eb6bdd3fdb
Alex <wolfplayeral@yahoo.co.uk>
2021-07-03 10:39:42 +0100

    Enabled drag drop operation on the directory view item

--- bulb/source/Views/FileExplorer/DirectoryViewItem.xaml.cs
+++ bulb/source/Views/FileExplorer/DirectoryViewItem.xaml.cs
@@ -7,5 +7,20 @@ namespace Garlic.Bulb {
         public DirectoryViewItem() {
             InitializeComponent();
         }
+
+        protected override void OnMouseMove(MouseEventArgs e) {
+            base.OnMouseMove(e);
+
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                var viewModel = (DirectoryItemViewModel)DataContext;
+                //Only drag-drop files for now
+                if (viewModel.Type == ObjectType.File) {
+                    DataObject data = new DataObject();
+                    data.SetData(DataFormats.StringFormat, viewModel.FullName);
+
+                    DragDrop.DoDragDrop(this, data, DragDropEffects.Copy);
+                }
+            }
+        }
     }
 }
