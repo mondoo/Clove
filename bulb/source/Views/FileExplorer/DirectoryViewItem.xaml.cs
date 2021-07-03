f51bc1b33fa8fbb1e6d88cac67bf0c0b709e47d0
Alex <wolfplayeral@yahoo.co.uk>
2021-07-03 10:37:58 +0100

    Flattened the DirectoryViewItem to be a button itself
    This was primarily done to fix event propagation issues

--- bulb/source/Views/FileExplorer/DirectoryViewItem.xaml.cs
+++ bulb/source/Views/FileExplorer/DirectoryViewItem.xaml.cs
@@ -1,7 +1,9 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Garlic.Bulb {
-    public partial class DirectoryViewItem : UserControl {
+    public partial class DirectoryViewItem : Button {
         public DirectoryViewItem() {
             InitializeComponent();
         }
