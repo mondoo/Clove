fad950c81b83ada74bd8bd2a17632706a2b34d8f
Alex <wolfplayeral@yahoo.co.uk>
2021-07-03 10:58:47 +0100

    StaticModelComponentViewModel now receives file path when dropped

--- bulb/source/Views/EntityInfo.xaml.cs
+++ bulb/source/Views/EntityInfo.xaml.cs
@@ -9,5 +9,16 @@ namespace Garlic.Bulb {
         public EntityInfo() {
             InitializeComponent();
         }
+
+        private void TextBox_Drop(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.StringFormat)) {
+                var textBox = (TextBlock)sender;
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                textBox.Text = dataString;
+
+                e.Handled = true;
+            }
+        }
     }
 }
