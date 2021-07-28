c4d98284f6738cad4b1044e2cca2a84c05724102
Alex <wolfplayeral@yahoo.co.uk>
2021-07-28 07:50:20 +0100

    Shader compiler inverts the Y on MSL vertex shaders

--- clove/components/core/graphics/source/ShaderCompiler.cpp
+++ clove/components/core/graphics/source/ShaderCompiler.cpp
@@ -4,8 +4,8 @@
 
 #include <Clove/Definitions.hpp>
 #include <Clove/Log/Log.hpp>
-#include <glslc/file_includer.h>
 #include <fstream>
+#include <glslc/file_includer.h>
 #include <libshaderc_util/file_finder.h>
 #include <shaderc/shaderc.hpp>
 #include <spirv.hpp>
@@ -113,7 +113,7 @@ namespace garlic::clove::ShaderCompiler {
 
             shaderc::Compiler compiler{};
             shaderc::SpvCompilationResult spirvResult{ compiler.CompileGlslToSpv(source.data(), source.size(), getShadercStage(shaderStage), shaderName.data(), options) };
-            
+
             if(spirvResult.GetCompilationStatus() != shaderc_compilation_status_success) {
                 CLOVE_LOG(LOG_CATEGORY_CLOVE, LogLevel::Error, "Failed to compile shader {0}:\n\t{1}", shaderName, spirvResult.GetErrorMessage());
                 return Unexpected{ std::runtime_error{ "Failed to compile shader. See output log for details." } };
@@ -149,36 +149,40 @@ namespace garlic::clove::ShaderCompiler {
 
     Expected<std::string, std::runtime_error> spirvToMSL(std::span<uint32_t> spirvSource) {
         spirv_cross::CompilerMSL msl{ spirvSource.data(), spirvSource.size() };
-		msl.set_msl_options(spirv_cross::CompilerMSL::Options{
-			.platform                  = spirv_cross::CompilerMSL::Options::Platform::macOS,
-			.msl_version               = spirv_cross::CompilerMSL::Options::make_msl_version(2, 0),
-			.argument_buffers          = true,
-			.enable_decoration_binding = true,
-		});
+        msl.set_common_options(spirv_cross::CompilerGLSL::Options{
+            .vertex = {
+                .flip_vert_y = true,
+            },
+        });
+        msl.set_msl_options(spirv_cross::CompilerMSL::Options{
+            .platform                  = spirv_cross::CompilerMSL::Options::Platform::macOS,
+            .msl_version               = spirv_cross::CompilerMSL::Options::make_msl_version(2, 0),
+            .argument_buffers          = true,
+            .enable_decoration_binding = true,
+        });
 
-		spirv_cross::ShaderResources resources{ msl.get_shader_resources() };
-		
-		if(!resources.push_constant_buffers.empty()) {
-			auto const &resource{ resources.push_constant_buffers.front() };
-			spirv_cross::ID const bufferId{ resource.id };
-			spirv_cross::TypeID const bufferTypeId{ resource.base_type_id };
-			spirv_cross::SmallVector<spirv_cross::BufferRange> const bufferRanges{ msl.get_active_buffer_ranges(bufferId) };
-			
-			//Remove the padding added onto push constants when using an offset. The offset is not required in metal
-			//as pushing small chunks of data works much like uploading a buffer.
-			if(!bufferRanges.empty()) {
-				size_t const initialOffset{ msl.get_member_decoration(bufferTypeId, bufferRanges.front().index, spv::Decoration::DecorationOffset) };
-				for(auto const &range : bufferRanges) {
-					//Unset decoration does not seem to work for msl so just manually override the decoration instead.
-					msl.set_member_decoration(bufferTypeId, range.index, spv::Decoration::DecorationOffset, range.offset - initialOffset);
-				}
-			}
-			
-			//Remap the slot the push constant is in to stop overlap with descriptor sets.
-			msl.set_decoration(bufferId, spv::Decoration::DecorationBinding, pushConstantSlot);
-		}
-		
-		
+        spirv_cross::ShaderResources resources{ msl.get_shader_resources() };
+
+        if(!resources.push_constant_buffers.empty()) {
+            auto const &resource{ resources.push_constant_buffers.front() };
+            spirv_cross::ID const bufferId{ resource.id };
+            spirv_cross::TypeID const bufferTypeId{ resource.base_type_id };
+            spirv_cross::SmallVector<spirv_cross::BufferRange> const bufferRanges{ msl.get_active_buffer_ranges(bufferId) };
+
+            //Remove the padding added onto push constants when using an offset. The offset is not required in metal
+            //as pushing small chunks of data works much like uploading a buffer.
+            if(!bufferRanges.empty()) {
+                size_t const initialOffset{ msl.get_member_decoration(bufferTypeId, bufferRanges.front().index, spv::Decoration::DecorationOffset) };
+                for(auto const &range : bufferRanges) {
+                    //Unset decoration does not seem to work for msl so just manually override the decoration instead.
+                    msl.set_member_decoration(bufferTypeId, range.index, spv::Decoration::DecorationOffset, range.offset - initialOffset);
+                }
+            }
+
+            //Remap the slot the push constant is in to stop overlap with descriptor sets.
+            msl.set_decoration(bufferId, spv::Decoration::DecorationBinding, pushConstantSlot);
+        }
+
         return msl.compile();
     }
 }
