f864d4d5ba10a629cc29ad9d42c98614b1bfe758
Alex <wolfplayeral@yahoo.co.uk>
2021-12-13 07:34:03 +0000

    Updated syntax for StaticModelComponent

--- clove/source/Components/StaticModelComponent.cpp
+++ clove/source/Components/StaticModelComponent.cpp
@@ -5,4 +5,4 @@
 
 CLOVE_REFLECT_BEGIN(clove::StaticModelComponent, clove::EditorVisibleComponent{.name = "Static Model Component"})
 CLOVE_REFLECT_MEMBER(model, clove::EditorEditableMember{})
-CLOVE_REFLECT_END(clove::StaticModelComponent)
+CLOVE_REFLECT_END
