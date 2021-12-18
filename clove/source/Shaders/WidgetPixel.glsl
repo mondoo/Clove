6563686b9b1e60c20fa3e9f77426335e3c66d657
Alex <wolfplayeral@yahoo.co.uk>
2021-12-18 16:57:26 +0000

    Fixed binding slots in WidgetPixel

--- clove/source/Shaders/WidgetPixel.glsl
+++ clove/source/Shaders/WidgetPixel.glsl
@@ -7,8 +7,8 @@ layout(std140, set = 0, binding = 1) uniform Colour{
     vec4 colour;
 };
 
-layout(set = 0, binding = 0) uniform texture2D widgetTexture;
-layout(set = 0, binding = 1) uniform sampler texSampler;
+layout(set = 0, binding = 2) uniform texture2D widgetTexture;
+layout(set = 0, binding = 3) uniform sampler texSampler;
 
 layout(location = 0) in vec2 texCoord;
 
