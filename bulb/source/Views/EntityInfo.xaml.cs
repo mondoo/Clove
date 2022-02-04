516edc09af49a53cb6a033c2e00185a5ce06eedd
Alex <wolfplayeral@yahoo.co.uk>
2022-02-04 07:38:41 +0000

    Removed unused code

--- bulb/source/Views/EntityInfo.xaml.cs
+++ bulb/source/Views/EntityInfo.xaml.cs
@@ -20,37 +20,5 @@ namespace Bulb {
                 InfoStackPanel.Visibility = Visibility.Visible;
             }
         }
-
-        private void TextBox_Drop(object sender, DragEventArgs e) {
-            if (e.Data.GetDataPresent(DataFormats.StringFormat)) {
-                var textBox = (TextBlock)sender;
-                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
-
-                textBox.Text = dataString;
-
-                e.Handled = true;
-            }
-        }
-
-        private void TransformPropertyLostFocus(object sender, RoutedEventArgs e) {
-            //var element = sender as FrameworkElement;
-            //var viewmodel = element.DataContext as TransformComponentViewModel;
-
-            //viewmodel.RefreshValues();
-        }
-
-        private void RigidBodyPropertyLostFocus(object sender, RoutedEventArgs e) {
-            //var element = sender as FrameworkElement;
-            //var viewmodel = element.DataContext as RigidBodyComponentViewModel;
-
-            //viewmodel.RefreshValues();
-        }
-
-        private void CollisionShapePropertyLostFocus(object sender, RoutedEventArgs e) {
-            //var element = sender as FrameworkElement;
-            //var viewmodel = element.DataContext as CollisionShapeComponentViewModel;
-
-            //viewmodel.RefreshValues();
-        }
     }
 }
